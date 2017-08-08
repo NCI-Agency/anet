3de534fc7b96be21e70442277d40649e4074ddf2
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-08-08 15:01:42 +0200

    Add General Banner Text variable to admin settings for to show general notifications

--- insertBaseData.sql
+++ insertBaseData.sql
@@ -576,3 +576,4 @@ INSERT INTO adminSettings ([key], value) VALUES ('HELP_LINK_URL', 'http://google
 INSERT INTO adminSettings ([key], value) VALUES ('CONTACT_EMAIL', 'team-anet@dds.mil');
 INSERT INTO adminsettings ([key], value) VALUES ('DAILY_ROLLUP_MAX_REPORT_AGE_DAYS', '14');
 INSERT INTO adminsettings ([key], value) VALUES ('EXTERNAL_DOCUMENTATION_LINK_URL', '');
+INSERT INTO adminsettings ([key], value) VALUES ('GENERAL_BANNER_TEXT', '');
