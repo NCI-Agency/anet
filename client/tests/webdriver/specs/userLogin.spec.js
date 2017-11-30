18cacbee9a3c2a915bf0e821f8b2d12a058448e7
Edwin van den Houdt <edwin@vdhoudt.nl>
2017-11-30 09:59:24 +0100

    Move test specs back to correct file

--- client/tests/webdriver/specs/userLogin.spec.js
+++ client/tests/webdriver/specs/userLogin.spec.js
@@ -29,35 +29,5 @@ describe('Anet user login', () => {
 		// const securityText = Home.searchBar.getText()
 		// expect(securityText).to.equal('DEMO USE ONLY || DMIN, Arthur (edit)')
 	})
-	describe('When creating a Principle user', () => {
-		beforeEach('On the create a new user page...', () => {
-			CreatePerson.open()
-			CreatePerson.waitForFormToLoad()
-		})
-		it('Should save a principle with only a last name', () => {
-			CreatePerson.lastName.setValue('valid last name')
-			CreatePerson.submitForm()
-			CreatePerson.waitForAlertSuccessToLoad()
-			const alertMessage = CreatePerson.alertSuccess.getText()
-			expect(alertMessage).to.equal('Person saved successfully')
-		})
-		it('Should not save a principle without a valid email address', () => {
-			CreatePerson.lastName.setValue("VALID_PERSON_PRINCIPAL.lastName")
-			CreatePerson.emailAddress.setValue('notValidEmail@')
-			CreatePerson.submitForm()
-			const errorMessage = browser.element('input#emailAddress + span.help-block')
-			errorMessage.waitForExist(5000)
-			errorMessage.waitForVisible()
-			expect(errorMessage.getText()).to.equal('Valid email address is required')
-
-			// perform submit form to prevent warning dialog
-			CreatePerson.emailAddress.clearElement()
-			CreatePerson.emailAddress.setValue('valid@example.com')
-			CreatePerson.submitForm()
-			CreatePerson.waitForAlertSuccess()
-			const alertMessage = CreatePerson.alertSuccess.getText()
-			expect(alertMessage).to.equal('Person saved successfully')
-		})
-	})
 
 })
