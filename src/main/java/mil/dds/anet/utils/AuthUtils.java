c09d8d40239eb88869629c740474b3b578cab7d8
Ben Warfield <ben.warfield@hq.dhs.gov>
2017-12-04 16:39:17 -0500

    Added debug logging to AuthUtils.
    There was a fairly weird bug with OrganizationDao and postgresql which was
    traced using these statements: leaving them in for future confused people.

--- src/main/java/mil/dds/anet/utils/AuthUtils.java
+++ src/main/java/mil/dds/anet/utils/AuthUtils.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response.Status;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import mil.dds.anet.AnetObjectEngine;
 import mil.dds.anet.beans.Organization;
 import mil.dds.anet.beans.Organization.OrganizationType;
@@ -14,9 +17,12 @@ import mil.dds.anet.beans.Position.PositionType;
 
 public class AuthUtils {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthUtils.class);
+
 	public static String UNAUTH_MESSAGE = "You do not have permissions to do this";
 	
 	public static void assertAdministrator(Person user) { 
+		LOGGER.debug("Asserting admin status for {}", user);
 		if (user.loadPosition() != null
 				&& user.getPosition().getType() == PositionType.ADMINISTRATOR) { 
 			return;
@@ -25,12 +31,24 @@ public class AuthUtils {
 	}
 	
 	public static boolean isSuperUserForOrg(final Person user, final Organization org) {
-		if (org == null || org.getId() == null) { return false; } 
+		if (org == null || org.getId() == null) {
+			LOGGER.error("Organization {} is null or has a null ID in SuperUser check for {}",
+					org, user); // DANGER: possible log injection vector here?
+			return false;
+		}
 		Position position = user.loadPosition();
-		if (position == null) { return false; } 
-		if (position.getType() == PositionType.ADMINISTRATOR) { return true; }
+		if (position == null) {
+			LOGGER.warn("User {} has no position, hence no permissions", user);
+			return false;
+		}
+		if (position.getType() == PositionType.ADMINISTRATOR) {
+			LOGGER.debug("User {} is an administrator, automatically a superuser", user);
+			return true;
+		}
+		LOGGER.debug("Position for user {} is {}", user, position);
 		if (position.getType() != PositionType.SUPER_USER) { return false; } 
 
+		// Given that we know it's a super-user position, does it actually match this organization?
 		Organization loadedOrg = AnetObjectEngine.getInstance().getOrganizationDao().getById(org.getId());
 		if (loadedOrg.getType() == OrganizationType.PRINCIPAL_ORG) { return true; }
 		
@@ -47,11 +65,14 @@ public class AuthUtils {
 	}
 	
 	public static void assertSuperUserForOrg(Person user, Organization org) {
-		if (isSuperUserForOrg(user, org)) { return; } 
+		// log injection possibility here?
+		LOGGER.debug("Asserting superuser status for {} in {}", user, org);
+		if (isSuperUserForOrg(user, org)) { return; }
 		throw new WebApplicationException(UNAUTH_MESSAGE, Status.FORBIDDEN);
 	}
 
 	public static void assertSuperUser(Person user) {
+		LOGGER.debug("Asserting some superuser status for {}", user);
 		Position position = user.loadPosition();
 		if (position != null
 			&& (position.getType() == PositionType.SUPER_USER
