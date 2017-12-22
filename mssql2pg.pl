c49e5efd00c2c969644923ff5f6e123df70c448a
Ben Warfield <ben.warfield@hq.dhs.gov>
2017-12-22 13:12:54 -0500

    Updated sql-munging scripts to handle new special cases.
    Updated mssql2sqlite.sh and mssql2pg.pl to handle date math, and mssql2pg.pl
    to handle one more integer/boolean conversion.

--- mssql2pg.pl
+++ mssql2pg.pl
@@ -11,3 +11,8 @@ s/(?<!")([a-z]\w+[A-Z]\w+)/"$1"/g;
 s/(?<=\Q(SELECT max(id) FROM reports), \E)([10])/$1 ? 'TRUE' : 'FALSE'/ie;
 # This one is for "deleted" in "positionRelationships"
 s/(?<=CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, )([10])(?=\s*\))/$1 ? 'TRUE' : 'FALSE'/e;
+if (/^INSERT INTO positions.* authorized/) { # very very specific
+    s/([10])(?=\s*,\s*NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP\))/$1 ? 'TRUE' : 'FALSE'/e;
+}
+# standard date-time math would be so nice...
+s/DATEADD\s*\(day,\s*-(\d+),\s*CURRENT_TIMESTAMP\)/CURRENT_TIMESTAMP - INTERVAL '$1 DAYS'/g;
--- mssql2sqlite.sh
+++ mssql2sqlite.sh
@@ -15,13 +15,29 @@ function dtg_mac {
 
 while read input
 do
+	# First, handle special cases cheaply
 	if [[ "$input" == "SET "* ]]; then
 		echo "-- $input" # comment out any mssql-specific session-modifying commands
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		continue;
+	fi
+	if [[ "$input" == "TRUNCATE"* ]]; then
+		echo $input | sed "s/TRUNCATE TABLE/DELETE FROM/";
+		continue;
+	fi
+	# regenerate the current time down to the millisecond, so that things don't have identical timestamps
+	if [[ "$OSTYPE" == "darwin"* ]]; then
 		time=`dtg_mac`;
-		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -E "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/date('\1 00:00:00.000 -0000')/g" | sed "s/TRUNCATE TABLE/DELETE FROM/"
 	else
 		time=`dtg_linux`;
-		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -r "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/'\1 00:00:00.000 -0000'/g" | sed "s/TRUNCATE TABLE/DELETE FROM/"
+	fi
+	# Handle one more time-related special case...
+	if [[ "$input" == *"DATEADD"* ]]; then
+		input=`echo $input | sed "s/DATEADD (day, -2, CURRENT_TIMESTAMP)/DATETIME('${time}', '-2 days')/g"`
+	fi
+	# And sub in the actual time for remaining CURRENT_TIMESTAMP instances
+	if [[ "$OSTYPE" == "darwin"* ]]; then
+		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -E "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/date('\1 00:00:00.000 -0000')/g"
+	else
+		echo ${input} | sed "s/CURRENT_TIMESTAMP/'${time}'/g" | sed -r "s/'([0-9]{4}-[0-9]{2}-[0-9]{2})'/'\1 00:00:00.000 -0000'/g"
 	fi
 done
