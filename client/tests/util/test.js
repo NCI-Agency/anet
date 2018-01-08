1fbf767fd507ff79f6132ad19bb98d44f34dbd0f
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2018-01-08 15:37:39 +0100

    Switch test defaults to IE 11

--- client/tests/util/test.js
+++ client/tests/util/test.js
@@ -18,8 +18,8 @@ if (testEnv === 'local') {
     require('./keep-alive.js')
     let config = require('config')
     capabilities = {
-        browserName: 'Chrome',   // or 'IE'
-        browser_version: '61.0', // or '11.0'
+        browserName: 'IE',   // or 'Chrome'
+        browser_version: '11.0', // or '61.0'
         os: 'Windows',
         os_version: '7',
         resolution: '2048x1536',
