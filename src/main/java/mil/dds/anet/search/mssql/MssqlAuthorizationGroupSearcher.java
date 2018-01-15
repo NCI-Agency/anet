c11666cd3e1874c91898df573ea82644b41b5caf
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2018-01-15 14:08:05 +0100

    Revert search for authorization groups to pre-Fast Search
    NCI-Agency/anet#328

--- src/main/java/mil/dds/anet/search/mssql/MssqlAuthorizationGroupSearcher.java
+++ src/main/java/mil/dds/anet/search/mssql/MssqlAuthorizationGroupSearcher.java
@@ -1,97 +1,39 @@
 package mil.dds.anet.search.mssql;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-
-import jersey.repackaged.com.google.common.base.Joiner;
 
 import org.skife.jdbi.v2.Handle;
 import org.skife.jdbi.v2.Query;
 
 import mil.dds.anet.beans.AuthorizationGroup;
 import mil.dds.anet.beans.lists.AbstractAnetBeanList.AuthorizationGroupList;
-import mil.dds.anet.beans.search.ISearchQuery.SortOrder;
 import mil.dds.anet.beans.search.AuthorizationGroupSearchQuery;
-import mil.dds.anet.beans.search.AuthorizationGroupSearchQuery.AuthorizationGroupSearchSortBy;
 import mil.dds.anet.database.mappers.AuthorizationGroupMapper;
 import mil.dds.anet.search.IAuthorizationGroupSearcher;
-import mil.dds.anet.utils.DaoUtils;
 import mil.dds.anet.utils.Utils;
 
 public class MssqlAuthorizationGroupSearcher implements IAuthorizationGroupSearcher {
 
 	@Override
 	public AuthorizationGroupList runSearch(AuthorizationGroupSearchQuery query, Handle dbHandle) {
-		final List<String> whereClauses = new LinkedList<String>();
-		final Map<String,Object> sqlArgs = new HashMap<String,Object>();
-		final StringBuilder sql = new StringBuilder("/* MssqlAuthorizationGroupSearch */ SELECT authorizationGroups.*");
-
-		final String text = query.getText();
-		final boolean doFullTextSearch = (text != null && !text.trim().isEmpty());
-		if (doFullTextSearch) {
-			// If we're doing a full-text search, add a pseudo-rank (the sum of all search ranks)
-			// so we can sort on it (show the most relevant hits at the top).
-			// Note that summing up independent ranks is not ideal, but it's the best we can do now.
-			// See https://docs.microsoft.com/en-us/sql/relational-databases/search/limit-search-results-with-rank
-			sql.append(", ISNULL(c_authorizationGroups.rank, 0) + ISNULL(f_authorizationGroups.rank, 0)");
-			sql.append(" AS search_rank");
-		}
-		sql.append(", count(*) over() as totalCount FROM authorizationGroups");
-
-		if (doFullTextSearch) {
-			sql.append(" LEFT JOIN CONTAINSTABLE (authorizationGroups, (name, description), :containsQuery) c_authorizationGroups"
-					+ " ON authorizationGroups.id = c_authorizationGroups.[Key]"
-					+ " LEFT JOIN FREETEXTTABLE(authorizationGroups, (name, description), :freetextQuery) f_authorizationGroups"
-					+ " ON authorizationGroups.id = f_authorizationGroups.[Key]");
-			whereClauses.add("c_authorizationGroups.rank IS NOT NULL");
-			sqlArgs.put("containsQuery", Utils.getSqlServerFullTextQuery(text));
-			sqlArgs.put("freetextQuery", text);
-		}
-
-		if (query.getStatus() != null) {
-			whereClauses.add("authorizationGroups.status = :status");
-			sqlArgs.put("status", DaoUtils.getEnumId(query.getStatus()));
-		}
-
-		final AuthorizationGroupList result = new AuthorizationGroupList();
+		AuthorizationGroupList result = new AuthorizationGroupList();
 		result.setPageNum(query.getPageNum());
 		result.setPageSize(query.getPageSize());
-
-		if (whereClauses.isEmpty()) {
+		if (query.getText() == null || query.getText().trim().length() == 0) {
 			return result;
 		}
 
-		sql.append(" WHERE ");
-		sql.append(Joiner.on(" AND ").join(whereClauses));
+		final Map<String,Object> args = new HashMap<String,Object>();
+		final StringBuilder sql = new StringBuilder(
+				"/* MssqlLocationSearch */ SELECT *, count(*) over() as totalCount "
+						+ "FROM authorizationGroups WHERE CONTAINS (name, :name) "
+						+ "ORDER BY name ASC, id ASC");
+		args.put("name", Utils.getSqlServerFullTextQuery(query.getText()));
 
-		//Sort Ordering
-		final List<String> orderByClauses = new LinkedList<>();
-		if (doFullTextSearch && query.getSortBy() == null) {
-			// We're doing a full-text search without an explicit sort order,
-			// so sort first on the search pseudo-rank.
-			orderByClauses.addAll(Utils.addOrderBy(SortOrder.DESC, null, "search_rank"));
-		}
-
-		if (query.getSortBy() == null) { query.setSortBy(AuthorizationGroupSearchSortBy.NAME); }
-		if (query.getSortOrder() == null) { query.setSortOrder(SortOrder.ASC); }
-		switch (query.getSortBy()) {
-			case CREATED_AT:
-				orderByClauses.addAll(Utils.addOrderBy(query.getSortOrder(), "authorizationGroups", "createdAt"));
-				break;
-			case NAME:
-			default:
-				orderByClauses.addAll(Utils.addOrderBy(query.getSortOrder(), "authorizationGroups", "name"));
-				break;
-		}
-		orderByClauses.addAll(Utils.addOrderBy(SortOrder.ASC, "authorizationGroups", "id"));
-		sql.append(" ORDER BY ");
-		sql.append(Joiner.on(", ").join(orderByClauses));
-
-		final Query<AuthorizationGroup> sqlQuery = MssqlSearcher.addPagination(query, dbHandle, sql, sqlArgs)
+		final Query<AuthorizationGroup> map = MssqlSearcher.addPagination(query, dbHandle, sql, args)
 			.map(new AuthorizationGroupMapper());
-		return AuthorizationGroupList.fromQuery(sqlQuery, query.getPageNum(), query.getPageSize());
+		return AuthorizationGroupList.fromQuery(map, query.getPageNum(), query.getPageSize());
 	}
 
 }
