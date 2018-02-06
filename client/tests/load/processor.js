ab27c4bc6e719e70043458c097a6cc1d461824b6
vassil <vassil.iordanov@gmail.com>
2018-02-06 02:48:39 +0100

    Make load tester more realistic
    Implements NCI-Agency/anet#379

--- client/tests/load/processor.js
+++ client/tests/load/processor.js
@@ -1,7 +1,17 @@
   module.exports = {
-    continueIfResult: continueIfResult
+    continueIfResult: continueIfResult,
+    generateRandomData: generateRandomData
   }
 
+  const Faker = require('faker');
+
+  function generateRandomData(requestParams, context, ee, next) {
+    context.vars.report = {}
+    context.vars.report.intent = Faker.lorem.paragraph()
+    context.vars.report.engagementDate = Faker.date.recent()
+
+    return next();
+  }
 
   function continueIfResult(requestParams, response, context, ee, next) {
     if (context.vars.result != null)
