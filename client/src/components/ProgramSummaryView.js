035db7500c3897957e3f6a8dd9e917b66937d7bd
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2018-03-14 19:07:35 +0100

    Add aggregations
    nci-agency/anet#477

--- client/src/components/ProgramSummaryView.js
+++ client/src/components/ProgramSummaryView.js
@@ -5,15 +5,22 @@ import ReactTable from "react-table"
 import "react-table/react-table.css"
 import Settings from 'Settings'
 import LinkTo from 'components/LinkTo'
+import moment from 'moment'
 
 export default class ProgramSummaryView extends React.Component {
     constructor() {
-        super();
+        super()
         this.state = {
             data: []
         }
     }
 
+    static STATUS = {
+		COMPLETED: 'completed',
+		ONGOING: 'ongoing',
+		OVERDUE: 'overdue'
+    }
+    
     findParents = task => {
         const parents = []
         const matchTask = element => {
@@ -27,9 +34,49 @@ export default class ProgramSummaryView extends React.Component {
 
     render() {
         const {data} = this.state
-        const phaseAccessors = _.transform(Settings.fields.task.customFieldEnum2.enum, (result,val,key) => {result.push( {   id : "_phase" + key,  Header: val,
+        const now = new Date()
+        const phaseAccessors = _.transform(Settings.fields.task.customFieldEnum2.enum, (result,val,key) => {
+            result.push( {   id : "_phase" + key,  Header: val,
                 accessor: task => {
-                    return task.customFieldEnum2 === key
+                    if (task.customFieldEnum2 !== key)
+                        return null
+                    if (task.projectedCompletion)
+                        return ProgramSummaryView.STATUS.COMPLETED
+                    else if (task.plannedCompletion < now)
+                        return ProgramSummaryView.STATUS.OVERDUE
+                    else 
+                        return ProgramSummaryView.STATUS.ONGOING
+                },
+                aggregate: (values, rows) => values.reduce((accumulator, currentValue) => 
+                {
+                    if (typeof currentValue === 'string') 
+                    {    
+                        ++ accumulator[currentValue]
+                        return accumulator
+                    }
+                    else if (currentValue) 
+                    {
+                    accumulator[ProgramSummaryView.STATUS.COMPLETED] = currentValue[ProgramSummaryView.STATUS.COMPLETED] + accumulator[ProgramSummaryView.STATUS.COMPLETED]
+                    accumulator[ProgramSummaryView.STATUS.OVERDUE] = currentValue[ProgramSummaryView.STATUS.OVERDUE] + accumulator[ProgramSummaryView.STATUS.OVERDUE]
+                    accumulator[ProgramSummaryView.STATUS.ONGOING] = currentValue[ProgramSummaryView.STATUS.ONGOING] + accumulator[ProgramSummaryView.STATUS.ONGOING]
+                    }
+                    return accumulator
+                }, {completed:0, ongoing:0, overdue:0}),
+                Cell: row => {
+                    if (!row.value)
+                        return null
+                    if (typeof row.value === 'string')
+                        return row.value
+                    const number = row.value.overdue > 0 ? row.value.overdue : row.value.ongoing
+                    const all = row.value.ongoing+row.value.overdue+row.value.completed
+                    const rest = all - number
+                    return <div style={{background: 'lightGray'}}>                
+                        <div style={{width: ''+100*number/all+'%', float:'left', 'textAlign':'right', background: row.value.overdue > 0 ? 'red' : 'green'}}>
+                            {number > 0 ? number : ""}
+                        </div>
+                        {number === 0 ? 0 : ""}
+                        {rest>0 && "/"+ rest }
+                    </div>
                 }
             })},[])
 
@@ -40,7 +87,7 @@ export default class ProgramSummaryView extends React.Component {
                         columns={_.union([
                         {
                             id: "EF",
-                            Header: "EF",
+                            Header: "EF2",
                             accessor: task => {
                                 const parents = this.findParents(task)
                                 return parents.length > 0 && (parents[0].shortName + " " + parents[0].longName)
@@ -59,15 +106,22 @@ export default class ProgramSummaryView extends React.Component {
                             Aggregated: row => null,
                             Cell: row => row.original && (<LinkTo task={row.original} key={row.original.id}>{row.original.shortName + " " + row.original.longName}</LinkTo>)
                         }
-                    ], phaseAccessors)}
+                    ], phaseAccessors, [
+                        {
+                            id: "plannedCompletion",
+                            Header: "Planned completion",
+                            accessor: "plannedCompletion",
+                            aggregate: (values, rows) => _.max(values),
+                            Cell: row => row.row.plannedCompletion && (<div style={{color: row.row.plannedCompletion < now && !row.row.projectedCompletion ? 'red' : 'black'}}> { moment(row.row.plannedCompletion).format('DD MMM YYYY')} </div>)
+                        }])}
                         defaultPageSize={25}/>
-        );
+        )
     }
 
     fetchData() {
         const chartQuery = API.query(/* GraphQL */
         `taskList (f:getAll pageSize:10000) { 
-            totalCount, list { id, shortName, longName, customFieldEnum2, parentTask { id }, }
+            totalCount, list { id, shortName, longName, customFieldEnum2, plannedCompletion, projectedCompletion, parentTask { id }, }
         }`)
 
         Promise
