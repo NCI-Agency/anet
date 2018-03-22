de50711269fc5c4e7d5d4ff038d5453ac884e2d5
Mara Dragan <mara@west.nl>
2018-03-22 15:37:32 +0100

    NCI-Agency/anet#375: Remove navigation warning functionality for now
    It gives an error with React 16, to be fixed later.

--- client/src/components/NavigationWarning.js
+++ client/src/components/NavigationWarning.js
@@ -7,39 +7,41 @@ import {withRouter} from 'react-router'
 
 class NavigationWarning extends Component {
 
-	formHasUnsavedChanges() {
-		return !_isEqual(this.props.current, this.props.original)
-	}
-
-	@autobind
-	onBeforeUnloadListener(event) {
-		if (this.formHasUnsavedChanges()) {
-			event.returnValue = 'Are you sure you wish to navigate away from the page? You will lose unsaved changes.'
-			event.preventDefault()
-		}
-	}
-
-	@autobind
-	routeLeaveHook(nextRoute) {
-        const skipPageLeaveWarning = _get(nextRoute, ['state', 'skipPageLeaveWarning'])
-		if (this.formHasUnsavedChanges() && !skipPageLeaveWarning) {
-			return 'Are you sure you wish to navigate away from the page? You will lose unsaved changes.'
-		}
-        if (skipPageLeaveWarning) {
-            nextRoute.state.skipPageLeaveWarning = false
-        }
-	}
-
-	componentWillMount() {
-		this.unsetRouteLeaveHook = 
-			this.props.router.setRouteLeaveHook(_last(this.props.routes), this.routeLeaveHook)
-		window.addEventListener('beforeunload', this.onBeforeUnloadListener)
-	}
-
-	componentWillUnmount() {
-		this.unsetRouteLeaveHook()
-		window.removeEventListener('beforeunload', this.onBeforeUnloadListener)
-	}
+// FIXME React16
+// FIXME idea: https://reacttraining.com/react-router/web/example/preventing-transitions
+//	formHasUnsavedChanges() {
+//		return !_isEqual(this.props.current, this.props.original)
+//	}
+//
+//	@autobind
+//	onBeforeUnloadListener(event) {
+//		if (this.formHasUnsavedChanges()) {
+//			event.returnValue = 'Are you sure you wish to navigate away from the page? You will lose unsaved changes.'
+//			event.preventDefault()
+//		}
+//	}
+//
+//	@autobind
+//	routeLeaveHook(nextRoute) {
+//        const skipPageLeaveWarning = _get(nextRoute, ['state', 'skipPageLeaveWarning'])
+//		if (this.formHasUnsavedChanges() && !skipPageLeaveWarning) {
+//			return 'Are you sure you wish to navigate away from the page? You will lose unsaved changes.'
+//		}
+//        if (skipPageLeaveWarning) {
+//            nextRoute.state.skipPageLeaveWarning = false
+//        }
+//	}
+//
+//	componentWillMount() {
+//		this.unsetRouteLeaveHook =
+//			this.props.router.setRouteLeaveHook(_last(this.props.routes), this.routeLeaveHook)
+//		window.addEventListener('beforeunload', this.onBeforeUnloadListener)
+//	}
+//
+//	componentWillUnmount() {
+//		this.unsetRouteLeaveHook()
+//		window.removeEventListener('beforeunload', this.onBeforeUnloadListener)
+//	}
 
     render() {
         return null
