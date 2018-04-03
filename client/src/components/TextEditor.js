696a4cb1fb0324982ed84b7f940b64ecb2ffe18b
Gertjan van Oosten <gertjan@west.nl>
2018-04-03 16:31:32 +0200

    NCI-Agency/anet#375: Re-add Alloy Editor
    And rewrite it such that the proper defines are there;
    rewrite is loosely based on https://github.com/liferay/alloy-editor/issues/770#issuecomment-355055340

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,49 +1,49 @@
 import React, {Component} from 'react'
-//import autobind from 'autobind-decorator'
-//
-//import AlloyEditor from 'alloyeditor/dist/alloy-editor/alloy-editor-no-react'
-//import './alloy-editor-atlas.css'
-//
-//// this just removes a number of features we don't want from the Alloy toolbar
-//let buttons = AlloyEditor.Selections[3].buttons
-//buttons.splice(4, 2)
-//buttons.splice(0, 1)
-//
-//const ALLOY_CONFIG = {
-//	toolbars: {
-//		styles: { selections: AlloyEditor.Selections },
-//		add: { buttons: ['ul', 'ol', 'hline', 'table'] },
-//	}
-//}
+import autobind from 'autobind-decorator'
+
+import AlloyEditor from 'alloyeditor/dist/alloy-editor/alloy-editor-no-react'
+import './alloy-editor-atlas.css'
+
+// this just removes a number of features we don't want from the Alloy toolbar
+let buttons = AlloyEditor.Selections[3].buttons
+buttons.splice(4, 2)
+buttons.splice(0, 1)
+
+const ALLOY_CONFIG = {
+	toolbars: {
+		styles: { selections: AlloyEditor.Selections },
+		add: { buttons: ['ul', 'ol', 'hline', 'table'] },
+	}
+}
 
 export default class TextEditor extends Component {
-//	componentDidMount() {
-//		if (!this.editor) {
-//			this.editor = AlloyEditor.editable(this.container, ALLOY_CONFIG)
-//			this.nativeEditor = this.editor.get('nativeEditor')
-//			this.nativeEditor.on('change', this.onChange)
-//		}
-//
-//		this.componentWillReceiveProps(this.props)
-//	}
-//
-//	componentWillUnmount() {
-//		this.editor && this.editor.destroy()
-//	}
-//
-//	componentWillReceiveProps(newProps) {
-//		let html = newProps.value
-//		if (html !== this.nativeEditor.getData()) {
-//			this.nativeEditor.setData(html)
-//		}
-//	}
-//
-//	@autobind
-//	onChange(event) {
-//		const html = this.nativeEditor.getData()
-//		this.props.onChange(html)
-//	}
-//
+	componentDidMount() {
+		if (!this.editor) {
+			this.editor = AlloyEditor.editable(this.container, ALLOY_CONFIG)
+			this.nativeEditor = this.editor.get('nativeEditor')
+			this.nativeEditor.on('change', this.onChange)
+		}
+
+		this.componentWillReceiveProps(this.props)
+	}
+
+	componentWillUnmount() {
+		this.editor && this.editor.destroy()
+	}
+
+	componentWillReceiveProps(newProps) {
+		let html = newProps.value
+		if (html !== this.nativeEditor.getData()) {
+			this.nativeEditor.setData(html)
+		}
+	}
+
+	@autobind
+	onChange(event) {
+		const html = this.nativeEditor.getData()
+		this.props.onChange(html)
+	}
+
 	render() {
 		return <div className="text-editor" ref={(el) => { this.container = el }} />
 	}
