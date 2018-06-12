NCI-Agency/anet

commit bc8b7b2b786cebff2a8dbf0124bf5e1d200373c6

NCI-Agency/anet#382: Fix React warnings about componentWillReceiveProps()
See https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes

client/src/components/AssignPersonModal.js
@@ -22,9 +22,12 @@ export default class AssignPersonModal extends Component {
 		}
 	}
 
-	componentWillReceiveProps(nextProps, nextContext) {
-		let person = nextProps.position.person
-		this.setState({person})
+	static getDerivedStateFromProps(props, state) {
+		const person = props.position.person
+		if (person !== state.person) {
+			return {person: person}
+		}
+		return null
 	}
 
 	render() {

client/src/components/AssignPositionModal.js
@@ -26,9 +26,12 @@ export default class AssignPositionModal extends Component {
 		}
 	}
 
-	componentWillReceiveProps(nextProps, nextContext) {
-		let position = nextProps.person.position
-		this.setState({position})
+	static getDerivedStateFromProps(props, state) {
+		const position = props.person.position
+		if (position !== state.position) {
+			return {position: position}
+		}
+		return null
 	}
 
 	render() {

client/src/components/AdvisorReports/OrganizationAdvisorsTable.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { Table } from 'react-bootstrap'
+import _isEqual from 'lodash/isEqual'
 import AdvisorReportsModal from 'components/AdvisorReports/AdvisorReportsModal'
 import AdvisorReportsRow from 'components/AdvisorReports/AdvisorReportsRow'
 import AdvisorReportsTableHead from 'components/AdvisorReports/AdvisorReportsTableHead'
@@ -17,8 +18,11 @@ class OrganizationAdvisorsTable extends Component {
         this.handleSelectAllRows = this.handleSelectAllRows.bind(this)
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.setState({ data: nextProps.data })
+    static getDerivedStateFromProps(props, state) {
+        if (!_isEqual(props.data, state.data)) {
+            return { data: props.data }
+        }
+        return null
     }
 
     handleSelectRow(index) {

client/src/components/Autocomplete.js
@@ -51,7 +51,7 @@ export default class Autocomplete extends Component {
 
 		this.fetchSuggestionsDebounced = _debounce(this.fetchSuggestions, 200)
 
-		let value = this.componentWillReceiveProps(props)
+		let value = this._getValue(props)
 		let stringValue = this.getStringValue(value)
 
 		this.state = {
@@ -63,20 +63,24 @@ export default class Autocomplete extends Component {
 		}
 	}
 
-	componentWillReceiveProps(props) {
-		let value = props.value
+	@autobind
+	_getValue(props) {
+		const {value} = props
 		if (Array.isArray(value)) {
 			this.selectedIds = value.map(object => object.id)
 			return {}
 		}
 
+		return value
+	}
+
+	componentDidUpdate(prevProps, prevState) {
 		//Ensure that we update the stringValue if we get an updated value
-		if (this.state) {
-			let stringValue = this.getStringValue(value)
+		const value = this._getValue(this.props)
+		const stringValue = this.getStringValue(value)
+		if (stringValue !== this.state.originalStringValue) {
 			this.setState({stringValue, originalStringValue: stringValue})
 		}
-
-		return value
 	}
 
 	render() {

client/src/components/CancelledEngagementReports.js
@@ -270,23 +270,17 @@ export default class CancelledEngagementReports extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps, nextContext) {
-    if (nextProps.date.valueOf() !== this.props.date.valueOf()) {
-      this.setState({
-        reportsPageNum: 0,
-        focusedReason: '',
-        focusedOrg: ''
-      })  // reset focus when changing the date
-    }
-  }
-
   componentDidMount() {
     this.fetchData()
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.date.valueOf() !== this.props.date.valueOf()) {
-      this.fetchData()
+      this.setState({
+        reportsPageNum: 0,
+        focusedReason: '',  // reset focus when changing the date
+        focusedOrg: ''
+      }, () => this.fetchData())
     }
   }
 }

client/src/components/EditAssociatedPositionsModal.js
@@ -31,9 +31,12 @@ export default class EditAssociatedPositionsModal extends Component {
 		}
 	}
 
-	componentWillReceiveProps(nextProps, nextContext) {
-		let assoc = nextProps.position.associatedPositions.slice()
-		this.setState({error: null, associatedPositions: assoc})
+	static getDerivedStateFromProps(props, state) {
+		const assoc = props.position.associatedPositions.slice()
+		if (assoc !== state.associatedPositions) {
+			return {error: null, associatedPositions: assoc}
+		}
+		return null
 	}
 
 	render() {

client/src/components/FormField.js
@@ -246,13 +246,17 @@ export default class FormField extends Component {
 		return props.value || this.getValue(props, context) || ''
 	}
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.validateBeforeUserTouches) {
-			this.setState({userHasTouchedField: true})
+	static getDerivedStateFromProps(props, state) {
+		if (props.validateBeforeUserTouches !== undefined
+				&& props.validateBeforeUserTouches !== state.userHasTouchedField) {
+			return {userHasTouchedField: props.validateBeforeUserTouches}
 		}
+		return null
+	}
 
-		if (nextProps.required !== this.props.required) {
-			this.updateValidationState(nextProps)
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.required !== this.props.required) {
+			this.updateValidationState(this.props)
 		}
 	}
 

client/src/components/FutureEngagementsByLocation.js
@@ -254,19 +254,13 @@ export default class FutureEngagementsByLocation extends Component {
     this.fetchData()
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.datePropsChanged(nextProps)) {
-      this.setState({
-        reportsPageNum: 0,
-        focusedDate: '',
-        focusedLocation: ''
-      })
-    }
-  }
-
   componentDidUpdate(prevProps, prevState) {
     if (this.datePropsChanged(prevProps)) {
-      this.fetchData()
+      this.setState({
+        reportsPageNum: 0,
+        focusedDate: '',  // reset focus when changing the date
+        focusedLocation: ''
+      }, () => this.fetchData())
     }
   }
 

commit eace26555a4af009dfb1d13058da18042a17658a

NCI-Agency/anet#382: Fix bug in getDerivedStateFromProps
Bug was introduced in commit bc8b7b2.

client/src/components/AssignPersonModal.js
@@ -22,12 +22,10 @@ export default class AssignPersonModal extends Component {
 		}
 	}
 
-	static getDerivedStateFromProps(props, state) {
-		const person = props.position.person
-		if (person !== state.person) {
-			return {person: person}
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.position.person !== this.props.position.person) {
+			this.setState({person: this.props.position.person})
 		}
-		return null
 	}
 
 	render() {

client/src/components/AssignPositionModal.js
@@ -26,12 +26,10 @@ export default class AssignPositionModal extends Component {
 		}
 	}
 
-	static getDerivedStateFromProps(props, state) {
-		const position = props.person.position
-		if (position !== state.position) {
-			return {position: position}
+	componentDidUpdate(prevProps, prevState) {
+		if (prevProps.person.position !== this.props.person.position) {
+			this.setState({position: this.props.person.position})
 		}
-		return null
 	}
 
 	render() {