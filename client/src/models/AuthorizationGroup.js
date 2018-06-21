be61810b07d150a3c9d74168d1d3a2e63cce12e3
Gertjan van Oosten <gertjan@west.nl>
2018-06-21 13:50:36 +0200

    Remove unused import

--- client/src/models/AuthorizationGroup.js
+++ client/src/models/AuthorizationGroup.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import encodeQuery from 'querystring/encode'
 import utils from 'utils'
 
--- client/src/models/Task.js
+++ client/src/models/Task.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types'
 import React from 'react'
 
 import Model from 'components/Model'
