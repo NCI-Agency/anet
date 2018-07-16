NCI-Agency/anet

commit aac0d361d34724e9b4d743ae565403a80fb266d0

NCI-Agency/anet#375: Fix 'leave page' warning when there are unsaved changes

client/src/pages/admin/authorizationgroup/Form.js
@@ -14,6 +14,7 @@ import API from 'api'
 import {AuthorizationGroup} from 'models'
 
 import { withRouter } from 'react-router-dom'
+import NavigationWarning from 'components/NavigationWarning'
 
 class AuthorizationGroupForm extends ValidatableFormWrapper {
 	static propTypes = {
@@ -25,6 +26,7 @@ class AuthorizationGroupForm extends ValidatableFormWrapper {
 		super(props)
 
 		this.state = {
+			isBlocking: false,
 			errors: {},
 		}
 	}
@@ -37,6 +39,8 @@ class AuthorizationGroupForm extends ValidatableFormWrapper {
 		const {ValidatableForm, RequiredField} = this
 		return (
 			<div>
+				<NavigationWarning isBlocking={this.state.isBlocking} />
+
 				<Messages success={this.state.success} error={this.state.error} />
 
 				<ValidatableForm formFor={authorizationGroup} onChange={this.onChange} onSubmit={this.onSubmit} horizontal submitText="Save authorization group">
@@ -78,6 +82,9 @@ class AuthorizationGroupForm extends ValidatableFormWrapper {
 
 	@autobind
 	onChange() {
+		this.setState({
+			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+		})
 		this.forceUpdate()
 	}
 

client/src/pages/admin/authorizationgroup/New.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import NavigationWarning from 'components/NavigationWarning'
 import Breadcrumbs from 'components/Breadcrumbs'
 import Messages from 'components/Messages'
 import ValidatableFormWrapper from 'components/ValidatableFormWrapper'
@@ -26,11 +25,10 @@ export default class AuthorizationGroupNew extends ValidatableFormWrapper {
 
 		return (
 			<div>
-				<NavigationWarning original={new AuthorizationGroup()} current={authorizationGroup} />
 				<Breadcrumbs items={[['Create new authorization group', AuthorizationGroup.pathForNew()]]} />
 				<Messages success={this.state.success} error={this.state.error} />
 
-				<AuthorizationGroupForm authorizationGroup={authorizationGroup} />
+				<AuthorizationGroupForm original={new AuthorizationGroup()} authorizationGroup={authorizationGroup} />
 			</div>
 		)
 	}

client/src/pages/locations/Edit.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Page from 'components/Page'
 
 import Messages from 'components/Messages'
-import NavigationWarning from 'components/NavigationWarning'
 
 import LocationForm from './Form'
 import {Location} from 'models'
@@ -40,8 +39,7 @@ export default class LocationEdit extends Page {
 			<div>
 				<Messages error={this.state.error} success={this.state.success} />
 
-				<NavigationWarning original={this.state.originalLocation} current={location} />
-				<LocationForm anetLocation={location} edit />
+				<LocationForm original={this.state.originalLocation} anetLocation={location} edit />
 			</div>
 		)
 	}

client/src/pages/locations/Form.js
@@ -4,7 +4,6 @@ import {Button} from 'react-bootstrap'
 import autobind from 'autobind-decorator'
 
 import Fieldset from 'components/Fieldset'
-import NavigationWarning from 'components/NavigationWarning'
 import Form from 'components/Form'
 import ButtonToggleGroup from 'components/ButtonToggleGroup'
 import Messages from 'components/Messages'
@@ -15,6 +14,7 @@ import API from 'api'
 import {Location} from 'models'
 
 import { withRouter } from 'react-router-dom'
+import NavigationWarning from 'components/NavigationWarning'
 
 class LocationForm extends ValidatableFormWrapper {
 	static propTypes = {
@@ -26,6 +26,7 @@ class LocationForm extends ValidatableFormWrapper {
 		super(props)
 
 		this.state = {
+			isBlocking: false,
 			markers: [{id: 0, draggable: true, onMove: this.onMarkerMove}]
 		}
 	}
@@ -55,7 +56,7 @@ class LocationForm extends ValidatableFormWrapper {
 
 		return (
 			<div>
-				<NavigationWarning original={new Location()} current={location} />
+				<NavigationWarning isBlocking={this.state.isBlocking} />
 
 				<Messages success={this.state.success} error={this.state.error} />
 
@@ -96,6 +97,9 @@ class LocationForm extends ValidatableFormWrapper {
 
 	@autobind
 	onChange() {
+		this.setState({
+			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+		})
 		this.forceUpdate()
 	}
 

client/src/pages/locations/New.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import NavigationWarning from 'components/NavigationWarning'
 import Breadcrumbs from 'components/Breadcrumbs'
 import Messages from 'components/Messages'
 import ValidatableFormWrapper from 'components/ValidatableFormWrapper'
@@ -26,12 +25,10 @@ export default class LocationNew extends ValidatableFormWrapper {
 
 		return (
 			<div>
-				<NavigationWarning original={new Location()} current={location} />
-
 				<Breadcrumbs items={[['Create new Location', Location.pathForNew()]]} />
 				<Messages success={this.state.success} error={this.state.error} />
 
-				<LocationForm anetLocation={location} />
+				<LocationForm original={new Location()} anetLocation={location} />
 			</div>
 		)
 	}

client/src/pages/organizations/Edit.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Page from 'components/Page'
-import NavigationWarning from 'components/NavigationWarning'
 
 import OrganizationForm from './Form'
 import Breadcrumbs from 'components/Breadcrumbs'
@@ -47,12 +46,10 @@ export default class OrganizationEdit extends Page {
 
 		return (
 			<div>
-				<NavigationWarning original={this.state.originalOrganization} current={organization} />
-
 				<Breadcrumbs items={[[`Edit ${organization.shortName}`, Organization.pathForEdit(organization)]]} />
 				<Messages error={this.state.error} success={this.state.success} />
 
-				<OrganizationForm organization={organization} edit />
+				<OrganizationForm original={this.state.originalOrganization} organization={organization} edit />
 			</div>
 		)
 	}

client/src/pages/organizations/Form.js
@@ -21,6 +21,7 @@ import DictionaryField from '../../HOC/DictionaryField'
 import REMOVE_ICON from 'resources/delete.png'
 
 import { withRouter } from 'react-router-dom'
+import NavigationWarning from 'components/NavigationWarning'
 
 class OrganizationForm extends ValidatableFormWrapper {
 	static propTypes = {
@@ -35,6 +36,7 @@ class OrganizationForm extends ValidatableFormWrapper {
 	constructor(props) {
 		super(props)
 		this.state = {
+			isBlocking: false,
 			error: null,
 		}
 		this.IdentificationCodeFieldWithLabel = DictionaryField(Form.Field)
@@ -51,7 +53,10 @@ class OrganizationForm extends ValidatableFormWrapper {
 
 		const orgSettings = isPrincipalOrg ? Settings.fields.principal.org : Settings.fields.advisor.org
 
-		return <ValidatableForm formFor={organization}
+		return <div>
+			<NavigationWarning isBlocking={this.state.isBlocking} />
+
+			<ValidatableForm formFor={organization}
 			onChange={this.onChange}
 			onSubmit={this.onSubmit}
 			submitText="Save organization"
@@ -104,6 +109,7 @@ class OrganizationForm extends ValidatableFormWrapper {
 				}
 			</div>}
 		</ValidatableForm>
+		</div>
 	}
 
 	renderApprovalStep(step, index) {
@@ -214,6 +220,9 @@ class OrganizationForm extends ValidatableFormWrapper {
 
 	@autobind
 	onChange() {
+		this.setState({
+			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+		})
 		this.forceUpdate()
 	}
 

client/src/pages/people/Edit.js
@@ -5,7 +5,6 @@ import moment from 'moment'
 
 import PersonForm from './Form'
 import Breadcrumbs from 'components/Breadcrumbs'
-import NavigationWarning from 'components/NavigationWarning'
 
 import API from 'api'
 import {Person} from 'models'
@@ -62,8 +61,7 @@ export default class PersonEdit extends Page {
 					<Breadcrumbs items={[[`Edit ${person.name}`, Person.pathForEdit(person)]]} />
 				}
 
-				<NavigationWarning original={originalPerson} current={person} />
-				<PersonForm person={person} edit showPositionAssignment={canEditPosition}
+				<PersonForm original={originalPerson} person={person} edit showPositionAssignment={canEditPosition}
 					legendText={legendText} saveText={saveText} />
 			</div>
 		)

commit 61025d0a656d51b3f327e9a83b8eabadd0e7e9ed

Show navigation warning only when object has been edited
Compare to the correct original object (and make it a required prop).
Fixes bugs introduced in commit aac0d361

client/src/pages/admin/authorizationgroup/Form.js
@@ -19,6 +19,7 @@ import NavigationWarning from 'components/NavigationWarning'
 class AuthorizationGroupForm extends ValidatableFormWrapper {
 	static propTypes = {
 		authorizationGroup: PropTypes.object.isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool
 	}
 
@@ -84,7 +85,7 @@ class AuthorizationGroupForm extends ValidatableFormWrapper {
 	@autobind
 	onChange() {
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.authorizationGroup, this.props.original),
 		})
 	}
 

client/src/pages/admin/authorizationgroup/New.js
@@ -19,6 +19,7 @@ class AuthorizationGroupNew extends Page {
 
 		this.state = {
 			authorizationGroup: new AuthorizationGroup(),
+			originalAuthorizationGroup : new AuthorizationGroup()
 		}
 	}
 
@@ -30,7 +31,7 @@ class AuthorizationGroupNew extends Page {
 				<Breadcrumbs items={[['Create new authorization group', AuthorizationGroup.pathForNew()]]} />
 				<Messages success={this.state.success} error={this.state.error} />
 
-				<AuthorizationGroupForm original={new AuthorizationGroup()} authorizationGroup={authorizationGroup} />
+				<AuthorizationGroupForm original={this.state.originalAuthorizationGroup} authorizationGroup={authorizationGroup} />
 			</div>
 		)
 	}

client/src/pages/locations/Edit.js
@@ -21,8 +21,8 @@ class LocationEdit extends Page {
 		super(props, PAGE_PROPS_NO_NAV)
 
 		this.state = {
-			location: {},
-			originalLocation : {}
+			location: new Location(),
+			originalLocation : new Location()
 		}
 	}
 

client/src/pages/locations/Form.js
@@ -19,6 +19,7 @@ import NavigationWarning from 'components/NavigationWarning'
 class LocationForm extends ValidatableFormWrapper {
 	static propTypes = {
 		anetLocation: PropTypes.object.isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 	}
 
@@ -97,7 +98,7 @@ class LocationForm extends ValidatableFormWrapper {
 	@autobind
 	onChange() {
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.anetLocation, this.props.original),
 		})
 	}
 

client/src/pages/locations/New.js
@@ -21,6 +21,7 @@ class LocationNew extends Page {
 
 		this.state = {
 			location: new Location(),
+			originalLocation : new Location()
 		}
 	}
 
@@ -32,7 +33,7 @@ class LocationNew extends Page {
 				<Breadcrumbs items={[['Create new Location', Location.pathForNew()]]} />
 				<Messages success={this.state.success} error={this.state.error} />
 
-				<LocationForm original={new Location()} anetLocation={location} />
+				<LocationForm original={this.state.originalLocation} anetLocation={location} />
 			</div>
 		)
 	}

client/src/pages/organizations/Edit.js
@@ -23,6 +23,7 @@ class OrganizationEdit extends Page {
 		super(props, PAGE_PROPS_NO_NAV)
 
 		this.state = {
+			originalOrganization: new Organization(),
 			organization: new Organization(),
 		}
 	}

client/src/pages/organizations/Form.js
@@ -27,6 +27,7 @@ import NavigationWarning from 'components/NavigationWarning'
 class BaseOrganizationForm extends ValidatableFormWrapper {
 	static propTypes = {
 		organization: PropTypes.object,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 		currentUser: PropTypes.instanceOf(Person),
 	}
@@ -239,7 +240,7 @@ class BaseOrganizationForm extends ValidatableFormWrapper {
 	@autobind
 	onChange() {
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.organization, this.props.original),
 		})
 	}
 

client/src/pages/people/Edit.js
@@ -26,6 +26,7 @@ class BasePersonEdit extends Page {
 		super(props, PAGE_PROPS_NO_NAV)
 
 		this.state = {
+			originalPerson: new Person(),
 			person: new Person(),
 		}
 	}

client/src/pages/people/Form.js
@@ -30,6 +30,7 @@ import NavigationWarning from 'components/NavigationWarning'
 class BasePersonForm extends ValidatableFormWrapper {
 	static propTypes = {
 		person: PropTypes.object.isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 		legendText: PropTypes.string,
 		saveText: PropTypes.string,
@@ -342,8 +343,9 @@ class BasePersonForm extends ValidatableFormWrapper {
 
 	@autobind
 	onChange() {
+		const person = Object.without(this.props.person, 'firstName', 'lastName')
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(person, this.props.original),
 		})
 	}
 

client/src/pages/positions/Form.js
@@ -22,6 +22,7 @@ import LinkTo from 'components/LinkTo'
 class BasePositionForm extends ValidatableFormWrapper {
 	static propTypes = {
 		position: PropTypes.object.isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 		error: PropTypes.object,
 		success: PropTypes.object,
@@ -149,7 +150,7 @@ class BasePositionForm extends ValidatableFormWrapper {
 	@autobind
 	onChange() {
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.position, this.props.original),
 		})
 	}
 

client/src/pages/reports/Form.js
@@ -38,6 +38,7 @@ import 'components/reactToastify.css'
 class BaseReportForm extends ValidatableFormWrapper {
 	static propTypes = {
 		report: PropTypes.instanceOf(Report).isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 		onDelete: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
 		currentUser: PropTypes.instanceOf(Person),
@@ -521,7 +522,7 @@ class BaseReportForm extends ValidatableFormWrapper {
 		this.setState({
 			errors : this.validateReport(),
 			reportChanged: true,
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.report, this.props.original),
 		})
 	}
 

client/src/pages/tasks/Form.js
@@ -37,6 +37,7 @@ const customEnumButtons = (list) => {
 class BaseTaskForm extends ValidatableFormWrapper {
 	static propTypes = {
 		task: PropTypes.object.isRequired,
+		original: PropTypes.object.isRequired,
 		edit: PropTypes.bool,
 		currentUser: PropTypes.instanceOf(Person),
 	}
@@ -156,7 +157,7 @@ class BaseTaskForm extends ValidatableFormWrapper {
 	@autobind
 	onChange() {
 		this.setState({
-			isBlocking: this.formHasUnsavedChanges(this.state.report, this.props.original),
+			isBlocking: this.formHasUnsavedChanges(this.props.task, this.props.original),
 		})
 	}
 