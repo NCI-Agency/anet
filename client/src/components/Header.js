a7be59f194cd1b32b0443d94815cccaa6f90f099
Edwin van den Houdt <edwin@vdhoudt.nl>
2018-09-04 16:29:12 +0200

    GH-511 Add specific breakpoints to improve mobile UX

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import {Grid, Row, Col} from 'react-bootstrap'
+import {Grid, Row, Col, Image} from 'react-bootstrap'
 
 import SearchBar from 'components/SearchBar'
 import CreateButton from 'components/CreateButton'
@@ -22,29 +22,32 @@ export default class Header extends Component {
 			<header style={backgroundCss} className="header">
 				<Grid fluid>
 					<Row>
-						<Col xs={3}>
+						<Col mdHidden lgHidden xs={2} sm={1}>
+							{this.props.sidebarButton}
+						</Col>
+						<Col xs={6} sm={2} md={3} lg={2}>
 							{
 								this.props.minimalHeader ?
 									<span className="logo"><img src={logo} alt="ANET Logo" /></span> :
 									<Link to="/" className="logo">
-										<img src={logo} alt="ANET logo" />
+										<Image src={logo} alt="ANET logo" className="img-responsive" />
 									</Link>
 							}
 						</Col>
-
 						{ !this.props.minimalHeader &&
-							<Col xs={7} className="middle-header">
-								<SearchBar />
-							</Col>
-						}
-
-						{ !this.props.minimalHeader &&
-							<Col xs={2}>
-								<div className="pull-right">
+							<Col xs={4} smPush={7} sm={2} mdPush={6} md={3} lgPush={8} lg={2}>
+								<div className="pull-right create-button">
 									<CreateButton />
 								</div>
 							</Col>
 						}
+						{ !this.props.minimalHeader &&
+							<Col xs={12} smPull={2} sm={7} mdPull={3} md={6} lgPull={2} lg={8}>
+								<SearchBar />
+							</Col>
+						}
+
+
 					</Row>
 				</Grid>
 			</header>
