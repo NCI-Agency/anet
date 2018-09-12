f98a544cb7b3ee8263750b93fb2da964959f3ca9
Edwin van den Houdt <edwin@vdhoudt.nl>
2018-09-12 15:27:59 +0200

    GH-511 Fix floating menu and refactor routing
    Floating menu aligned under the topbar
    Routing refactored to separate file

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,69 +1,22 @@
-import PropTypes from 'prop-types'
 import React from 'react'
-import Page, {mapDispatchToProps, propTypes as pagePropTypes} from 'components/Page'
-import {Grid, Row, Col} from 'react-bootstrap'
+import PropTypes from 'prop-types'
+import Page, { mapDispatchToProps, propTypes as pagePropTypes } from 'components/Page'
 import autobind from 'autobind-decorator'
 
 import LoadingBar from 'react-redux-loading-bar'
 import TopBar from 'components/TopBar'
 import Nav from 'components/Nav'
+import { Element } from 'react-scroll'
 
 import API from 'api'
-import {Person, Organization} from 'models'
-
-import {Route, Switch, Redirect} from 'react-router'
-import Home from 'pages/Home'
-import Search from 'pages/Search'
-import RollupShow from 'pages/rollup/Show'
-import GraphiQL from 'pages/GraphiQL'
-import Help from 'pages/Help'
-import PageMissing from 'pages/PageMissing'
-
-import ReportNew from 'pages/reports/New'
-import ReportShow from 'pages/reports/Show'
-import ReportEdit from 'pages/reports/Edit'
-import ReportMinimal from 'pages/reports/Minimal'
-import MyReports from 'pages/reports/MyReports'
-
-import PersonShow from 'pages/people/Show'
-import PersonNew from 'pages/people/New'
-import PersonEdit from 'pages/people/Edit'
-
-import OrganizationShow from 'pages/organizations/Show'
-import OrganizationNew from 'pages/organizations/New'
-import OrganizationEdit from 'pages/organizations/Edit'
-
-import LocationShow from 'pages/locations/Show'
-import LocationEdit from 'pages/locations/Edit'
-import LocationNew from 'pages/locations/New'
-
-import PositionShow from 'pages/positions/Show'
-import PositionEdit from 'pages/positions/Edit'
-import PositionNew from 'pages/positions/New'
-
-import TaskShow from 'pages/tasks/Show'
-import TaskNew from 'pages/tasks/New'
-import TaskEdit from 'pages/tasks/Edit'
-
-import AdminIndex from 'pages/admin/Index'
-import MergePeople from 'pages/admin/MergePeople'
-import AuthorizationGroups from 'pages/admin/AuthorizationGroups'
-import AuthorizationGroupShow from 'pages/admin/authorizationgroup/Show'
-import AuthorizationGroupEdit from 'pages/admin/authorizationgroup/Edit'
-import AuthorizationGroupNew from 'pages/admin/authorizationgroup/New'
-
-import InsightsShow from  'pages/insights/Show'
-
-import OnboardingShow from 'pages/onboarding/Show'
-import OnboardingEdit from 'pages/onboarding/Edit'
-
+import { Person, Organization } from 'models'
 import AppContext from 'components/AppContext'
+import Routing from 'pages/Routing'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-import {Element} from 'react-scroll'
-class App extends Page {
 
+class App extends Page {
 	static propTypes = {
 		...pagePropTypes,
 		pageProps: PropTypes.object,
@@ -129,7 +82,6 @@ class App extends Page {
 				}
 			})
 		})
-
 	}
 
 	processData(data) {
@@ -150,118 +102,61 @@ class App extends Page {
 	}
 
 	render() {
-		const routing = <Switch>
-			<Route exact path="/" component={Home} />
-			<Route path="/search" component={Search} />
-			<Route path="/rollup" component={RollupShow} />
-			<Route path="/graphiql" component={GraphiQL} />
-			<Route path="/help" component={Help} />
-			<Route
-				path="/reports"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={ReportNew} />
-					<Route path={`${url}/:id/edit`} component={ReportEdit} />
-					<Route path={`${url}/:id/min`} component={ReportMinimal} />
-					<Route path={`${url}/mine`} component={MyReports} />
-					<Route path={`${url}/:id`} component={ReportShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/people"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={PersonNew} />
-					<Route path={`${url}/:id/edit`} component={PersonEdit} />
-					<Route path={`${url}/:id`} component={PersonShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/organizations"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={OrganizationNew} />
-					<Route path={`${url}/:id/edit`} component={OrganizationEdit} />
-					<Route path={`${url}/:id/:action?`} component={OrganizationShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/locations"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={LocationNew} />
-					<Route path={`${url}/:id/edit`} component={LocationEdit} />
-					<Route path={`${url}/:id`} component={LocationShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/positions"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={PositionNew} />
-					<Route path={`${url}/:id/edit`} component={PositionEdit} />
-					<Route path={`${url}/:id`} component={PositionShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/tasks"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/new`} component={TaskNew} />
-					<Route path={`${url}/:id/edit`} component={TaskEdit} />
-					<Route path={`${url}/:id`} component={TaskShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/admin"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route exact path={`${url}/`} component={AdminIndex} />
-					<Route path={`${url}/mergePeople`} component={MergePeople} />
-					<Route exact path={`${url}/authorizationGroups`} component={AuthorizationGroups} />
-					<Route path={`${url}/authorizationGroups/new`} component={AuthorizationGroupNew} />
-					<Route path={`${url}/authorizationGroups/:id/edit`} component={AuthorizationGroupEdit} />
-					<Route path={`${url}/authorizationGroups/:id`} component={AuthorizationGroupShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/insights"
-				render={({ match: { url } }) => (
-				<Switch>
-					<Route path={`${url}/:insight`} component={InsightsShow} />
-				</Switch>
-			)}
-			/>
-			<Route
-				path="/onboarding"
-				render={({ match: { url } }) => (
-					this.state.currentUser.isNewUser() ? (
-						<Switch>
-							<Route exact path={`${url}/`} component={OnboardingShow} />
-							<Route path={`${url}/edit`} component={OnboardingEdit} />
-						</Switch>
-					) : ( // Redirect to home if user account exists already. Some users bookmark the onboarding - the very first page they hit
-						<Redirect to="/"/>
-					)
-			)}
-			/>
-
-			<Route path="*" component={PageMissing} />
-		</Switch>
-
+		const { currentUser, settings, floatingMenu, organizations } = this.state
+		const { pageProps, location } = this.props
 		const heightTopBar = 125
+
+		const container = {
+			height: '100%',
+			display: 'flex',
+			overflow: 'hidden',
+			paddingTop: heightTopBar
+		}
+		const mainViewportContainer = {
+			height: '100%',
+			width: '100%',
+			display:'flex',
+			flexDirection:'column'
+		}
+		const mainViewport = {
+			flexGrow: 1,
+			overflowY: 'auto',
+			paddingLeft: 15,
+			paddingRight: 15
+		}
+		const sidebarContainer = {
+			height: '100%',
+			display: 'flex',
+			flexDirection: 'column',
+			flexShrink: 0
+		}
+		const sidebar = {
+			flexGrow: 1,
+			overflowY: 'auto',
+			minWidth: 200,
+			paddingTop: 10,
+			paddingLeft: 8,
+			paddingRight: 8
+		}
+		const glassPane = {
+			position: 'absolute',
+			backgroundColor: `rgba(0, 0, 0, 0.6)`,
+			width: '100%',
+			height: '100vh',
+			marginTop: -8,
+			left: 0,
+			zIndex: 99,
+		}
+		const loadingBar = {
+			marginTop: -8,
+			backgroundColor: '#29d'
+		}
+
 		return (
 			<AppContext.Provider
 				value={{
-					appSettings: this.state.settings,
-					currentUser: this.state.currentUser,
+					appSettings: settings,
+					currentUser: currentUser,
 					loadAppData: this.loadData,
 					showFloatingMenu: this.showFloatingMenu,
 				}}
@@ -269,37 +164,36 @@ class App extends Page {
 				<div className="anet">
 					<TopBar
 						updateTopbarOffset={this.updateTopbarOffset}
-						minimalHeader={this.props.pageProps.minimalHeader}
-						location={this.props.location}
+						minimalHeader={pageProps.minimalHeader}
+						location={location}
 						toggleMenuAction={() => {
-							this.showFloatingMenu(!this.state.floatingMenu)
+							this.showFloatingMenu(!floatingMenu)
 						}} />
 
-					<div style={{height: '100%', display: 'flex', overflow: 'hidden', paddingTop: heightTopBar}}>
-					<LoadingBar showFastActions style={{marginTop: -8, backgroundColor: '#29d'}} />
+					<div style={container}>
+						<LoadingBar showFastActions style={loadingBar} />
 						<div
-							style={{marginTop: -heightTopBar}}
-							className={ this.state.floatingMenu === false ? null : "glass-pane" }
+							style={floatingMenu === false ? null : glassPane}
 							onClick={() => {
 								this.showFloatingMenu(false)
 							}}
 						/>
-						{(this.props.pageProps.useNavigation === true || this.state.floatingMenu === true) &&
-							<div className={ this.state.floatingMenu === false ? "hidden-xs" : "nav-overlay"}>
-								<div style={{height: '100%', display: 'flex', flexDirection: 'column', flexShrink: 0}}>
-									<div style={{flexGrow: 1, overflowY: 'auto', minWidth: 200, paddingTop: 10, paddingLeft: 8, paddingRight: 8}}>
-										<Nav organizations={this.state.organizations} />
+						{(pageProps.useNavigation === true || floatingMenu === true) &&
+							<div className={ floatingMenu === false ? "hidden-xs" : "nav-overlay"}>
+								<div style={sidebarContainer}>
+									<div style={sidebar}>
+										<Nav organizations={organizations} />
 									</div>
 								</div>
 							</div>
 						}
-						<div style={{height: '100%', width: '100%', display:'flex', flexDirection:'column'}}>
+						<div style={mainViewportContainer}>
 							<Element
-								style={{flexGrow: 1, overflowY: 'auto', paddingLeft: 15, paddingRight: 15}}
+								style={mainViewport}
 								name="mainViewport"
 								id="main-viewport"
 							>
-								{routing}
+								<Routing/>
 							</Element>
 						</div>
 					</div>
