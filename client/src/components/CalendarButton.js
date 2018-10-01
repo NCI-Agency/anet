3b1bf81c31761adaef4c22f3e67e400f542dc419
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2018-10-01 17:14:19 +0200

    Pass props from CalendarButton to DatePicker

--- client/src/components/CalendarButton.js
+++ client/src/components/CalendarButton.js
@@ -4,10 +4,11 @@ import DatePicker from 'react-16-bootstrap-date-picker'
 
 import CALENDAR_ICON from 'resources/calendar.png'
 
-export default () => 
+export default (props) => 
 	<DatePicker
 		showTodayButton
 		showClearButton={false}
 		style={{}}
 		customControl = {<Button><img src={CALENDAR_ICON} height={20} alt="Pick a date" /></Button>}
+		{...props}
 	/> 
