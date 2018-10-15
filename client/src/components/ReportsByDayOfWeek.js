afe2dad08f557d1c45124369e0762b2552e48f9d
Gertjan van Oosten <gertjan@west.nl>
2018-10-15 15:01:28 +0200

    NCI-Agency/anet#995: Extract common parent ReportsVisualization

--- client/src/components/ReportsByDayOfWeek.js
+++ client/src/components/ReportsByDayOfWeek.js
@@ -1,42 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import API from 'api'
 import autobind from 'autobind-decorator'
 
 import BarChart from 'components/BarChart'
 import ReportCollection, {FORMAT_MAP, FORMAT_SUMMARY, FORMAT_TABLE} from 'components/ReportCollection'
 
-import {Report} from 'models'
-
-import _isEqual from 'lodash/isEqual'
-
 import { connect } from 'react-redux'
 import LoaderHOC, {mapDispatchToProps} from 'HOC/LoaderHOC'
 
+import ReportsVisualisation from 'components/ReportsVisualisation'
 import ContainerDimensions from 'react-container-dimensions'
-import MosaicLayout from 'components/MosaicLayout'
 
-const d3 = require('d3')
-const chartId = 'reports_by_day_of_week'
-const GQL_CHART_FIELDS =  /* GraphQL */`
-  uuid
-  engagementDayOfWeek
-`
 const BarChartWithLoader = connect(null, mapDispatchToProps)(LoaderHOC('isLoading')('data')(BarChart))
-
-const GQL_MAP_FIELDS =  /* GraphQL */`
-  uuid
-  intent
-  location { uuid, name, lat, lng },
-`
-
 const Context = React.createContext()
 
 /*
  * Component displaying a chart with number of reports released within a certain
  * period. The counting is done grouped by day of the week. 
  */
-class ReportsByDayOfWeek extends Component {
+class ReportsByDayOfWeek extends ReportsVisualisation {
   static propTypes = {
     queryParams: PropTypes.object,
     showLoading: PropTypes.func.isRequired,
@@ -45,6 +27,12 @@ class ReportsByDayOfWeek extends Component {
 
   constructor(props) {
     super(props)
+
+    this.CHART_ID = 'reports_by_day_of_week'
+    this.GQL_CHART_FIELDS =  /* GraphQL */`
+      uuid
+      engagementDayOfWeek
+    `
     this.VISUALIZATIONS = [
       {
         id: 'rbdow-chart',
@@ -62,7 +50,7 @@ class ReportsByDayOfWeek extends Component {
         renderer: this.getReportMap,
       },
     ]
-    this.INITIAL_NODE = {
+    this.INITIAL_LAYOUT = {
       direction: 'column',
       first: {
         direction: 'row',
@@ -71,7 +59,9 @@ class ReportsByDayOfWeek extends Component {
       },
       second: this.VISUALIZATIONS[2].id,
     }
-    this.SELECTED_BAR_CLASS = 'selected-bar'
+    this.DESCRIPTION = `The reports are grouped by day of the week.
+      In order to see the list of published reports for a day of the week,
+      click on the bar corresponding to the day of the week.`
 
     this.state = {
       graphData: [],
@@ -84,6 +74,10 @@ class ReportsByDayOfWeek extends Component {
     }
   }
 
+  get additionalReportParams() {
+    return {engagementDayOfWeek: this.state.focusedSelection.dayOfWeekInt}
+  }
+
   @autobind
   getBarChart(id) {
     return <Context.Consumer>{context => (
@@ -92,14 +86,14 @@ class ReportsByDayOfWeek extends Component {
           <BarChartWithLoader
             width={width}
             height={height}
-            chartId={chartId}
+            chartId={this.chartId}
             data={context.graphData}
             xProp='dayOfWeekInt'
             yProp='reportsCount'
             xLabel='dayOfWeekString'
             onBarClick={this.goToSelection}
             updateChart={context.updateChart}
-            selectedBarClass={this.SELECTED_BAR_CLASS}
+            selectedBarClass={this.selectedBarClass}
             selectedBar={context.focusedSelection ? 'bar_' + context.focusedSelection.dayOfWeekInt : ''}
             isLoading={context.isLoading}
           />
@@ -141,23 +135,16 @@ class ReportsByDayOfWeek extends Component {
   }
 
   render() {
-    return <Context.Provider value={this.state}>
-      <MosaicLayout
-        visualizations={this.VISUALIZATIONS}
-        initialNode={this.INITIAL_NODE}
-        description={`The reports are grouped by day of the week. In order to see the list
-                      of published reports for a day of the week, click on the bar
-                      corresponding to the day of the week.`}
-      />
-    </Context.Provider>
+    return (
+      <Context.Provider value={this.state}>
+        {super.render()}
+      </Context.Provider>
+    )
   }
 
-  fetchData() {
-    this.setState( {isLoading: true} )
-    this.props.showLoading()
-    // Query used by the chart
-    const chartQuery = this.runChartQuery(this.chartQueryParams())
-    Promise.all([chartQuery]).then(values => {
+  @autobind
+  fetchChartData(chartQuery) {
+    return Promise.all([chartQuery]).then(values => {
       // The server returns values from 1 to 7
       let daysOfWeekInt = [1, 2, 3, 4, 5, 6, 7]
       // The day of the week (returned by the server) with value 1 is Sunday
@@ -178,110 +165,12 @@ class ReportsByDayOfWeek extends Component {
             r.reportsCount = simplifiedValues.filter(item => item.dayOfWeek === r.dayOfWeekInt).length
             return r})
       })
-      this.props.hideLoading()
     })
-    this.fetchReportData(true)
-  }
-
-  fetchReportData(includeAll) {
-    // Query used by the reports collection
-    const queries = [this.runReportsQuery(this.reportsQueryParams(false), false)]
-    if (includeAll) {
-      // Query used by the map
-      queries.push(this.runReportsQuery(this.reportsQueryParams(true), true))
-    }
-    Promise.all(queries).then(values => {
-      const stateUpdate = {
-        updateChart: false,  // only update the report list
-        reports: values[0].reportList
-      }
-      if (includeAll) {
-        Object.assign(stateUpdate, {
-          allReports: values[1].reportList.list
-        })
-      }
-      this.setState(stateUpdate)
-    })
-  }
-
-  chartQueryParams = () => {
-    const chartQueryParams = {}
-    Object.assign(chartQueryParams, this.props.queryParams)
-    Object.assign(chartQueryParams, {
-      pageNum: 0,
-      pageSize: 0,  // retrieve all the filtered reports
-    })
-    return chartQueryParams
-  }
-
-  runChartQuery = (chartQueryParams) => {
-    return API.query(/* GraphQL */`
-      reportList(query:$chartQueryParams) {
-        totalCount, list {
-          ${GQL_CHART_FIELDS}
-        }
-      }`, {chartQueryParams}, '($chartQueryParams: ReportSearchQueryInput)')
-  }
-
-  reportsQueryParams = (forMap) => {
-    const pageSize = forMap ? 0 : 10
-    const pageNum = forMap ? 0 : this.state.reportsPageNum
-    const reportsQueryParams = {}
-    Object.assign(reportsQueryParams, this.props.queryParams)
-    Object.assign(reportsQueryParams, {
-      pageNum,
-      pageSize
-    })
-    if (this.state.focusedSelection) {
-      Object.assign(reportsQueryParams, {engagementDayOfWeek: this.state.focusedSelection.dayOfWeekInt})
-    }
-    return reportsQueryParams
-  }
-
-  runReportsQuery = (reportsQueryParams, forMap) => {
-    return API.query(/* GraphQL */`
-      reportList(query:$reportsQueryParams) {
-        pageNum, pageSize, totalCount, list {
-          ${forMap ? GQL_MAP_FIELDS : ReportCollection.GQL_REPORT_FIELDS}
-        }
-      }`, {reportsQueryParams}, '($reportsQueryParams: ReportSearchQueryInput)')
   }
 
   @autobind
-  goToReportsPage(newPage) {
-    this.setState({updateChart: false, reportsPageNum: newPage}, () => this.fetchReportData(false))
-  }
-
-  @autobind
-  goToSelection(item) {
-    this.updateHighlight(item, true)
-    // Note: we set updateChart to false as we do not want to re-render the chart
-    // when changing the focus.
-    if (!item || item === this.state.focusedSelection) {
-      this.setState({updateChart: false, reportsPageNum: 0, focusedSelection: ''}, () => this.fetchReportData(true))
-    } else {
-      this.setState({updateChart: false, reportsPageNum: 0, focusedSelection: item}, () => this.fetchReportData(true))
-      this.updateHighlight(item, false)
-    }
-  }
-
   updateHighlight(focusedSelection, clear) {
-    if (clear) {
-      // remove highlighting of the bars
-      d3.selectAll('#' + chartId + ' rect').classed(this.SELECTED_BAR_CLASS, false)
-    } else if (focusedSelection) {
-      // highlight the bar corresponding to the selected day of the week
-      d3.select('#' + chartId + ' #bar_' + focusedSelection.dayOfWeekInt).classed(this.SELECTED_BAR_CLASS, true)
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!_isEqual(prevProps.queryParams, this.props.queryParams)) {
-      this.setState({
-        reportsPageNum: 0,
-        focusedSelection: ''  // reset focus when changing the queryParams
-      }, () => this.fetchData())
-    }
+    super.updateHighlight(focusedSelection ? focusedSelection.dayOfWeekInt : '', clear)
   }
 }
 
--- client/src/components/ReportsByTask.js
+++ client/src/components/ReportsByTask.js
@@ -1,44 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import API from 'api'
 import autobind from 'autobind-decorator'
 
 import BarChart from 'components/BarChart'
 import ReportCollection, {FORMAT_MAP, FORMAT_SUMMARY, FORMAT_TABLE} from 'components/ReportCollection'
 
-import {Report} from 'models'
-
-import _isEqual from 'lodash/isEqual'
-
 import { connect } from 'react-redux'
 import LoaderHOC, {mapDispatchToProps} from 'HOC/LoaderHOC'
 import Settings from 'Settings'
 
+import ReportsVisualisation from 'components/ReportsVisualisation'
 import ContainerDimensions from 'react-container-dimensions'
-import MosaicLayout from 'components/MosaicLayout'
 
 import pluralize from 'pluralize'
 
-const d3 = require('d3')
-const chartId = 'reports_by_task'
-const GQL_CHART_FIELDS =  /* GraphQL */`
-  uuid
-  tasks { uuid, shortName }
-`
 const BarChartWithLoader = connect(null, mapDispatchToProps)(LoaderHOC('isLoading')('data')(BarChart))
-
-const GQL_MAP_FIELDS =  /* GraphQL */`
-  uuid
-  intent
-  location { uuid, name, lat, lng },
-`
-
 const Context = React.createContext()
 
 /*
  * Component displaying a chart with number of reports per Task.
  */
-class ReportsByTask extends Component {
+class ReportsByTask extends ReportsVisualisation {
   static propTypes = {
     queryParams: PropTypes.object,
     showLoading: PropTypes.func.isRequired,
@@ -47,7 +29,13 @@ class ReportsByTask extends Component {
 
   constructor(props) {
     super(props)
+
     this.taskShortLabel = Settings.fields.task.shortLabel
+    this.CHART_ID = 'reports_by_task'
+    this.GQL_CHART_FIELDS =  /* GraphQL */`
+      uuid
+      tasks { uuid shortName }
+    `
     this.VISUALIZATIONS = [
       {
         id: 'rbt-chart',
@@ -65,7 +53,7 @@ class ReportsByTask extends Component {
         renderer: this.getReportMap,
       },
     ]
-    this.INITIAL_NODE = {
+    this.INITIAL_LAYOUT = {
       direction: 'column',
       first: {
         direction: 'row',
@@ -74,7 +62,9 @@ class ReportsByTask extends Component {
       },
       second: this.VISUALIZATIONS[2].id,
     }
-    this.SELECTED_BAR_CLASS = 'selected-bar'
+    this.DESCRIPTION = `The reports are grouped by ${this.taskShortLabel}.
+      In order to see the list of published reports for a ${this.taskShortLabel},
+      click on the bar corresponding to the ${this.taskShortLabel}.`
 
     this.state = {
       graphData: [],
@@ -87,6 +77,10 @@ class ReportsByTask extends Component {
     }
   }
 
+  get additionalReportParams() {
+    return {taskUuid: this.state.focusedSelection.task.uuid}
+  }
+
   @autobind
   getBarChart(id) {
     return <Context.Consumer>{context => (
@@ -95,15 +89,15 @@ class ReportsByTask extends Component {
           <BarChartWithLoader
             width={width}
             height={height}
-            chartId={chartId}
+            chartId={this.chartId}
             data={context.graphData}
             xProp='task.uuid'
             yProp='reportsCount'
             xLabel='task.shortName'
             onBarClick={this.goToSelection}
             updateChart={context.updateChart}
-            selectedBarClass={this.SELECTED_BAR_CLASS}
-            selectedBar={context.focusedSelection ? 'bar_' + context.focusedSelection.uuid : ''}
+            selectedBarClass={this.selectedBarClass}
+            selectedBar={context.focusedSelection ? 'bar_' + context.focusedSelection.task.uuid : ''}
             isLoading={context.isLoading}
           />
         )}</ContainerDimensions>
@@ -144,29 +138,22 @@ class ReportsByTask extends Component {
   }
 
   render() {
-    return <Context.Provider value={this.state}>
-      <MosaicLayout
-        visualizations={this.VISUALIZATIONS}
-        initialNode={this.INITIAL_NODE}
-        description={`The reports are grouped by ${this.taskShortLabel}. In order to see the
-                      list of published reports for a ${this.taskShortLabel}, click on the bar
-                      corresponding to the ${this.taskShortLabel}.`}
-      />
-    </Context.Provider>
+    return (
+      <Context.Provider value={this.state}>
+        {super.render()}
+      </Context.Provider>
+    )
   }
 
-  fetchData() {
-    this.setState( {isLoading: true} )
-    this.props.showLoading()
-    // Query used by the chart
-    const chartQuery = this.runChartQuery(this.chartQueryParams())
-    const noTaskMessage = `No ${Settings.fields.task.shortLabel}`
-    const noTask = {
-      uuid: "-1",
-      shortName: noTaskMessage,
-      longName: noTaskMessage
-    }
-    Promise.all([chartQuery]).then(values => {
+  @autobind
+  fetchChartData(chartQuery) {
+    return Promise.all([chartQuery]).then(values => {
+      const noTaskMessage = `No ${Settings.fields.task.shortLabel}`
+      const noTask = {
+        uuid: "-1",
+        shortName: noTaskMessage,
+        longName: noTaskMessage
+      }
       let simplifiedValues = values[0].reportList.list ? values[0].reportList.list.map(d => {return {reportUuid: d.uuid, tasks: d.tasks.map(p => p.uuid)}}): []
       let tasks = values[0].reportList.list ? values[0].reportList.list.map(d => d.tasks) : []
       tasks = [].concat.apply([], tasks)
@@ -184,111 +171,12 @@ class ReportsByTask extends Component {
             r.reportsCount = (d.uuid ? simplifiedValues.filter(item => item.tasks.indexOf(d.uuid) > -1).length : simplifiedValues.filter(item => item.tasks.length === 0).length)
             return r}),
       })
-      this.props.hideLoading()
     })
-    this.fetchReportData(true)
-  }
-
-  fetchReportData(includeAll) {
-    // Query used by the reports collection
-    const queries = [this.runReportsQuery(this.reportsQueryParams(false), false)]
-    if (includeAll) {
-      // Query used by the map
-      queries.push(this.runReportsQuery(this.reportsQueryParams(true), true))
-    }
-    Promise.all(queries).then(values => {
-      const stateUpdate = {
-        updateChart: false,  // only update the report list
-        reports: values[0].reportList
-      }
-      if (includeAll) {
-        Object.assign(stateUpdate, {
-          allReports: values[1].reportList.list
-        })
-      }
-      this.setState(stateUpdate)
-    })
-  }
-
-  chartQueryParams = () => {
-    const chartQueryParams = {}
-    Object.assign(chartQueryParams, this.props.queryParams)
-    Object.assign(chartQueryParams, {
-      pageNum: 0,
-      pageSize: 0,  // retrieve all the filtered reports
-    })
-    return chartQueryParams
-  }
-
-  runChartQuery = (chartQueryParams) => {
-    return API.query(/* GraphQL */`
-      reportList(query:$chartQueryParams) {
-        totalCount, list {
-          ${GQL_CHART_FIELDS}
-        }
-      }`, {chartQueryParams}, '($chartQueryParams: ReportSearchQueryInput)')
-  }
-
-  reportsQueryParams = (forMap) => {
-    const pageSize = forMap ? 0 : 10
-    const pageNum = forMap ? 0 : this.state.reportsPageNum
-    const reportsQueryParams = {}
-    Object.assign(reportsQueryParams, this.props.queryParams)
-    Object.assign(reportsQueryParams, {
-      pageNum,
-      pageSize
-    })
-    if (this.state.focusedSelection) {
-      Object.assign(reportsQueryParams, {taskUuid: this.state.focusedSelection.uuid})
-    }
-    return reportsQueryParams
-  }
-
-  runReportsQuery = (reportsQueryParams, forMap) => {
-    return API.query(/* GraphQL */`
-      reportList(query:$reportsQueryParams) {
-        pageNum, pageSize, totalCount, list {
-          ${forMap ? GQL_MAP_FIELDS : ReportCollection.GQL_REPORT_FIELDS}
-        }
-      }`, {reportsQueryParams}, '($reportsQueryParams: ReportSearchQueryInput)')
   }
 
   @autobind
-  goToReportsPage(newPage) {
-    this.setState({updateChart: false, reportsPageNum: newPage}, () => this.fetchReportData(false))
-  }
-
-  @autobind
-  goToSelection(item) {
-    item = item ? item.task : ''
-    this.updateHighlight(item, true)
-    // Note: we set updateChart to false as we do not want to re-render the chart
-    // when changing the focus.
-    if (!item || item === this.state.focusedSelection) {
-      this.setState({updateChart: false, reportsPageNum: 0, focusedSelection: ''}, () => this.fetchReportData(true))
-    } else {
-      this.setState({updateChart: false, reportsPageNum: 0, focusedSelection: item}, () => this.fetchReportData(true))
-      this.updateHighlight(item, false)
-    }
-  }
-
   updateHighlight(focusedSelection, clear) {
-    if (clear) {
-      // remove highlighting of the bars
-      d3.selectAll('#' + chartId + ' rect').classed(this.SELECTED_BAR_CLASS, false)
-    } else if (focusedSelection) {
-      // highlight the bar corresponding to the selected task
-      d3.select('#' + chartId + ' #bar_' + focusedSelection.uuid).classed(this.SELECTED_BAR_CLASS, true)
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (!_isEqual(prevProps.queryParams, this.props.queryParams)) {
-      this.setState({
-        reportsPageNum: 0,
-        focusedSelection: ''  // reset focus when changing the queryParams
-      }, () => this.fetchData())
-    }
+    super.updateHighlight(focusedSelection ? focusedSelection.task.uuid : '', clear)
   }
 }
 
