3d3bcf3ace30c2f8712b158a62a80f0e7a56c3ea
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2018-11-19 15:43:17 +0100

    Add api.js for node targets
    implements nci-agency/anet#379

---
+++ client/platform/node/api.js
@@ -0,0 +1,56 @@
+import XMLHttpRequest from 'xhr2'
+import fetch from 'node-fetch'
+
+const API = {
+	_fetch(pathName, params, accept) {
+		// TODO implement
+	},
+
+	_send(url, data, params) {
+		// TODO implement
+	},
+
+	_queryCommon(query, variables, variableDef, output, isMutation, params) {
+		variables = variables || {}
+		variableDef = variableDef || ''
+		const queryType = isMutation ? 'mutation' : 'query'
+		query = queryType + ' ' + variableDef + ' { ' + query + ' }'
+		output = output || ''
+		return API._send('/graphql', {query, variables, output}, params)
+	},
+
+	mutation(query, variables, variableDef, params) {
+		return API._queryCommon(query, variables, variableDef, undefined, true, params).then(json => json.data)
+	},
+
+	query(query, variables, variableDef, params) {
+		return API._queryCommon(query, variables, variableDef, undefined, undefined, params).then(json => json.data)
+	},
+
+	queryExport(query, variables, variableDef, output) {
+		return API._queryCommon(query, variables, variableDef, output).then(response => response.blob())
+	},
+
+	loadFileAjaxSync(filePath, mimeType) {
+		// TODO implement
+		return "{}"
+	},
+
+	_getAuthParams: function() {
+		// TODO implement
+		return {
+			user: "arthur",
+			pass: ""
+		}
+	},
+
+	addAuthParams: function(url) {
+		// TODO implement
+	},
+
+	_getAuthHeader: function() {
+		// TODO implement
+	}
+}
+
+export default API
