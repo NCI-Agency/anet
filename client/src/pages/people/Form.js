e55d2587698f8c25dbb4ca823e130e9acf45e30a
Edwin van den Houdt <edwin@vdhoudt.nl>
2018-11-27 13:46:12 +0100

    GH-917 Use Formik for people form

--- client/src/pages/people/Form.js
+++ client/src/pages/people/Form.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { Component } from 'react'
 import {Button, Alert, HelpBlock, Radio, Col, ControlLabel, FormGroup} from 'react-bootstrap'
 import DatePicker from 'react-16-bootstrap-date-picker'
 import autobind from 'autobind-decorator'
 
-import ValidatableFormWrapper from 'components/ValidatableFormWrapper'
-import Form from 'components/Form'
+import { Formik, Form, Field } from 'formik'
+import * as FieldHelper from 'components/FieldHelper'
+
 import Fieldset from 'components/Fieldset'
 import Messages from 'components/Messages'
 import RichTextEditor from 'components/RichTextEditor'
@@ -29,7 +30,43 @@ import NavigationWarning from 'components/NavigationWarning'
 import { jumpToTop } from 'components/Page'
 import _isEmpty from 'lodash/isEmpty'
 
-class BasePersonForm extends ValidatableFormWrapper {
+// Handle events
+const handleLastNameOnKeyDown = (event) => {
+	// adding a "," to the last name results in jumping to the end of the first name
+	if (event.key === ',') {
+		event.preventDefault()
+		document.getElementById('firstName').focus()
+	}
+}
+
+const statusButtons = [
+	{
+		id: 'statusActiveButton',
+		value: Person.STATUS.ACTIVE,
+		label: 'ACTIVE',
+	},
+	{
+		id: 'statusInactiveButton',
+		value: Person.STATUS.INACTIVE,
+		label: 'INACTIVE'
+	},
+]
+
+const roleButtons = ({ isAdmin, title }) => [
+	{
+		id: 'roleAdvisorButton',
+		title: title,
+		value: Person.ROLE.ADVISOR,
+		label: Settings.fields.advisor.person.name,
+		disabled: !isAdmin
+	},
+	{
+		id: 'rolePrincipalButton',
+		value: Person.ROLE.PRINCIPAL,
+		label: Settings.fields.principal.person.name
+	},
+]
+class BasePersonForm extends Component {
 	static propTypes = {
 		person: PropTypes.object.isRequired,
 		original: PropTypes.object.isRequired,
@@ -87,14 +124,12 @@ class BasePersonForm extends ValidatableFormWrapper {
 	}
 
 	render() {
-		const { person } = this.props
-		if (!person) return null
-		const { edit } = this.props
-		const isAdvisor = person.isAdvisor()
 		const { fullName } = this.state
-		const legendText = this.props.legendText || (edit ? `Edit Person ${fullName}` : 'Create a new Person')
+		const { currentUser, person, edit, title, ...myFormProps } = this.props
 
-		const {ValidatableForm, RequiredField} = this
+		if (!person) return null
+		const isAdvisor = person.isAdvisor()
+		const legendText = this.props.legendText || (edit ? `Edit Person ${fullName}` : 'Create a new Person')
 
 		const willAutoKickPosition = person.status === Person.STATUS.INACTIVE && person.position && !!person.position.uuid
 		const warnDomainUsername = person.status === Person.STATUS.INACTIVE && !_isEmpty(person.domainUsername)
@@ -105,16 +140,7 @@ class BasePersonForm extends ValidatableFormWrapper {
 			// For new objects, assign default country if there's only one
 			person.country = countries[0]
 		}
-		const firstNameProps = {
-			id: "firstName",
-			type: "text",
-			display: "inline",
-			placeholder: "First name(s) - Lower-case except for the first letter of each name",
-			value: this.state.splitName.firstName,
-			onChange: this.handleOnChangeFirstName
-		}
 
-		const { currentUser } = this.props
 		const isAdmin = currentUser && currentUser.isAdmin()
 		const isSelf = Person.isEqual(currentUser, person)
 		// anyone with edit permissions can change status to INACTIVE, only admins can change back to ACTIVE (but nobody can change status of self!)
@@ -136,181 +162,247 @@ class BasePersonForm extends ValidatableFormWrapper {
 		const advisorPlural = pluralize(advisorSingular)
 		const superUserAdvisorTitle = isAdmin ? null : `Super users cannot create ${advisorSingular} profiles. ANET uses the domain user name to authenticate and uniquely identify each ANET user. To ensure that ${advisorPlural} have the correct domain name associated with their profile, it is required that each new ${advisorSingular} individually logs into ANET and creates their own ANET profile.`
 
-		return <div>
-			<NavigationWarning isBlocking={this.state.isBlocking} />
+		return <Formik
+			enableReinitialize={true}
+			onSubmit={this.onSubmit}
+			{...myFormProps}
+		>
+		{({
+			handleSubmit,
+			isSubmitting,
+			isValid,
+			dirty,
+			errors,
+			setFieldValue,
+			values,
+			submitForm
+		}) => {
+			const action = <div>
+				<Button key="submit" bsStyle="primary" type="button" onClick={submitForm} disabled={isSubmitting || !isValid}>Save person</Button>
+			</div>
+			return <React.Fragment>
+				<NavigationWarning isBlocking={dirty} />
+				<Form className="form-horizontal" method="post">
+					<Messages error={this.state.error} />
+					<Fieldset title={legendText} action={action} />
+					<Fieldset>
+						<FormGroup>
+							<Col sm={2} componentClass={ControlLabel}>Name</Col>
+							<Col sm={7}>
+								<Col sm={5}>
+									<Field
+										name="lastName"
+										component={FieldHelper.renderInputFieldNoLabel}
+										display="inline"
+										placeholder="LAST NAME"
+										disabled={!canEditName}
+										value={this.state.splitName.lastName}
+										onChange={this.handleOnChangeLastName}
+										onKeyDown={handleLastNameOnKeyDown}
+									/>
+								</Col>
+								<Col sm={1} className="name-input">,</Col>
+								<Col sm={6}>
+									<Field
+										name="firstName"
+										component={FieldHelper.renderInputFieldNoLabel}
+										display="inline"
+										placeholder="First name(s) - Lower-case except for the first letter of each name"
+										disabled={!canEditName}
+										value={this.state.splitName.firstName}
+										onChange={this.handleOnChangeFirstName}
+									/>
+								</Col>
+								<Field disabled={!canEditName} className="hidden" name="name" value={fullName} />
+							</Col>
 
-			<ValidatableForm formFor={person} onChange={this.onChange} onSubmit={this.onSubmit} horizontal
-			submitText={this.props.saveText || 'Save person'}>
+							{edit && !canEditName &&
+								<React.Fragment>
+									<TriggerableConfirm
+										onConfirm={this.confirmReset.bind(this)}
+										title="Confirm to reset account"
+										body="Are you sure you want to reset this account?"
+										confirmText={confirmLabel}
+										cancelText="No, I am not entirely sure at this point"
+										bsStyle="warning"
+										buttonLabel="Reset account"
+										className="hidden"
+										ref={confirmComponent => this.confirmHasReplacementButton = confirmComponent} />
+									<Button id="wrongPerson" onClick={this.showWrongPersonModal}>{nameMessage}</Button>
+									<OptionListModal
+										title={modalTitle}
+										showModal={this.state.showWrongPersonModal}
+										onCancel={this.hideWrongPersonModal.bind(this)}
+										onSuccess={this.hideWrongPersonModal.bind(this)}>
+										{(isSelf &&
+											<div>
+												<Radio name="wrongPerson" value="needNewAccount">
+													<em>{fullName}</em> has left and is replaced by me. I need to set up a new account.
+												</Radio>
+												<Radio name="wrongPerson" value="haveAccount">
+													<em>{fullName}</em> has left and is replaced by me. I already have an account.
+												</Radio>
+												<Radio name="wrongPerson" value="transferAccount">
+													<em>{fullName}</em> is still active, but this should be my account.
+												</Radio>
+												<Radio name="wrongPerson" value="misspelledName">
+													I am <em>{fullName}</em>, but my name is misspelled.
+												</Radio>
+												<Radio name="wrongPerson" value="otherError">
+													Something else is wrong.
+												</Radio>
+											</div>
+										) || (
+											<div>
+												<Radio name="wrongPerson" value="leftVacant">
+													<em>{fullName}</em> has left and the position is vacant.
+												</Radio>
+												<Radio name="wrongPerson" value="hasReplacement">
+													<em>{fullName}</em> has left and has a replacement.
+												</Radio>
+												<Radio name="wrongPerson" value="misspelledName">
+													The name of <em>{fullName}</em> is misspelled.
+												</Radio>
+												<Radio name="wrongPerson" value="otherError">
+													Something else is wrong.
+												</Radio>
+											</div>
+										)}
+									</OptionListModal>
+								</React.Fragment>
+							}
+						</FormGroup>
 
-			<Messages error={this.state.error} />
-
-			<Fieldset title={legendText}>
-				<FormGroup>
-					<Col sm={2} componentClass={ControlLabel}>Name</Col>
-					<Col sm={7}>
-						<Col sm={5}>
-							<RequiredField disabled={!canEditName}
-								id="lastName"
-								type="text"
-								display="inline"
-								placeholder="LAST NAME"
-								value={this.state.splitName.lastName}
-								onChange={this.handleOnChangeLastName}
-								onKeyDown={this.handleOnKeyDown}
-								/>
-						</Col>
-						<Col sm={1} className="name-input">,</Col>
-						<Col sm={6}>
-						{isAdvisor ?
-							<RequiredField disabled={!canEditName} {...firstNameProps} />
-							:
-							<Form.Field disabled={!canEditName} {...firstNameProps} />
+						{isAdmin &&
+							<Field
+								name="domainUsername"
+								component={FieldHelper.renderInputField}
+								extraColElem={<span className="text-danger">Be careful when changing this field; you might lock someone out or create duplicate accounts.</span>}
+							/>
 						}
-						</Col>
-						<RequiredField disabled={!canEditName} className="hidden" id="name" value={fullName} />
-					</Col>
 
-					{edit && !canEditName &&
-						<div>
-							<TriggerableConfirm
-								onConfirm={this.confirmReset.bind(this)}
-								title="Confirm to reset account"
-								body="Are you sure you want to reset this account?"
-								confirmText={confirmLabel}
-								cancelText="No, I am not entirely sure at this point"
-								bsStyle="warning"
-								buttonLabel="Reset account"
-								className="hidden"
-								ref={confirmComponent => this.confirmHasReplacementButton = confirmComponent} />
-							<Button id="wrongPerson" onClick={this.showWrongPersonModal}>{nameMessage}</Button>
-							<OptionListModal
-								title={modalTitle}
-								showModal={this.state.showWrongPersonModal}
-								onCancel={this.hideWrongPersonModal.bind(this)}
-								onSuccess={this.hideWrongPersonModal.bind(this)}>
-								{(isSelf &&
-									<div>
-										<Radio name="wrongPerson" value="needNewAccount">
-											<em>{fullName}</em> has left and is replaced by me. I need to set up a new account.
-										</Radio>
-										<Radio name="wrongPerson" value="haveAccount">
-											<em>{fullName}</em> has left and is replaced by me. I already have an account.
-										</Radio>
-										<Radio name="wrongPerson" value="transferAccount">
-											<em>{fullName}</em> is still active, but this should be my account.
-										</Radio>
-										<Radio name="wrongPerson" value="misspelledName">
-											I am <em>{fullName}</em>, but my name is misspelled.
-										</Radio>
-										<Radio name="wrongPerson" value="otherError">
-											Something else is wrong.
-										</Radio>
-									</div>
-								) || (
-									<div>
-										<Radio name="wrongPerson" value="leftVacant">
-											<em>{fullName}</em> has left and the position is vacant.
-										</Radio>
-										<Radio name="wrongPerson" value="hasReplacement">
-											<em>{fullName}</em> has left and has a replacement.
-										</Radio>
-										<Radio name="wrongPerson" value="misspelledName">
-											The name of <em>{fullName}</em> is misspelled.
-										</Radio>
-										<Radio name="wrongPerson" value="otherError">
-											Something else is wrong.
-										</Radio>
-									</div>
+						{edit ?
+							<Field
+								name="role"
+								component={FieldHelper.renderReadonlyField}
+								value={person.humanNameOfRole}
+							/>
+								:
+							<Field
+								name="role"
+								component={FieldHelper.renderButtonToggleGroup}
+								buttons={roleButtons({ isAdmin, title: superUserAdvisorTitle})}
+							/>
+						}
+
+						{disableStatusChange ?
+							<Field
+								name="status"
+								component={FieldHelper.renderReadonlyField}
+								type="text"
+							/>
+								:
+							person.isNewUser() ?
+								<Field
+									name="status"
+									component={FieldHelper.renderReadonlyField}
+								/>
+								:
+								<Field
+									name="status"
+									component={FieldHelper.renderButtonToggleGroup}
+									buttons={statusButtons}
+								>
+								{willAutoKickPosition && <HelpBlock>
+									<span className="text-danger">Settings this person to inactive will automatically remove them from the <strong>{person.position.name}</strong> position.</span>
+									</HelpBlock> }
+
+									{warnDomainUsername && <HelpBlock>
+										<span className="text-danger">Settings this person to inactive means the next person to logon with the user name <strong>{person.domainUsername}</strong> will have to create a new profile. Do you want the next person to login with this user name to create a new profile?</span>
+									</HelpBlock> }
+								</Field>
+						}
+
+						{!edit && isAdvisor &&
+							<Alert bsStyle="warning">
+								Creating a {Settings.fields.advisor.person.name} in ANET could result in duplicate accounts if this person logs in later. If you notice duplicate accounts, please contact an ANET administrator.
+							</Alert>
+						}
+					</Fieldset>
+
+					<Fieldset title="Additional information">
+						<Field
+							name="emailAddress"
+							label="Email"
+							type="email"
+							component={FieldHelper.renderInputField}
+						/>
+						<Field
+							name="phoneNumber"
+							label="Phone"
+							component={FieldHelper.renderInputField}
+						/>
+						<Field
+							name="rank"
+							component={FieldHelper.renderSpecialField}
+						>
+							<Field component="select">
+								<option />
+								{ranks.map(rank =>
+									<option key={rank} value={rank}>{rank}</option>
 								)}
-							</OptionListModal>
+							</Field>
+						</Field>
+						<Field
+							name="gender"
+							component={FieldHelper.renderSpecialField}
+						>
+							<Field component="select">
+								<option />
+								<option value="MALE" >Male</option>
+								<option value="FEMALE" >Female</option>
+							</Field>
+						</Field>
+						<Field
+							name="country"
+							label="Nationality"
+							component={FieldHelper.renderSpecialField}
+						>
+							<Field component="select">
+								<option />
+								{this.renderCountrySelectOptions(countries)}
+							</Field>
+						</Field>
+						<Field
+							name="endOfTourDate"
+							label="End of tour"
+							component={FieldHelper.renderSpecialField}
+							onChange={(value, formattedValue) => setFieldValue('endOfTourDate', value)}
+							addon={CALENDAR_ICON}
+						>
+							<DatePicker placeholder="End of Tour Date" dateFormat="DD/MM/YYYY" showClearButton={false} />
+						</Field>
+						<Field
+							name="biography"
+							component={FieldHelper.renderSpecialField}
+							onChange={(value) => setFieldValue('biography', value)}
+							className="biography"
+						>
+							<RichTextEditor />
+						</Field>
+					</Fieldset>
+					<div className="submit-buttons">
+						<div>
+							<Button onClick={this.onCancel}>Cancel</Button>
 						</div>
-					}
-				</FormGroup>
-
-				{isAdmin &&
-					<Form.Field id="domainUsername">
-						<Form.Field.ExtraCol>
-							<span className="text-danger">Be careful when changing this field; you might lock someone out or create duplicate accounts.</span>
-						</Form.Field.ExtraCol>
-					</Form.Field>
-				}
-
-				{edit ?
-					<Form.Field type="static" id="role" value={person.humanNameOfRole()} />
-					:
-					<Form.Field id="role">
-						<ButtonToggleGroup>
-							<Button id="roleAdvisorButton" disabled={!isAdmin} title={superUserAdvisorTitle} value={Person.ROLE.ADVISOR}>{Settings.fields.advisor.person.name}</Button>
-							<Button id="rolePrincipalButton" value={Person.ROLE.PRINCIPAL}>{Settings.fields.principal.person.name}</Button>
-						</ButtonToggleGroup>
-					</Form.Field>
-				}
-
-				{disableStatusChange ?
-					<Form.Field type="static" id="status" value={person.humanNameOfStatus()} />
-					:
-					person.isNewUser() ?
-						<Form.Field type="static" id="status" value="New user" />
-						:
-						<Form.Field id="status" >
-							<ButtonToggleGroup>
-								<Button id="statusActiveButton" value={ Person.STATUS.ACTIVE }>Active</Button>
-								<Button id="statusInactiveButton" value={ Person.STATUS.INACTIVE }>Inactive</Button>
-							</ButtonToggleGroup>
-
-							{willAutoKickPosition && <HelpBlock>
-								<span className="text-danger">Settings this person to inactive will automatically remove them from the <strong>{person.position.name}</strong> position.</span>
-							</HelpBlock> }
-
-							{warnDomainUsername && <HelpBlock>
-								<span className="text-danger">Settings this person to inactive means the next person to logon with the user name <strong>{person.domainUsername}</strong> will have to create a new profile. Do you want the next person to login with this user name to create a new profile?</span>
-							</HelpBlock> }
-						</Form.Field>
-				}
-
-				{!edit && isAdvisor &&
-					<Alert bsStyle="warning">
-						Creating a {Settings.fields.advisor.person.name} in ANET could result in duplicate accounts if this person logs in later. If you notice duplicate accounts, please contact an ANET administrator.
-					</Alert>
-				}
-			</Fieldset>
-
-			<Fieldset title="Additional information">
-				<RequiredField id="emailAddress" label="Email" required={isAdvisor}
-					humanName="Valid email address"
-					type="email"
-					validate={ this.handleEmailValidation } />
-				<Form.Field id="phoneNumber" label="Phone" />
-				<RequiredField id="rank"  componentClass="select"
-					required={isAdvisor}>
-
-					<option />
-					{ranks.map(rank =>
-						<option key={rank} value={rank}>{rank}</option>
-					)}
-				</RequiredField>
-
-				<RequiredField id="gender" componentClass="select"
-					required={isAdvisor}>
-					<option />
-					<option value="MALE" >Male</option>
-					<option value="FEMALE" >Female</option>
-				</RequiredField>
-
-				<RequiredField id="country" label="Nationality" componentClass="select"
-					required={isAdvisor}>
-					<option />
-					{this.renderCountrySelectOptions(countries)}
-				</RequiredField>
-
-				<RequiredField  id="endOfTourDate" label="End of tour" addon={CALENDAR_ICON} required={isAdvisor} addOnBlur={true}>
-					<DatePicker placeholder="End of Tour Date" dateFormat="DD/MM/YYYY" showClearButton={false} />
-				</RequiredField>
-
-				<Form.Field id="biography" componentClass={RichTextEditor} className="biography" />
-			</Fieldset>
-		</ValidatableForm>
-		</div>
+						<div>
+							<Button id="formBottomSubmit" bsStyle="primary" type="button" onClick={submitForm} disabled={isSubmitting || !isValid}>Save Person</Button>
+						</div>
+					</div>
+				</Form>
+			</React.Fragment>
+		}}
+		</Formik>
 	}
 
 	getFullName(splitName, editName) {
