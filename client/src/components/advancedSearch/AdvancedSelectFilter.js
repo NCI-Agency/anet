ba360773d51e09ea118f6c3d7a2f157cd83784cb
Mara Dragan <mara@west.nl>
2019-04-04 17:24:16 +0200

    NCI-Agency/anet#1326: Add AdvancedSelectFilter for advanced search
    This is a filter using the AdvancedSingleSelect widget.

---
+++ client/src/components/advancedSearch/AdvancedSelectFilter.js
@@ -0,0 +1,126 @@
+import API from "api"
+import autobind from "autobind-decorator"
+import AdvancedSingleSelect from "components/advancedSelectWidget/AdvancedSingleSelect"
+import _isEqualWith from "lodash/isEqualWith"
+import PropTypes from "prop-types"
+import React, { Component } from "react"
+import utils from "utils"
+
+export default class AdvancedSelectFilter extends Component {
+  static propTypes = {
+    // An AdvancedSingleSelect filter allows users to search the ANET database
+    // for existing records and use that records ID as the search term.
+    // the queryKey property tells this filter what property to set on the
+    // search query. (ie authorUuid, organizationUuid, etc)
+    queryKey: PropTypes.string.isRequired,
+
+    // Passed by the SearchFilter row
+    onChange: PropTypes.func,
+
+    // Passed by the SearchFilterDisplay row
+    asFormField: PropTypes.bool
+
+    // All other properties are passed directly to the AdvancedSingleSelect
+  }
+
+  static defaultProps = {
+    asFormField: true
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: props.value || {}
+    }
+  }
+
+  componentDidMount() {
+    this.updateFilter()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      !_isEqualWith(
+        prevProps.value,
+        this.props.value,
+        utils.treatFunctionsAsEqual
+      )
+    ) {
+      this.setState({ value: this.props.value }, this.updateFilter)
+    }
+  }
+
+  render() {
+    let advancedSelectProps = Object.without(
+      this.props,
+      "value",
+      "queryKey",
+      "asFormField",
+      "onChange"
+    )
+    return !this.props.asFormField ? (
+      <React.Fragment>{this.props.value[this.props.valueKey]}</React.Fragment>
+    ) : (
+      <AdvancedSingleSelect
+        {...advancedSelectProps}
+        fieldLabel={null}
+        vertical
+        showRemoveButton={false}
+        onChange={this.onChange}
+        value={this.state.value}
+      />
+    )
+  }
+
+  @autobind
+  onChange(event) {
+    if (typeof event === "object") {
+      this.setState({ value: event }, this.updateFilter)
+    }
+  }
+
+  @autobind
+  toQuery() {
+    return { [this.props.queryKey]: this.state.value && this.state.value.uuid }
+  }
+
+  @autobind
+  updateFilter() {
+    if (this.props.asFormField) {
+      let { value } = this.state
+      value.toQuery = this.toQuery
+      this.props.onChange(value)
+    }
+  }
+
+  @autobind
+  deserialize(query, key) {
+    if (query[this.props.queryKey]) {
+      const getInstanceName = this.props.objectType.getInstanceName
+      const graphQlQuery =
+        getInstanceName +
+        '(uuid:"' +
+        query[this.props.queryKey] +
+        '") { ' +
+        this.props.fields +
+        "}"
+      return API.query(graphQlQuery).then(data => {
+        if (data[getInstanceName]) {
+          const toQueryValue = {
+            [this.props.queryKey]: query[this.props.queryKey]
+          }
+          return {
+            key: key,
+            value: {
+              ...data[getInstanceName],
+              toQuery: () => toQueryValue
+            }
+          }
+        } else {
+          return null
+        }
+      })
+    }
+    return null
+  }
+}
