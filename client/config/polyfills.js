db04c832d286bf340b3d47147867228c1085a668
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2019-04-09 13:29:45 +0200

    Let unhandled promise rejections propagate

--- client/config/polyfills.js
+++ client/config/polyfills.js
@@ -1,10 +1,5 @@
 import 'core-js/stable'
 import 'locale-compare-polyfill'
 
-// Unhandled rejection may affect REACT state. Making sure these are logged
-// TODO: log on server
-// TODO: move this to platform specfic api.js for web and add nodejs implementation
-window.addEventListener('unhandledrejection', e => console.log('(**) Unhandled rejection', e.reason, e.promise))
-
 // fetch() polyfill for making API calls.
 require('whatwg-fetch')
