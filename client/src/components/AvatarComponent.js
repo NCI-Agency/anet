97ef77e24ec03f025f0bf1a0991ed8ce265bc75f
José García <jose@west.nl>
2019-05-09 14:58:25 +0200

    NCI-Agency/anet#498: Removed unnecessary component options

--- client/src/components/AvatarComponent.js
+++ client/src/components/AvatarComponent.js
@@ -122,88 +122,92 @@ export default class AvatarComponent extends React.Component {
             image={this.state.image}
           />
         </div>
-        <br />
-        New File:
-        <input name="newImage" type="file" onChange={this.handleNewImage} />
-        <br />
-        Zoom:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleScale}
-          min={this.state.allowZoomOut ? '0.1' : '1'}
-          max="2"
-          step="0.01"
-          defaultValue="1"
-        />
-        <br />
-        {'Allow Scale < 1'}
-        <input
-          name="allowZoomOut"
-          type="checkbox"
-          onChange={this.handleAllowZoomOut}
-          checked={this.state.allowZoomOut}
-        />
-        <br />
-        Border radius:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleBorderRadius}
-          min="0"
-          max="100"
-          step="1"
-          defaultValue="0"
-        />
-        <br />
-        Avatar Width:
-        <input
-          name="width"
-          type="number"
-          onChange={this.handleWidth}
-          min="50"
-          max="400"
-          step="10"
-          value={this.state.width}
-        />
-        <br />
-        Avatar Height:
-        <input
-          name="height"
-          type="number"
-          onChange={this.handleHeight}
-          min="50"
-          max="400"
-          step="10"
-          value={this.state.height}
-        />
-        <br />
-        X Position:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleXPosition}
-          min="0"
-          max="1"
-          step="0.01"
-          value={this.state.position.x}
-        />
-        <br />
-        Y Position:
-        <input
-          name="scale"
-          type="range"
-          onChange={this.handleYPosition}
-          min="0"
-          max="1"
-          step="0.01"
-          value={this.state.position.y}
-        />
-        <br />
-        Rotate:
-        <button onClick={this.rotateLeft}>Left</button>
-        <button onClick={this.rotateRight}>Right</button>
-        <br />
+        <div>
+          <br />
+          New File:
+          <input name="newImage" type="file" onChange={this.handleNewImage} />
+          <br />
+          Zoom:
+          <input
+            name="scale"
+            type="range"
+            onChange={this.handleScale}
+            min={this.state.allowZoomOut ? '0.1' : '1'}
+            max="2"
+            step="0.01"
+            defaultValue="1"
+          />
+          <br />
+          {/*
+          {'Allow Scale < 1'}
+          <input
+            name="allowZoomOut"
+            type="checkbox"
+            onChange={this.handleAllowZoomOut}
+            checked={this.state.allowZoomOut}
+          />
+          <br />
+          Border radius:
+          <input
+            name="scale"
+            type="range"
+            onChange={this.handleBorderRadius}
+            min="0"
+            max="100"
+            step="1"
+            defaultValue="0"
+          />
+          <br />
+          Avatar Width:
+          <input
+            name="width"
+            type="number"
+            onChange={this.handleWidth}
+            min="50"
+            max="400"
+            step="10"
+            value={this.state.width}
+          />
+          <br />
+          Avatar Height:
+          <input
+            name="height"
+            type="number"
+            onChange={this.handleHeight}
+            min="50"
+            max="400"
+            step="10"
+            value={this.state.height}
+          />
+          <br />
+          X Position:
+          <input
+            name="scale"
+            type="range"
+            onChange={this.handleXPosition}
+            min="0"
+            max="1"
+            step="0.01"
+            value={this.state.position.x}
+          />
+          <br />
+          Y Position:
+          <input
+            name="scale"
+            type="range"
+            onChange={this.handleYPosition}
+            min="0"
+            max="1"
+            step="0.01"
+            value={this.state.position.y}
+          />
+          */}
+          <br />
+          Rotate:
+          <button onClick={this.rotateLeft}>Left</button>
+          <button onClick={this.rotateRight}>Right</button>
+          <br />
+        </div>
       </div>
     )
   }
