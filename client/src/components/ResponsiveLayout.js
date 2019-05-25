fd7d28d9f55ae6103f6b573c1a772d6b4e8ac86e
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2019-05-25 19:44:29 +0200

    nci-agency/anet#1347: Improve notes user interface use bootstrap wherever possible improve layout styling for messages

--- client/src/components/ResponsiveLayout.js
+++ client/src/components/ResponsiveLayout.js
@@ -31,8 +31,7 @@ const mainViewportContainer = {
 }
 const notesViewportContainer = {
   paddingTop: 18,
-  maxWidth: "33%",
-  overflow: "auto"
+  maxWidth: "35%"
 }
 const sidebarContainer = {
   position: "relative",
