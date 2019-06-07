2bc232b50592a01d92104ab3769d293e2c4b62e3
Mara Dragan <mara@west.nl>
2019-06-07 10:30:18 +0200

    NCI-Agency/anet#1723: UseReportCollectionContainer on MyReports page

--- client/src/pages/reports/MyReports.js
+++ client/src/pages/reports/MyReports.js
@@ -6,9 +6,8 @@ import Page, {
   mapDispatchToProps,
   propTypes as pagePropTypes
 } from "components/Page"
-import ReportCollection from "components/ReportCollection"
+import ReportCollectionContainer from "components/ReportCollectionContainer"
 import SubNav from "components/SubNav"
-import GQL from "graphqlapi"
 import { Person, Report } from "models"
 import PropTypes from "prop-types"
 import React from "react"
@@ -18,70 +17,34 @@ import { connect } from "react-redux"
 class BaseMyReports extends Page {
   static propTypes = {
     ...pagePropTypes,
-    pagination: PropTypes.object,
     currentUser: PropTypes.instanceOf(Person)
   }
 
   constructor(props) {
     super(props)
-
-    this.state = {
-      draft: null,
-      future: null,
-      pending: null,
-      approved: null,
-      published: null,
-      cancelled: null
+    this.sectionQueryParams = {
+      draft: {
+        state: [Report.STATE.DRAFT, Report.STATE.REJECTED]
+      },
+      future: {
+        state: [Report.STATE.FUTURE]
+      },
+      pending: {
+        state: [Report.STATE.PENDING_APPROVAL]
+      },
+      approved: {
+        state: [Report.STATE.APPROVED]
+      },
+      published: {
+        state: [Report.STATE.PUBLISHED]
+      },
+      cancelled: {
+        state: [Report.STATE.CANCELLED]
+      }
     }
-    this.partFuncs = {
-      draft: this.getPart.bind(this, "draft", [
-        Report.STATE.DRAFT,
-        Report.STATE.REJECTED
-      ]),
-      future: this.getPart.bind(this, "future", [Report.STATE.FUTURE]),
-      pending: this.getPart.bind(this, "pending", [
-        Report.STATE.PENDING_APPROVAL
-      ]),
-      approved: this.getPart.bind(this, "approved", [Report.STATE.APPROVED]),
-      published: this.getPart.bind(this, "published", [Report.STATE.PUBLISHED]),
-      cancelled: this.getPart.bind(this, "cancelled", [Report.STATE.CANCELLED])
-    }
-  }
-
-  @autobind
-  getPart(partName, state, authorUuid, pageNum = 0) {
-    const queryConstPart = {
-      pageSize: 10,
-      pageNum: pageNum,
-      authorUuid: authorUuid,
-      state: state
-    }
-    const query = Object.assign({}, this.getSearchQuery(), queryConstPart)
-    return new GQL.Part(/* GraphQL */ `
-      ${partName}: reportList(query: $${partName}Query) {
-        pageNum, pageSize, totalCount, list {
-          ${ReportCollection.GQL_REPORT_FIELDS}
-        }
-      }`).addVariable(partName + "Query", "ReportSearchQueryInput", query)
-  }
-
-  _approvalStepParts = props => {
-    const { currentUser, pagination } = props
-    const authorUuid = currentUser.uuid
-    return Object.keys(this.partFuncs).map(part => {
-      const goToPageNum = this.getPaginatedNum(pagination[part])
-      return this.partFuncs[part](authorUuid, goToPageNum)
-    })
-  }
-
-  fetchData(props) {
-    const { currentUser } = props
-    if (!currentUser || !currentUser.uuid) {
-      return
-    }
-    const parts = this._approvalStepParts(props)
-    return GQL.run(parts).then(approvalSteps =>
-      this.setState({ ...approvalSteps })
+    Object.keys(this.sectionQueryParams).forEach(
+      key =>
+        (this.sectionQueryParams[key].authorUuid = props.currentUser.uuid)
     )
   }
 
@@ -107,97 +70,43 @@ class BaseMyReports extends Page {
           </Nav>
         </SubNav>
 
-        {this.renderSection(
-          "Draft Reports",
-          this.state.draft,
-          this.goToPage.bind(this, "draft"),
-          "draft-reports",
-          "draft"
-        )}
+        {this.renderSection("Draft Reports", "draft-reports", "draft")}
         {this.renderSection(
           "Upcoming Engagements",
-          this.state.future,
-          this.goToPage.bind(this, "future"),
           "upcoming-engagements",
           "future"
         )}
-        {this.renderSection(
-          "Pending Approval",
-          this.state.pending,
-          this.goToPage.bind(this, "pending"),
-          "pending-approval",
-          "pending"
-        )}
-        {this.renderSection(
-          "Approved",
-          this.state.approved,
-          this.goToPage.bind(this, "approved"),
-          "approved",
-          "approved"
-        )}
+        {this.renderSection("Pending Approval", "pending-approval", "pending")}
+        {this.renderSection("Approved", "approved", "approved")}
         {this.renderSection(
           "Published Reports",
-          this.state.published,
-          this.goToPage.bind(this, "published"),
           "published-reports",
           "published"
         )}
         {this.renderSection(
           "Cancelled Reports",
-          this.state.cancelled,
-          this.goToPage.bind(this, "cancelled"),
           "cancelled-reports",
           "cancelled"
         )}
       </div>
     )
   }
-
-  getPaginatedNum = (part, pageNum = 0) => {
-    let goToPageNum = pageNum
-    if (part !== undefined) {
-      goToPageNum = part.pageNum
-    }
-    return goToPageNum
-  }
-
-  renderSection = (title, reports, goToPage, id, section) => {
-    const paginatedPart = this.props.pagination[section]
-    const goToPageNum = this.getPaginatedNum(paginatedPart)
-    let content = <p>Loading...</p>
-    if (reports && reports.list) {
-      const paginatedReports = Object.assign(reports, { pageNum: goToPageNum })
-      content = (
-        <ReportCollection
-          paginatedReports={paginatedReports}
-          goToPage={goToPage}
-          mapId={id}
-        />
-      )
-    }
-
+  renderSection = (title, id, section) => {
+    const queryParams = Object.assign(
+      {},
+      this.sectionQueryParams[section],
+      this.getSearchQuery()
+    )
     return (
       <Fieldset title={title} id={id}>
-        {content}
+        <ReportCollectionContainer queryParams={queryParams} mapId={id} />
       </Fieldset>
     )
-  }
-
-  @autobind
-  goToPage(section, pageNum) {
-    const { currentUser, setPagination } = this.props
-    const part = this.partFuncs[section](currentUser.uuid, pageNum)
-    GQL.run([part]).then(data => {
-      let stateChange = {}
-      stateChange[section] = data[section]
-      this.setState(stateChange, () => setPagination(section, pageNum))
-    })
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  searchQuery: state.searchQuery,
-  pagination: state.pagination
+  searchQuery: state.searchQuery
 })
 
 const MyReports = props => (
