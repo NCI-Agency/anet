a4a3f3ddd6d44b39b601de609b46bbb8154c3816
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2019-06-21 17:34:09 +0200

    nci-agency/anet#1723: Tweak fullcalendar's theme to match ANET's

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -7,6 +7,7 @@ import "@fullcalendar/core/main.css"
 import "@fullcalendar/daygrid/main.css"
 import "@fullcalendar/timegrid/main.css"
 import "@fullcalendar/list/main.css"
+import "./Calendar.css"
 
 import React, { Component } from "react"
 
