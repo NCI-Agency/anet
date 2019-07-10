5885f0f468e40918190ea3721fd4e1b151b5fc74
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2019-07-10 16:18:37 +0200

    nci-agency/anet#335: Add aap6 symbology
    Also, sort positions by person's rank

--- client/src/components/graphs/OrganizationalChart.js
+++ client/src/components/graphs/OrganizationalChart.js
@@ -1,7 +1,31 @@
-import API from "api"
+import API, { Settings } from "api"
 import SVGCanvas from "components/graphs/SVGCanvas"
 import * as d3 from "d3"
 import PropTypes from "prop-types"
+import { Symbol, getColorMode } from "milsymbol"
+import Organization from "../../models/Organization"
+
+const ranks = Settings.fields.person.ranks.map(rank => rank.value)
+
+const sortPositions = (positions, truncateLimit) =>
+  [...positions].sort((p1, p2) =>
+    ranks.indexOf(p1.person && p1.person.rank) <
+    ranks.indexOf(p2.person && p2.person.rank)
+      ? 1
+      : -1
+  )
+
+const rankToUnit = {
+  "OF-9": "K",
+  "OF-8": "J",
+  "OF-7": "I",
+  "OF-6": "H",
+  "OF-5": "G",
+  "OF-4": "F",
+  "OF-3": "E",
+  "OF-2": "E",
+  "OF-1": "E"
+}
 
 export default class OrganizationalChart extends SVGCanvas {
   static propTypes = {
@@ -51,7 +75,7 @@ export default class OrganizationalChart extends SVGCanvas {
 
     this.canvas.attr(
       "transform",
-      "translate(" + this.props.height / 2 + "," + 20 + ")"
+      "translate(" + this.props.height / 2 + "," + 50 + ")"
     )
 
     if (!this.state.root) return
@@ -72,7 +96,7 @@ export default class OrganizationalChart extends SVGCanvas {
       .enter()
       .append("path")
       .attr("class", "link")
-      .attr("stroke-opacity"," 0.3")
+      .attr("stroke-opacity", " 0.3")
       .attr(
         "d",
         d3
@@ -86,46 +110,58 @@ export default class OrganizationalChart extends SVGCanvas {
       .data(root.descendants())
       .enter()
       .append("g")
-      .attr(
-        "class",
-        d => `node ${d.children ? "node--internal" : "node--leaf"}`
-      )
       .attr("transform", d => `translate(${d.x},${d.y})`)
 
-    node.append("circle").attr("r", 2.5)
+    const iconNodeG = node.append("g").attr("transform", "translate(-25,-25)")
 
-    node
+    iconNodeG.each(function(d) {
+      const positions = sortPositions(d.data.positions)
+      console.log(positions[0])
+      const unitcode =
+        rankToUnit[positions.length > 0 && positions[0].person.rank]
+      const sym = new Symbol(
+        `S${
+          d.data.type === Organization.TYPE.ADVISOR_ORG ? "F" : "N"
+        }GPU------${unitcode || "-"}`,
+        { size: 32 }
+      )
+      this.appendChild(sym.asDOM())
+    })
+
+    iconNodeG
       .append("a")
       .attr("href", d => `/organizations/${d.data.uuid}`)
       .append("text")
-      .attr("font-family","monospace")
-      .attr("dy", 3)
+      .attr("font-family", "monospace")
+      .attr("dy", -3)
       .attr("x", 8)
       .style("text-anchor", "start")
       .text(d => {
-        const result = `${d.data.shortName} (${d.data.longName})`
-        return result.length > 26 ? result.substring(0, 23) + "..." : result })
+        const result = `${d.data.shortName}${d.data.longName ? " - " : ""}${
+          d.data.longName
+        }`
+        return result.length > 26 ? result.substring(0, 23) + "..." : result
+      })
 
-    const ggg = node.append("g")
-    ggg
+    const positionsG = node.append("g")
+    positionsG
       .selectAll("a")
-      .data(d => d.data.positions || [])
+      .data(d => sortPositions(d.data.positions) || [])
       .enter()
       .append("a")
       .attr("href", d => `/positions/${d.uuid}`)
       .append("text")
       .attr("font-size", "9px")
-      .attr("font-family","monospace")
-      .attr("dy", (d, i) => 20 + i * 11)
-      .attr("x", 8)
+      .attr("font-family", "monospace")
+      .attr("dy", (d, i) => 5 + i * 11)
+      .attr("x", 28)
       .style("text-anchor", "start")
-      .text(
-        d => {
-          const result = `${d.person ? d.person.rank : ""} ${
-            d.person ? d.person.name : "unfilled"
-          }@${d.name}`
-          return result.length > 45 ? result.substring(0, 42) + "..." : result })
-
+      .text(d => {
+        const result = `${d.person ? d.person.rank : ""} ${
+          d.person ? d.person.name : "unfilled"
+        }@${d.name}`
+        return result.length > 45 ? result.substring(0, 42) + "..." : result
+      })
   }
 
   fetchData() {
@@ -137,6 +173,7 @@ export default class OrganizationalChart extends SVGCanvas {
             uuid
             shortName
             longName
+            type
             positions{
                 name
                 uuid
@@ -154,6 +191,7 @@ export default class OrganizationalChart extends SVGCanvas {
               uuid
               shortName
               longName
+              type
               childrenOrgs(query: {pageNum: 0, pageSize: 0}) {
                 uuid
               }
