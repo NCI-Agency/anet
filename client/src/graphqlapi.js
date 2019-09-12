NCI-Agency/anet

commit e16739d04d8858d8085b6d5d43d68bcfde9f37af

Use pre-parsed GQL for all calls to API.query

client/src/graphqlapi.js
@@ -1,4 +1,5 @@
 import API from "api"
+import { gql } from "apollo-boost"
 
 class GraphQLPart {
   constructor(queryString) {
@@ -19,10 +20,9 @@ class GraphQLPart {
 
 const GQL = {
   // Pass a variable number of GraphQLQuery to run
-  _runCommon(parts, apiCall, output) {
-    let query = parts.map(p => p.queryString).join(",\n")
+  _combine(parts) {
+    const query = parts.map(p => p.queryString).join("\n")
     let variables = {}
-    let variableDef
     let variableDefs = []
     parts.forEach(part => {
       part.variables.forEach(variable => {
@@ -30,19 +30,25 @@ const GQL = {
         variableDefs.push(`$${variable.name}: ${variable.type}`)
       })
     })
-    if (variableDefs.length) {
-      variableDef = "(" + variableDefs.join(", ") + ")"
-    }
-
-    return apiCall(query, variables, variableDef, output)
+    const variableDef = variableDefs.length
+      ? "(" + variableDefs.join(", ") + ")"
+      : ""
+    return { query, variables, variableDef }
   },
 
   run(parts) {
-    return this._runCommon(parts, API.query)
+    const { query, variables, variableDef } = this._combine(parts)
+    const graphql = gql`
+      query ${variableDef} {
+        ${query}
+      }
+    `
+    return API.query(graphql, variables)
   },
 
   runExport(parts, output) {
-    return this._runCommon(parts, API.queryExport, output)
+    const { query, variables } = this._combine(parts)
+    return API.queryExport(query, variables, output)
   },
 
   Part: GraphQLPart

client/src/baseAPI.js
@@ -1,5 +1,5 @@
 import querystring from "querystring"
-import ApolloClient, { gql } from "apollo-boost"
+import ApolloClient from "apollo-boost"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import _isEmpty from "lodash/isEmpty"
 
@@ -59,20 +59,13 @@ const BaseAPI = {
     return window.fetch(url, params)
   },
 
-  _buildGraphQl(query, variables, variableDef, isMutation) {
-    variables = variables || {}
-    variableDef = variableDef || ""
-    query = `${isMutation ? "mutation" : "query"} ${variableDef} { ${query} }`
-    return { query, variables }
-  },
-
-  queryExport(query, variables, variableDef, output) {
-    const data = BaseAPI._buildGraphQl(query, variables, variableDef)
-    data.output = output
+  queryExport(query, variables, output) {
     // Can't use client here as the response is not JSON
-    return BaseAPI._fetch(GRAPHQL_ENDPOINT, data, "*/*").then(response =>
-      response.blob()
-    )
+    return BaseAPI._fetch(
+      GRAPHQL_ENDPOINT,
+      { query, variables, output },
+      "*/*"
+    ).then(response => response.blob())
   },
 
   /**
@@ -134,14 +127,8 @@ const BaseAPI = {
   },
 
   query(query, variables, variableDef, params) {
-    const graphQl = BaseAPI._buildGraphQl(query, variables, variableDef)
     return client
-      .query({
-        query: gql`
-          ${graphQl.query}
-        `,
-        variables: graphQl.variables
-      })
+      .query({ query, variables })
       .then(BaseAPI._handleSuccess)
       .catch(BaseAPI._handleError)
   },

client/src/components/AdvisorReports/AdvisorReportsModal.js
@@ -1,8 +1,23 @@
 import API from "api"
+import { gql } from "apollo-boost"
 import AdvisorReportsTable from "components/AdvisorReports/AdvisorReportsTable"
 import SimpleModal from "components/SimpleModal"
 import PropTypes from "prop-types"
 import React, { Component } from "react"
+
+const GQL_GET_ADVISOR_REPORTS_INSIGHT = gql`
+  query($orgUuid: String!) {
+    advisorReportInsights(orgUuid: $orgUuid) {
+      uuid
+      name
+      stats {
+        week
+        nrReportsSubmitted
+        nrEngagementsAttended
+      }
+    }
+  }
+`
 
 class AdvisorReportsModal extends Component {
   constructor(props) {
@@ -18,21 +33,7 @@ class AdvisorReportsModal extends Component {
   }
 
   fetchAdvisors(orgUuid) {
-    API.query(
-      /* GraphQL */ `
-        advisorReportInsights(orgUuid: $orgUuid) {
-          uuid
-          name
-          stats {
-            week
-            nrReportsSubmitted
-            nrEngagementsAttended
-          }
-        }
-      `,
-      { orgUuid: orgUuid },
-      "($orgUuid: String!)"
-    ).then(data => {
+    API.query(GQL_GET_ADVISOR_REPORTS_INSIGHT, { orgUuid }).then(data => {
       this.setState({
         advisors: data.advisorReportInsights
       })

client/src/components/AdvisorReports/FilterableAdvisorReportsTable.js
@@ -1,4 +1,5 @@
 import API from "api"
+import { gql } from "apollo-boost"
 import OrganizationAdvisorsTable from "components/AdvisorReports/OrganizationAdvisorsTable"
 import Toolbar from "components/AdvisorReports/Toolbar"
 import LoaderHOC, { mapDispatchToProps } from "HOC/LoaderHOC"
@@ -7,6 +8,20 @@ import moment from "moment"
 import PropTypes from "prop-types"
 import React, { Component } from "react"
 import { connect } from "react-redux"
+
+const GQL_GET_ADVISOR_REPORTS_INSIGHT = gql`
+  query {
+    advisorReportInsights {
+      uuid
+      name
+      stats {
+        week
+        nrReportsSubmitted
+        nrEngagementsAttended
+      }
+    }
+  }
+`
 
 const DEFAULT_WEEKS_AGO = 3
 const OrganizationAdvisorsTableWithLoader = connect(
@@ -37,19 +52,7 @@ class FilterableAdvisorReportsTable extends Component {
   componentDidMount() {
     this.setState({ isLoading: true })
     this.props.showLoading()
-    API.query(
-      /* GraphQL */ `
-         advisorReportInsights {
-           uuid
-           name
-           stats {
-             week
-             nrReportsSubmitted
-             nrEngagementsAttended
-           }
-         }
-       `
-    ).then(data => {
+    API.query(GQL_GET_ADVISOR_REPORTS_INSIGHT).then(data => {
       this.setState({
         isLoading: false,
         data: data.advisorReportInsights

client/src/components/AssignPersonModal.js
@@ -62,7 +62,6 @@ export default class AssignPersonModal extends Component {
     const personFilters = {
       allPersons: {
         label: "All",
-        searchQuery: true,
         queryVars: personSearchQuery
       }
     }

client/src/components/AssignPositionModal.js
@@ -76,7 +76,6 @@ class BaseAssignPositionModal extends Component {
     const positionsFilters = {
       allAdvisorPositions: {
         label: "All",
-        searchQuery: true,
         queryVars: positionSearchQuery
       }
     }

client/src/components/EditAssociatedPositionsModal.js
@@ -105,7 +105,6 @@ class BaseEditAssociatedPositionsModal extends Component {
     const positionsFilters = {
       allAdvisorPositions: {
         label: "All",
-        searchQuery: true,
         queryVars: positionSearchQuery
       }
     }

client/src/components/ReportCollectionContainer.js
@@ -1,20 +1,36 @@
 import { setPagination } from "actions"
-import PropTypes from "prop-types"
-import React, { Component } from "react"
+import API from "api"
+import { gql } from "apollo-boost"
 import ReportCollection, {
   GQL_REPORT_FIELDS,
   GQL_BASIC_REPORT_FIELDS
 } from "components/ReportCollection"
-
-import API from "api"
-import { connect } from "react-redux"
 import _isEqualWith from "lodash/isEqualWith"
+import PropTypes from "prop-types"
+import React, { Component } from "react"
+import { connect } from "react-redux"
 import utils from "utils"
 
 export const FORMAT_CALENDAR = "calendar"
 export const FORMAT_SUMMARY = "summary"
 export const FORMAT_TABLE = "table"
 export const FORMAT_MAP = "map"
+
+const GQL_GET_REPORT_LIST = gql`
+  query($reportsQueryParams: ReportSearchQueryInput, $includeAll: Boolean!) {
+    reportList(query: $reportsQueryParams) {
+      pageNum
+      pageSize
+      totalCount
+      list @include(if: $includeAll) {
+        ${GQL_BASIC_REPORT_FIELDS}
+      }
+      list @skip(if: $includeAll) {
+        ${GQL_REPORT_FIELDS}
+      }
+    }
+  }
+`
 
 class ReportCollectionContainer extends Component {
   static propTypes = {
@@ -68,38 +84,19 @@ class ReportCollectionContainer extends Component {
     return reportsQueryParams
   }
 
-  getReportsQuery = (reportsQueryParams, reportFields) => {
-    return API.query(
-      /* GraphQL */ `
-        reportList(query: $reportsQueryParams) {
-          pageNum
-          pageSize
-          totalCount
-          list {
-            ${reportFields}
-          }
-        }
-      `,
-      { reportsQueryParams },
-      "($reportsQueryParams: ReportSearchQueryInput)"
-    )
+  getReportsQuery = (reportsQueryParams, includeAll) => {
+    return API.query(GQL_GET_REPORT_LIST, { reportsQueryParams, includeAll })
   }
 
   fetchReportData(includeAll, pageNum) {
     // Query used by the paginated views
     const queries = [
-      this.getReportsQuery(
-        this.reportsQueryParams(true, pageNum),
-        GQL_REPORT_FIELDS
-      )
+      this.getReportsQuery(this.reportsQueryParams(true, pageNum), false)
     ]
     if (includeAll) {
       // Query used by the map and calendar views
       queries.push(
-        this.getReportsQuery(
-          this.reportsQueryParams(false),
-          GQL_BASIC_REPORT_FIELDS
-        )
+        this.getReportsQuery(this.reportsQueryParams(false), includeAll)
       )
     }
     return Promise.all(queries).then(values => {

commit 9bc5d5b1417011d48bc15f9d1697bdd1218ed765

Fix search results export
Was broken in e16739d04d8858d8085b6d5d43d68bcfde9f37af

client/src/graphqlapi.js
@@ -47,8 +47,13 @@ const GQL = {
   },
 
   runExport(parts, output) {
-    const { query, variables } = this._combine(parts)
-    return API.queryExport(query, variables, output)
+    const { query, variables, variableDef } = this._combine(parts)
+    const graphql = `
+      query ${variableDef} {
+        ${query}
+      }
+    `
+    return API.queryExport(graphql, variables, output)
   },
 
   Part: GraphQLPart