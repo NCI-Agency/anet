292f0c65efa66dda2018ecebf21677e41c947894
Vassil <vassil.iordanov@ncia.nato.int>
2019-10-06 16:16:05 +0200

    Add explicit cache-control to all responses and assets

--- src/main/java/mil/dds/anet/views/ViewResponseFilter.java
+++ src/main/java/mil/dds/anet/views/ViewResponseFilter.java
@@ -1,6 +1,7 @@
 package mil.dds.anet.views;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.net.HttpHeaders;
 import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -20,9 +21,12 @@ public class ViewResponseFilter implements ContainerResponseFilter {
   public void filter(ContainerRequestContext requestContext,
       ContainerResponseContext responseContext) throws IOException {
     if (MediaType.APPLICATION_JSON_TYPE.equals(responseContext.getMediaType())) {
-      responseContext.getHeaders().put("Cache-Control",
+      responseContext.getHeaders().put(HttpHeaders.CACHE_CONTROL,
           ImmutableList.of("no-store, no-cache, must-revalidate, post-check=0, pre-check=0"));
-      responseContext.getHeaders().put("Pragma", ImmutableList.of("no-cache"));
+      responseContext.getHeaders().put(HttpHeaders.PRAGMA, ImmutableList.of("no-cache"));
+    } else {
+      responseContext.getHeaders().put(HttpHeaders.CACHE_CONTROL,
+          ImmutableList.of("max-age=259200, public"));
     }
   }
 
