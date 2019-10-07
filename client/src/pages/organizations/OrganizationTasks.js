99abb7eb2d0106c000c16201554d257eee563cc3
José García <jose@west.nl>
2019-10-07 12:01:29 +0200

    NCI-Agency/anet#2293: Add pagination to the bottom of lists
    - Also added it to OrganizationTasks

--- client/src/pages/organizations/OrganizationTasks.js
+++ client/src/pages/organizations/OrganizationTasks.js
@@ -4,13 +4,14 @@ import AppContext from "components/AppContext"
 import Fieldset from "components/Fieldset"
 import LinkTo from "components/LinkTo"
 import { mapDispatchToProps, useBoilerplate } from "components/Page"
-import UltimatePagination from "components/UltimatePagination"
+import UltimatePaginationTopDown from "components/UltimatePaginationTopDown"
 import { Person, Task } from "models"
 import pluralize from "pluralize"
 import PropTypes from "prop-types"
 import React, { useState } from "react"
 import { Table } from "react-bootstrap"
 import { connect } from "react-redux"
+import _get from "lodash/get"
 
 const GQL_GET_TASK_LIST = gql`
   query($taskQuery: TaskSearchQueryInput) {
@@ -55,6 +56,34 @@ const BaseOrganizationTasks = props => {
 
   const { pageSize, totalCount } = paginatedTasks
 
+  if (_get(tasks, "length", 0) === 0) {
+    return (
+      <em>This organization doesn't have any {pluralize(taskShortLabel)}</em>
+    )
+  }
+
+  const tableElement = (
+    <Table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {Task.map(tasks, (task, idx) => (
+          <tr key={task.uuid} id={`task_${idx}`}>
+            <td>
+              <LinkTo task={task}>{task.shortName}</LinkTo>
+            </td>
+            <td>{task.longName}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+
   return (
     <Fieldset
       id="tasks"
@@ -70,38 +99,15 @@ const BaseOrganizationTasks = props => {
         )
       }
     >
-      <UltimatePagination
-        Component="header"
+      <UltimatePaginationTopDown
         componentClassName="searchPagination"
         className="pull-right"
         pageNum={pageNum}
         pageSize={pageSize}
         totalCount={totalCount}
         goToPage={setPageNum}
+        contentElement={tableElement}
       />
-      <Table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          {Task.map(tasks, (task, idx) => (
-            <tr key={task.uuid} id={`task_${idx}`}>
-              <td>
-                <LinkTo task={task}>{task.shortName}</LinkTo>
-              </td>
-              <td>{task.longName}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-
-      {tasks.length === 0 && (
-        <em>This organization doesn't have any {pluralize(taskShortLabel)}</em>
-      )}
     </Fieldset>
   )
 }
