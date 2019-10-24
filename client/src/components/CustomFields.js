9d4bea435cbdfd79d19d5667e81b5ebd1a3048de
Mara Dragan <mara@west.nl>
2019-10-24 15:35:57 +0200

    NCI-Agency/anet#2352: Reduce some code duplication
    Move common props definition to the FieldComponent instantiation.

--- client/src/components/CustomFields.js
+++ client/src/components/CustomFields.js
@@ -5,54 +5,40 @@ import PropTypes from "prop-types"
 import React from "react"
 import { HelpBlock } from "react-bootstrap"
 
-const customFieldPropTypes = {
-  name: PropTypes.string,
-  fieldConfig: PropTypes.object,
-  formikProps: PropTypes.object
+export const TextField = fieldProps => {
+  const { onChange, onBlur, ...otherFieldProps } = fieldProps
+  return (
+    <Field
+      type="text"
+      component={FieldHelper.renderInputField}
+      {...otherFieldProps}
+    />
+  )
 }
 
-export const TextField = ({ name, fieldConfig, formikProps }) => (
-  <Field
-    name={name}
-    value={formikProps.values[name]}
-    type="text"
-    component={FieldHelper.renderInputField}
-    {...fieldConfig}
-  />
-)
-TextField.propTypes = customFieldPropTypes
-
-export const DateField = ({ name, fieldConfig, formikProps, withTime }) => (
-  <Field
-    name={name}
-    value={formikProps.values[name]}
-    component={FieldHelper.renderSpecialField}
-    onChange={value => formikProps.setFieldValue(name, value)}
-    onBlur={() => formikProps.setFieldTouched(name, true)}
-    widget={<CustomDateInput withTime={withTime} />}
-    {...fieldConfig}
-  />
-)
-DateField.propTypes = {
-  ...customFieldPropTypes,
-  withTime: PropTypes.bool
+export const DateField = fieldProps => {
+  const { withTime, ...otherFieldProps } = fieldProps
+  return (
+    <Field
+      component={FieldHelper.renderSpecialField}
+      widget={<CustomDateInput withTime={withTime} />}
+      {...otherFieldProps}
+    />
+  )
 }
 
 export const DateTimeField = props => <DateField {...props} withTime />
 
-export const ButtonToggleGroupField = ({ name, fieldConfig, formikProps }) => {
-  const { choices, ...restFieldConfig } = fieldConfig
+export const ButtonToggleGroupField = fieldProps => {
+  const { choices, ...otherFieldProps } = fieldProps
   return (
     <Field
-      name={name}
       buttons={FieldHelper.customEnumButtons(choices)}
       component={FieldHelper.renderButtonToggleGroup}
-      onChange={value => formikProps.setFieldValue(name, value)}
-      {...restFieldConfig}
+      {...otherFieldProps}
     />
   )
 }
-ButtonToggleGroupField.propTypes = customFieldPropTypes
 
 const FIELD_COMPONENTS = {
   text: TextField,
@@ -63,15 +49,17 @@ const FIELD_COMPONENTS = {
 
 const CustomFields = ({ fieldsConfig, formikProps }) => {
   return Object.keys(fieldsConfig).map(key => {
-    const config = fieldsConfig[key]
-    const { type, helpText, ...fieldConfig } = config
+    const fieldConfig = fieldsConfig[key]
+    const { type, helpText, ...fieldProps } = fieldConfig
     const FieldComponent = FIELD_COMPONENTS[type]
     return (
       <FieldComponent
         key={key}
         name={key}
-        fieldConfig={fieldConfig}
-        formikProps={formikProps}
+        value={formikProps.values[key]}
+        onChange={value => formikProps.setFieldValue(key, value)}
+        onBlur={() => formikProps.setFieldTouched(key, true)}
+        {...fieldProps}
       >
         {helpText && (
           <HelpBlock>
