b685f4364c73969929a7edbd1e838bc416d7cf25
Mara Dragan <mara@west.nl>
2019-10-30 14:49:16 +0100

    NCI-Agency/anet#2447: Use FastField in person form
    In the person form we are now using the FastField component instead of
    Field for fields which do not depend on other fields values. This
    results in those fields not being rerendered on any form change and this
    makes filling the fields less slow.

--- client/src/pages/people/Form.js
+++ client/src/pages/people/Form.js
@@ -12,7 +12,7 @@ import { jumpToTop } from "components/Page"
 import RichTextEditor from "components/RichTextEditor"
 import TriggerableConfirm from "components/TriggerableConfirm"
 import AvatarEditModal from "components/AvatarEditModal"
-import { Field, Form, Formik } from "formik"
+import { FastField, Field, Form, Formik } from "formik"
 import _isEmpty from "lodash/isEmpty"
 import { Person } from "models"
 import pluralize from "pluralize"
@@ -199,7 +199,7 @@ const BasePersonForm = props => {
                   </Col>
                   <Col sm={7}>
                     <Col sm={5}>
-                      <Field
+                      <FastField
                         name="lastName"
                         component={FieldHelper.renderInputFieldNoLabel}
                         display="inline"
@@ -212,7 +212,7 @@ const BasePersonForm = props => {
                       ,
                     </Col>
                     <Col sm={6}>
-                      <Field
+                      <FastField
                         name="firstName"
                         component={FieldHelper.renderInputFieldNoLabel}
                         display="inline"
@@ -302,7 +302,7 @@ const BasePersonForm = props => {
                 </FormGroup>
 
                 {isAdmin && (
-                  <Field
+                  <FastField
                     name="domainUsername"
                     component={FieldHelper.renderInputField}
                     extraColElem={
@@ -315,13 +315,13 @@ const BasePersonForm = props => {
                 )}
 
                 {edit ? (
-                  <Field
+                  <FastField
                     name="role"
                     component={FieldHelper.renderReadonlyField}
                     humanValue={Person.humanNameOfRole(values.role)}
                   />
                 ) : (
-                  <Field
+                  <FastField
                     name="role"
                     component={FieldHelper.renderButtonToggleGroup}
                     buttons={roleButtons}
@@ -345,17 +345,17 @@ const BasePersonForm = props => {
                         contact an ANET administrator.
                       </Alert>
                     )}
-                  </Field>
+                  </FastField>
                 )}
 
                 {disableStatusChange ? (
-                  <Field
+                  <FastField
                     name="status"
                     component={FieldHelper.renderReadonlyField}
                     humanValue={Person.humanNameOfStatus(values.status)}
                   />
                 ) : isNewUser ? (
-                  <Field
+                  <FastField
                     name="status"
                     component={FieldHelper.renderReadonlyField}
                     humanValue={Person.humanNameOfStatus(values.status)}
@@ -392,23 +392,23 @@ const BasePersonForm = props => {
               </Fieldset>
 
               <Fieldset title="Additional information">
-                <Field
+                <FastField
                   name="emailAddress"
                   label={Settings.fields.person.emailAddress}
                   type="email"
                   component={FieldHelper.renderInputField}
                 />
-                <Field
+                <FastField
                   name="phoneNumber"
                   label={Settings.fields.person.phoneNumber}
                   component={FieldHelper.renderInputField}
                 />
-                <Field
+                <FastField
                   name="rank"
                   label={Settings.fields.person.rank}
                   component={FieldHelper.renderSpecialField}
                   widget={
-                    <Field component="select" className="form-control">
+                    <FastField component="select" className="form-control">
                       <option />
                       {ranks.map(rank => (
                         <option key={rank.value} value={rank.value}>
@@ -416,37 +416,37 @@ const BasePersonForm = props => {
                           {rank.description && ` - ( ${rank.description} )`}
                         </option>
                       ))}
-                    </Field>
+                    </FastField>
                   }
                 />
-                <Field
+                <FastField
                   name="gender"
                   label={Settings.fields.person.gender}
                   component={FieldHelper.renderSpecialField}
                   widget={
-                    <Field component="select" className="form-control">
+                    <FastField component="select" className="form-control">
                       <option />
                       <option value="MALE">Male</option>
                       <option value="FEMALE">Female</option>
-                    </Field>
+                    </FastField>
                   }
                 />
-                <Field
+                <FastField
                   name="country"
                   label={Settings.fields.person.country}
                   component={FieldHelper.renderSpecialField}
                   widget={
-                    <Field component="select" className="form-control">
+                    <FastField component="select" className="form-control">
                       <option />
                       {countries.map(country => (
                         <option key={country} value={country}>
                           {country}
                         </option>
                       ))}
-                    </Field>
+                    </FastField>
                   }
                 />
-                <Field
+                <FastField
                   name="endOfTourDate"
                   label={Settings.fields.person.endOfTourDate}
                   component={FieldHelper.renderSpecialField}
@@ -460,8 +460,8 @@ const BasePersonForm = props => {
                       Be aware that the end of tour date is in the past.
                     </Alert>
                   )}
-                </Field>
-                <Field
+                </FastField>
+                <FastField
                   name="biography"
                   component={FieldHelper.renderSpecialField}
                   onChange={value => setFieldValue("biography", value)}
