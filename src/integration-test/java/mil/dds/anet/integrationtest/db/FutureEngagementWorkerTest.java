db2613f11d30381dc53bf90d519837d0e27ccace
José García <jose@west.nl>
2019-11-19 10:02:07 +0100

    Apply minor reformating

--- src/integration-test/java/mil/dds/anet/integrationtest/db/FutureEngagementWorkerTest.java
+++ src/integration-test/java/mil/dds/anet/integrationtest/db/FutureEngagementWorkerTest.java
@@ -1,6 +1,7 @@
 package mil.dds.anet.integrationtest.db;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import com.google.common.collect.ImmutableList;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import java.io.IOException;
