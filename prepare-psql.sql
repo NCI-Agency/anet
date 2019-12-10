853c140a9d2b4c40d144708f93f9f76aa8019a8e
Gertjan van Oosten <gertjan@west.nl>
2019-12-10 11:53:13 +0100

    Move create/delete of PostgreSQL text search configuration to migrations
    So repeatedly running dbMigrate works, and dbRollback properly rolls back.

--- prepare-psql.sql
+++ prepare-psql.sql
@@ -3,9 +3,6 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- make sure the preconditions for full-text search are met
 CREATE EXTENSION IF NOT EXISTS unaccent;
-DROP TEXT SEARCH CONFIGURATION IF EXISTS anet;
-CREATE TEXT SEARCH CONFIGURATION anet ( COPY = pg_catalog.english );
-ALTER TEXT SEARCH CONFIGURATION anet ALTER MAPPING FOR hword, hword_part, word WITH unaccent, english_stem;
 
 CREATE OR REPLACE AGGREGATE tsvector_agg (tsvector) (
   SFUNC = tsvector_concat,
