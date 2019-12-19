bb013efeba006190f232aa6b4aa87151740c16d0
Gertjan van Oosten <gertjan@west.nl>
2019-12-19 11:20:16 +0100

    Remove synthetic Report::user field
    It is obsolete since we can get the user from the context.

--- src/main/java/mil/dds/anet/search/AbstractReportSearcher.java
+++ src/main/java/mil/dds/anet/search/AbstractReportSearcher.java
@@ -42,12 +42,7 @@ public abstract class AbstractReportSearcher extends AbstractSearcher<Report, Re
     outerQb.addSqlArgs(qb.getSqlArgs());
     outerQb.addListArgs(qb.getListArgs());
     addOrderByClauses(outerQb, query);
-    final AnetBeanList<Report> result =
-        outerQb.buildAndRun(getDbHandle(), query, new ReportMapper());
-    for (final Report report : result.getList()) {
-      report.setUser(query.getUser());
-    }
-    return result;
+    return outerQb.buildAndRun(getDbHandle(), query, new ReportMapper());
   }
 
   protected void buildQuery(ReportSearchQuery query) {
