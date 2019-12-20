22d1cf39e3a51962873441f79324e201bbe0f05e
Edwin van den Houdt <edwin@vdhoudt.nl>
2019-12-20 12:14:42 +0100

    NCI-Agency/Anet#GH-1810: FIx lint error

--- client/src/HOC/LoaderHOC.js
+++ client/src/HOC/LoaderHOC.js
@@ -11,10 +11,7 @@ export const mapDispatchToProps = (dispatch, ownProps) => ({
 const LoaderHOC = isLoading => dataPropName => WrappedComponent => {
   return class LoaderHOC extends Component {
     static propTypes = {
-      loaderMessage: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.object
-      ])
+      loaderMessage: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
     }
 
     isLoadingData(prop) {
