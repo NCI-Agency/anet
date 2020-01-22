e87870ee0f59c44599bab9fb7fb69adff2e67779
José García <jose@west.nl>
2020-01-22 12:18:09 +0100

    Cleanup code

--- client/src/components/editor/LinkSourceAnet.js
+++ client/src/components/editor/LinkSourceAnet.js
@@ -199,8 +199,10 @@ class LinkSourceAnet extends Component {
             onChange={this.changeObjectType}
           >
             {Object.values(ENTITY_TYPES).map(entityType => {
-              const entityName = entityType === "anetLocation" ? "location" : entityType
-              const capitalized = entityName[0].toUpperCase() + entityName.slice(1)
+              const entityName =
+                entityType === "anetLocation" ? "location" : entityType
+              const capitalized =
+                entityName[0].toUpperCase() + entityName.slice(1)
               return (
                 <Button key={entityType} value={entityType}>
                   {pluralize(capitalized)}
