232f9e11813026b81263b5ab7344e0fd3e7078ab
Gertjan van Oosten <gertjan@west.nl>
2020-01-23 09:30:28 +0100

    Order imports

--- client/src/components/graphs/Pie.js
+++ client/src/components/graphs/Pie.js
@@ -1,7 +1,7 @@
-import React, { useRef, useEffect } from "react"
-import PropTypes from "prop-types"
 import SVGCanvas from "components/graphs/SVGCanvas"
 import * as d3 from "d3"
+import PropTypes from "prop-types"
+import React, { useEffect, useRef } from "react"
 
 const Pie = ({ label, segmentFill, segmentLabel, data, width, height }) => {
   const canvasRef = useRef(null)
