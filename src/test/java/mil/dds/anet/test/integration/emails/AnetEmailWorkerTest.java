69c1173ff1c9172fadba306903f0100b89bd726c
José García <jose@west.nl>
2020-02-18 12:20:41 +0100

    Fix test configuration
    - Removed pending FIXME
    - Moved test to JUnit5
    - Removed mocked configuration, replaced it with actual config from `anet.yml

--- src/test/java/mil/dds/anet/test/integration/emails/AnetEmailWorkerTest.java
+++ src/test/java/mil/dds/anet/test/integration/emails/AnetEmailWorkerTest.java
@@ -1,92 +1,74 @@
 package mil.dds.anet.test.integration.emails;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
-import mil.dds.anet.AnetObjectEngine;
+import mil.dds.anet.AnetApplication;
 import mil.dds.anet.beans.AnetEmail;
 import mil.dds.anet.config.AnetConfiguration;
-import mil.dds.anet.config.AnetConfiguration.SmtpConfiguration;
 import mil.dds.anet.database.EmailDao;
 import mil.dds.anet.test.integration.config.AnetTestConfiguration;
 import mil.dds.anet.test.integration.utils.EmailResponse;
 import mil.dds.anet.test.integration.utils.FakeSmtpServer;
 import mil.dds.anet.threads.AnetEmailWorker;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({EmailDao.class, AnetConfiguration.class, ScheduledExecutorService.class,
-    AnetObjectEngine.class})
+@ExtendWith(DropwizardExtensionsSupport.class)
 public class AnetEmailWorkerTest {
 
-  private AnetEmailWorker emailWorker;
-  private EmailDao emailDao;
-  private FakeSmtpServer emailServer;
-  private AnetObjectEngine instance;
+  private static AnetEmailWorker emailWorker;
+  private static EmailDao emailDao;
+  private static FakeSmtpServer emailServer;
+
+  private static final DropwizardAppExtension<AnetConfiguration> app =
+      new DropwizardAppExtension<AnetConfiguration>(AnetApplication.class, "anet.yml");
 
   /**
    * Sets up the test.
    * 
    * @throws Exception If the setup fails
    */
-  @Before
-  public void setUp() throws Exception {
+  @BeforeAll
+  public static void setUp() throws Exception {
     assumeTrue(Boolean.parseBoolean(
         AnetTestConfiguration.getConfiguration().get("emailServerTestsExecute").toString()));
 
-    emailDao = PowerMockito.mock(EmailDao.class, Mockito.RETURNS_MOCKS);
+    emailDao = mock(EmailDao.class, Mockito.RETURNS_DEEP_STUBS);
 
-    final AnetConfiguration config =
-        PowerMockito.mock(AnetConfiguration.class, Mockito.RETURNS_MOCKS);
     final ScheduledExecutorService scheduler =
-        PowerMockito.mock(ScheduledExecutorService.class, Mockito.RETURNS_MOCKS);
+        mock(ScheduledExecutorService.class, Mockito.RETURNS_DEEP_STUBS);
 
     // Configuration
-    when(config.getDictionaryEntry("SUPPORT_EMAIL_ADDR")).thenReturn("support@example.com");
-    when(config.getDictionaryEntry("dateFormats.email.date")).thenReturn("d MMMM yyyy");
-    when(config.getDictionaryEntry("dateFormats.email.withTime")).thenReturn("d MMMM yyyy @ HH:mm");
-    when(config.getDictionaryEntry("engagementsIncludeTimeAndDuration")).thenReturn(true);
-    when(config.getDictionaryEntry("activeDomainNames")).thenReturn(Arrays.asList("anet.com"));
-    when(config.getDictionaryEntry("fields")).thenReturn(new HashMap<String, Object>());
+    app.getConfiguration().getDictionary().put("SUPPORT_EMAIL_ADDR", "support@example.com");
+    app.getConfiguration().getDictionary().put("dateFormats.email.date", "d MMMM yyyy");
+    app.getConfiguration().getDictionary().put("dateFormats.email.withTime", "d MMMM yyyy @ HH:mm");
+    app.getConfiguration().getDictionary().put("engagementsIncludeTimeAndDuration", true);
+    app.getConfiguration().getDictionary().put("activeDomainNames", Arrays.asList("anet.com"));
+    app.getConfiguration().getDictionary().put("fields", new HashMap<String, Object>());
+    app.getConfiguration().setEmailFromAddr("test_from_address@anet.com");
 
-    when(config.getEmailFromAddr()).thenReturn("test_from_address@anet.com");
-
-    final SmtpConfiguration smtpConfig =
-        PowerMockito.mock(SmtpConfiguration.class, Mockito.RETURNS_MOCKS);
-    // FIXME: Hard-coded; get this from the config via a DropwizardAppRule
-    when(smtpConfig.getStartTls()).thenReturn(false);
-    when(smtpConfig.getHostname()).thenReturn("localhost");
-    when(smtpConfig.getPort()).thenReturn(1125);
-    when(config.getSmtp()).thenReturn(smtpConfig);
-
-    emailServer = new FakeSmtpServer(config.getSmtp());
-    emailWorker = new AnetEmailWorker(emailDao, config, scheduler);
-
-    // Email engine
-    PowerMockito.mockStatic(AnetObjectEngine.class);
-    instance = PowerMockito.mock(AnetObjectEngine.class);
-    when(instance.getContext()).thenReturn(new HashMap<String, Object>());
-    when(AnetObjectEngine.getInstance()).thenReturn(instance);
+    emailServer = new FakeSmtpServer(app.getConfiguration().getSmtp());
+    emailWorker = new AnetEmailWorker(emailDao, app.getConfiguration(), scheduler);
 
     // Clear the email server before starting test
     emailServer.clearEmailServer();
   }
 
-  @After
-  public void tearDown() throws Exception {
+  @AfterAll
+  public static void tearDown() throws Exception {
     // Clear the email server after test
     emailServer.clearEmailServer();
   }
@@ -113,7 +95,7 @@ public class AnetEmailWorkerTest {
   }
 
   private AnetEmail createTestEmail(int id, List<String> toAddresses, String comment) {
-    final AnetEmail email = PowerMockito.mock(AnetEmail.class, Mockito.RETURNS_MOCKS);
+    final AnetEmail email = mock(AnetEmail.class, Mockito.RETURNS_DEEP_STUBS);
     when(email.getId()).thenReturn(id);
     when(email.getToAddresses()).thenReturn(toAddresses);
     when(email.getCreatedAt()).thenReturn(Instant.now());
