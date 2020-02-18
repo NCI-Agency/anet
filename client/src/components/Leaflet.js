3d5e61d20438262eceb82754ecfa70fc35f2feee
Mara Dragan <mara@west.nl>
2020-02-18 15:22:10 +0100

    NCI-Agency/anet#2579: Remove not used effects from Leaflet
    Leaflet is not being used with updated markers props, when passing new
    markers the component is also always mounted. We therefore removed the
    code which was taking care of the markers update.

--- client/src/components/Leaflet.js
+++ client/src/components/Leaflet.js
@@ -12,15 +12,12 @@ import { MarkerClusterGroup } from "leaflet.markercluster"
 import "leaflet.markercluster/dist/MarkerCluster.css"
 import "leaflet.markercluster/dist/MarkerCluster.Default.css"
 import "leaflet/dist/leaflet.css"
-import _isEqualWith from "lodash/isEqualWith"
-import _sortBy from "lodash/sortBy"
 import { Location } from "models"
 import PropTypes from "prop-types"
 import React, { useCallback, useEffect, useRef, useState } from "react"
 import MARKER_ICON_2X from "resources/leaflet/marker-icon-2x.png"
 import MARKER_ICON from "resources/leaflet/marker-icon.png"
 import MARKER_SHADOW from "resources/leaflet/marker-shadow.png"
-import utils from "utils"
 
 const css = {
   zIndex: 1
@@ -104,13 +101,6 @@ const Leaflet = ({
     marginBottom: marginBottom
   })
 
-  const latestMarkers = useRef(markers)
-  const markersIdsUnchanged = _isEqualWith(
-    _sortBy(latestMarkers.current.map(m => m.id)),
-    _sortBy(markers.map(m => m.id)),
-    utils.treatFunctionsAsEqual
-  )
-
   const latestWidth = useRef(width)
   const widthPropUnchanged = latestWidth.current === width
 
@@ -122,11 +112,8 @@ const Leaflet = ({
   const [doInitializeMarkerLayer, setDoInitializeMarkerLayer] = useState(false)
 
   const updateMarkerLayer = useCallback(
-    (markersToAdd, markersToRemove) => {
-      const markers = markersToAdd || []
-      markersToRemove = markersToRemove || []
-      const newMarkers = []
-      markers.forEach(m => {
+    (newMarkers = []) => {
+      newMarkers.forEach(m => {
         const latLng = Location.hasCoordinates(m)
           ? [m.lat, m.lng]
           : map.getCenter()
@@ -142,13 +129,7 @@ const Leaflet = ({
         if (m.onMove) {
           marker.on("move", event => m.onMove(event, map))
         }
-        newMarkers.push(marker)
         markerLayer.addLayer(marker)
-      })
-
-      markersToRemove.forEach(m => {
-        const ml = markerLayer.getLayers().find(ml => ml.options.id === m.id)
-        markerLayer.removeLayer(ml)
       })
 
       if (newMarkers.length > 0) {
@@ -193,25 +174,7 @@ const Leaflet = ({
       updateMarkerLayer(markers)
       setDoInitializeMarkerLayer(false)
     }
-  }, [
-    setDoInitializeMarkerLayer,
-    markers,
-    updateMarkerLayer,
-    doInitializeMarkerLayer
-  ])
-
-  useEffect(() => {
-    // Update markerLayer when the markerIds changed
-    if (!markersIdsUnchanged) {
-      const markersToAdd = markers.filter(
-        m => latestMarkers.findIndex(pm => pm.id === m.id) === -1
-      )
-      const markersToRemove = latestMarkers.filter(
-        pm => markers.findIndex(m => m.id === pm.id) === -1
-      )
-      updateMarkerLayer(markersToAdd, markersToRemove)
-    }
-  }, [markers, markersIdsUnchanged, updateMarkerLayer])
+  }, [doInitializeMarkerLayer, markers, updateMarkerLayer])
 
   useEffect(() => {
     if (map && !(widthPropUnchanged && heightPropUnchanged)) {
