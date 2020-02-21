ac5cb0fcba36ae1072c76928a11f2f8823e109d1
Mara Dragan <mara@west.nl>
2020-02-21 15:33:13 +0100

    NCI-Agency/anet#2579: Fix OrganizationAdvisorsTable component
    HandleSelectRowData was not being called properly.

--- client/src/components/AdvisorReports/OrganizationAdvisorsTable.js
+++ client/src/components/AdvisorReports/OrganizationAdvisorsTable.js
@@ -4,15 +4,62 @@ import AdvisorReportsRow from "components/AdvisorReports/AdvisorReportsRow"
 import AdvisorReportsTableHead from "components/AdvisorReports/AdvisorReportsTableHead"
 import pluralize from "pluralize"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useEffect, useMemo, useRef, useState } from "react"
 import { Table } from "react-bootstrap"
 import "./OrganizationAdvisorsTable.css"
 
-const OrganizationAdvisorsTable = props => {
-  const [data, setData] = useState(props.data || [])
+const OrganizationAdvisorsTable = ({
+  data: dataProp,
+  filterText,
+  columnGroups,
+  onRowSelection
+}) => {
+  const [data, setData] = useState(dataProp || [])
   const [selectedAll, setSelectedAll] = useState(false)
-  const rows = createAdvisorReportsRows(data)
-  const showRows = props.filterText ? search(rows, props.filterText) : rows
+
+  const latestOnRowSelection = useRef(onRowSelection)
+
+  const rows = useMemo(() => {
+    const handleSelectRow = index => {
+      const rows = data.slice()
+      // Toggle row selection
+      rows[index].selected = !data[index].selected
+      setData(rows)
+    }
+    return data.map((organization, index) => {
+      const checked =
+        organization.selected === undefined ? false : organization.selected
+      const modalLink = (
+        <AdvisorReportsModal
+          name={organization.name}
+          uuid={organization.uuid}
+          columnGroups={columnGroups}
+        />
+      )
+      return (
+        <AdvisorReportsRow
+          link={modalLink}
+          row={organization}
+          columnGroups={columnGroups}
+          checked={checked}
+          withOrganizationLink
+          onSelectRow={() => handleSelectRow(index)}
+          key={index}
+        />
+      )
+    })
+  }, [columnGroups, data])
+
+  useEffect(() => {
+    latestOnRowSelection.current = onRowSelection
+  }, [onRowSelection])
+
+  useEffect(() => {
+    const selectedData = data.filter(row => row.selected)
+    latestOnRowSelection.current(selectedData)
+  }, [data])
+
+  const filteredRows = filterText ? filterRows(rows, filterText) : rows
 
   return (
     <div className="organization-advisors-table">
@@ -22,86 +69,38 @@ const OrganizationAdvisorsTable = props => {
           organization's {pluralize(Settings.fields.advisor.person.name)}
         </caption>
         <AdvisorReportsTableHead
-          columnGroups={props.columnGroups}
+          columnGroups={columnGroups}
           title="Organization name"
           onSelectAllRows={handleSelectAllRows}
         />
-        <tbody>{showRows}</tbody>
+        <tbody>{filteredRows}</tbody>
       </Table>
     </div>
   )
 
-  function handleSelectRow(index) {
-    const newData = data.slice()
-    newData[index].selected = toggleRowSelection(index)
-    setData(newData)
-    handleSelectRowData(newData)
-  }
-
   function handleSelectAllRows() {
     const toggleSelect = !selectedAll
-    const rows = toggleSelectAllRows(toggleSelect)
-    setData(rows)
-    setSelectedAll(toggleSelect)
-    handleSelectRowData(data)
-  }
-
-  function handleSelectRowData() {
-    const selectedData = data.filter(row => {
-      return row.selected
-    })
-    props.onRowSelection(selectedData)
-  }
-
-  function toggleRowSelection(index) {
-    return !data[index].selected
-  }
-
-  function toggleSelectAllRows(selected) {
+    // Toggle select all rows
     const rows = data.slice()
     rows.forEach(item => {
-      item.selected = selected
+      item.selected = toggleSelect
     })
-    return rows
+    setData(rows)
+    setSelectedAll(toggleSelect)
   }
 
-  function search(rows, filterText) {
+  function filterRows(rows, filterText) {
     const nothingFound = (
       <tr className="nothing-found">
         <td colSpan="8">No organizations found...</td>
       </tr>
     )
-    const search = rows.filter(element => {
+    const filterResult = rows.filter(element => {
       const elemProps = element.props.row
       const orgName = elemProps.name.toLowerCase()
       return orgName.indexOf(filterText.toLowerCase()) !== -1
     })
-    return search.length > 0 ? search : nothingFound
-  }
-
-  function createAdvisorReportsRows(data) {
-    return data.map((organization, index) => {
-      const checked =
-        organization.selected === undefined ? false : organization.selected
-      const modalLink = (
-        <AdvisorReportsModal
-          name={organization.name}
-          uuid={organization.uuid}
-          columnGroups={props.columnGroups}
-        />
-      )
-      return (
-        <AdvisorReportsRow
-          link={modalLink}
-          row={organization}
-          columnGroups={props.columnGroups}
-          checked={checked}
-          withOrganizationLink
-          onSelectRow={() => handleSelectRow(index)}
-          key={index}
-        />
-      )
-    })
+    return filterResult.length > 0 ? filterResult : nothingFound
   }
 }
 
