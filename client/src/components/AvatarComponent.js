d8a608e636ccb8fefb368d590ac6ef14bc315e9e
Mara Dragan <mara@west.nl>
2020-02-24 16:12:18 +0100

    NCI-Agency/anet#2579: Make AvatarComponent a functional component

--- client/src/components/AvatarComponent.js
+++ client/src/components/AvatarComponent.js
@@ -1,61 +1,59 @@
-import React from "react"
+import React, { useState } from "react"
 import Avatar from "react-avatar-edit"
 import DEFAULT_AVATAR from "resources/default_avatar.svg"
 import PropTypes from "prop-types"
 
 // More info about this component: https://github.com/kirill3333/react-avatar
-export default class AvatarComponent extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      preview: null
-    }
+const AvatarComponent = ({
+  onChangePreview,
+  src,
+  editCurrent,
+  showPreview
+}) => {
+  const [preview, setPreview] = useState(null)
+
+  const previewImage = showPreview && (
+    <div style={{ float: "left" }}>
+      <div style={{ fontWeight: "bold" }}>Preview</div>
+      <img src={preview} alt="Preview" />
+    </div>
+  )
+  const image =
+    editCurrent &&
+    (src === null || src === ""
+      ? DEFAULT_AVATAR
+      : "data:image/jpeg;base64," + src)
+
+  return (
+    <span style={{ margin: "0 auto", display: "table", overflow: "scroll" }}>
+      <Avatar
+        onCrop={onCrop}
+        onClose={onClose}
+        src={image}
+        shadingColor="white"
+        closeIconColor="black"
+        backgroundColor="white"
+        imageWidth="512" // image
+        width="512" // editor
+      />
+      {previewImage}
+    </span>
+  )
+
+  function onClose() {
+    setPreview(null)
   }
 
-  static propTypes = {
-    onChangePreview: PropTypes.func.isRequired,
-    src: PropTypes.string,
-    editCurrent: PropTypes.bool,
-    showPreview: PropTypes.bool
-  }
-
-  onClose = () => {
-    this.setState({ preview: null })
-  }
-
-  onCrop = preview => {
-    this.setState({ preview })
-    this.props.onChangePreview(preview)
-  }
-
-  render() {
-    const previewImage = this.props.showPreview && (
-      <div style={{ float: "left" }}>
-        <div style={{ fontWeight: "bold" }}>Preview</div>
-        <img src={this.state.preview} alt="Preview" />
-      </div>
-    )
-
-    const image =
-      this.props.editCurrent &&
-      (this.props.src === null || this.props.src === ""
-        ? DEFAULT_AVATAR
-        : "data:image/jpeg;base64," + this.props.src)
-
-    return (
-      <span style={{ margin: "0 auto", display: "table", overflow: "scroll" }}>
-        <Avatar
-          onCrop={this.onCrop}
-          onClose={this.onClose}
-          src={image}
-          shadingColor="white"
-          closeIconColor="black"
-          backgroundColor="white"
-          imageWidth="512" // image
-          width="512" // editor
-        />
-        {previewImage}
-      </span>
-    )
+  function onCrop(preview) {
+    setPreview(preview)
+    onChangePreview(preview)
   }
 }
+AvatarComponent.propTypes = {
+  onChangePreview: PropTypes.func.isRequired,
+  src: PropTypes.string,
+  editCurrent: PropTypes.bool,
+  showPreview: PropTypes.bool
+}
+
+export default AvatarComponent
