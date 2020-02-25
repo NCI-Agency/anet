ee8d020b0bf4f69ad4c59dd88f1a03cc9adcebcc
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2020-02-25 18:38:18 +0100

    nci-agency/anet#2579: Fix not rendering default avatar bug when creating a new Person

--- client/src/components/AvatarDisplayComponent.js
+++ client/src/components/AvatarDisplayComponent.js
@@ -2,16 +2,16 @@ import React from "react"
 import DEFAULT_AVATAR from "resources/default_avatar.svg"
 import PropTypes from "prop-types"
 
-const AvatarDisplayComponent = ({ avatar, height, width, style }) => {
-  const image =
-    avatar === null || avatar === ""
-      ? DEFAULT_AVATAR
-      : "data:image/jpeg;base64," + avatar
+const AvatarDisplayComponent = ({ avatar, height, width, style }) => (
+  <img
+    src={avatar ? `data:image/jpeg;base64,${avatar}` : DEFAULT_AVATAR}
+    height={height}
+    width={width}
+    alt="Avatar"
+    style={style}
+  />
+)
 
-  return (
-    <img src={image} height={height} width={width} alt="Avatar" style={style} />
-  )
-}
 AvatarDisplayComponent.propTypes = {
   avatar: PropTypes.string,
   height: PropTypes.number,
