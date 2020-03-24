1836ca9657e514026d4c0382f21c995329c53d19
Mara Dragan <mara@west.nl>
2020-03-24 10:12:09 +0100

    NCI-Agency/anet#2926: Refactor AddAssessmentModal component
    In order to make the validation work the formik form now has
    as initialValues an object following the yupSchema and the form
    fields structure.

--- client/src/components/assessments/AddAssessmentModal.js
+++ client/src/components/assessments/AddAssessmentModal.js
@@ -3,14 +3,13 @@ import {
   CustomFieldsContainer,
   customFieldsJSONString
 } from "components/CustomFields"
-import { GQL_CREATE_NOTE, NOTE_TYPE } from "components/Model"
+import Model, { GQL_CREATE_NOTE, NOTE_TYPE } from "components/Model"
 import Messages from "components/Messages"
-
 import { Form, Formik } from "formik"
 import _isEmpty from "lodash/isEmpty"
 import { Task } from "models"
 import PropTypes from "prop-types"
-import React, { useState } from "react"
+import React, { useMemo, useState } from "react"
 import { Button, Modal } from "react-bootstrap"
 
 const AddAssessmentModal = ({
@@ -28,21 +27,16 @@ const AddAssessmentModal = ({
   const yupSchema = isTopLevelTask
     ? Task.topLevelAssessmentCustomFieldsSchema
     : Task.subLevelAssessmentCustomFieldsSchema
+  const initialValues = useMemo(() => Model.fillObject({}, yupSchema), [
+    yupSchema
+  ])
   return (
     <Modal show={showModal} onHide={closeModal}>
       <Formik
         enableReinitialize
         onSubmit={onAssessmentSubmit}
         validationSchema={yupSchema}
-        initialValues={{
-          type: NOTE_TYPE.ASSESSMENT,
-          noteRelatedObjects: [
-            {
-              relatedObjectType: "tasks",
-              relatedObjectUuid: task.uuid
-            }
-          ]
-        }}
+        initialValues={initialValues}
       >
         {({
           isSubmitting,
@@ -52,57 +46,62 @@ const AddAssessmentModal = ({
           validateForm,
           values,
           submitForm
-        }) => (
-          <Form>
-            <Modal.Header closeButton>
-              <Modal.Title>
-                Assessment for {task.shortName} for{" "}
-                {assessmentPeriod.start.format("MMM-YYYY")}
-              </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  padding: 5,
-                  height: "100%"
-                }}
-              >
-                <Messages error={assessmentError} />
-                <CustomFieldsContainer
-                  fieldsConfig={fieldSettings.assessment?.customFields}
-                  formikProps={{
-                    setFieldTouched,
-                    setFieldValue,
-                    values,
-                    validateForm
+        }) => {
+          return (
+            <Form>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  Assessment for {task.shortName} for{" "}
+                  {assessmentPeriod.start.format("MMM-YYYY")}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: 5,
+                    height: "100%"
                   }}
-                  vertical
-                />
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button className="pull-left" onClick={closeModal}>
-                Cancel
-              </Button>
-              <Button
-                onClick={submitForm}
-                bsStyle="primary"
-                disabled={isSubmitting || !isValid}
-              >
-                Save
-              </Button>
-            </Modal.Footer>
-          </Form>
-        )}
+                >
+                  <Messages error={assessmentError} />
+                  <CustomFieldsContainer
+                    fieldsConfig={fieldSettings.assessment?.customFields}
+                    fieldNamePrefix="taskAssessment"
+                    formikProps={{
+                      setFieldTouched,
+                      setFieldValue,
+                      values,
+                      validateForm
+                    }}
+                    vertical
+                  />
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button className="pull-left" onClick={closeModal}>
+                  Cancel
+                </Button>
+                <Button
+                  onClick={submitForm}
+                  bsStyle="primary"
+                  disabled={isSubmitting}
+                >
+                  Save
+                </Button>
+              </Modal.Footer>
+            </Form>
+          )
+        }}
       </Formik>
     </Modal>
   )
 
   function closeModal() {
+    setAssessmentError(null)
     onCancel()
   }
+
   function onAssessmentSubmit(values, form) {
     return saveAssessment(values, form)
       .then(response => onSubmitSuccess(response, values, form))
@@ -118,13 +117,15 @@ const AddAssessmentModal = ({
 
   function saveAssessment(values, form) {
     const updatedNote = {
-      uuid: values.uuid,
-      author: values.author,
-      type: values.type,
-      noteRelatedObjects: values.noteRelatedObjects,
-      text: values.text
+      type: NOTE_TYPE.ASSESSMENT,
+      noteRelatedObjects: [
+        {
+          relatedObjectType: "tasks",
+          relatedObjectUuid: task.uuid
+        }
+      ]
     }
-    updatedNote.text = customFieldsJSONString(values, true)
+    updatedNote.text = customFieldsJSONString(values, true, "taskAssessment")
     return API.mutation(GQL_CREATE_NOTE, {
       note: updatedNote
     })
