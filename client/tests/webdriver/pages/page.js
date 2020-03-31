c3fd88e6aa13b9069a39f44d6c71f5435e534a50
Gertjan van Oosten <gertjan@west.nl>
2020-03-31 11:34:13 +0200

    Update webdriver v6 method calls to use options object

--- client/tests/webdriver/pages/page.js
+++ client/tests/webdriver/pages/page.js
@@ -15,7 +15,11 @@ class Page {
     browser.url(this._buildUrl(pathName, credentials))
     browser
       .$("div.loader")
-      .waitForExist(30000, true, "Expected everything to be loaded by now")
+      .waitForExist({
+        timeout: 30000,
+        reverse: true,
+        timeoutMsg: "Expected everything to be loaded by now"
+      })
   }
 
   open(pathName = "/", credentials = Page.DEFAULT_CREDENTIALS.user) {
