cb64d28da0cd1af7d750a7cd06180174bdcf8000
Gertjan van Oosten <gertjan@west.nl>
2020-05-06 20:24:21 +0200

    Fix potential inconsistency in approval step type

--- src/main/java/mil/dds/anet/utils/Utils.java
+++ src/main/java/mil/dds/anet/utils/Utils.java
@@ -23,6 +23,7 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response.Status;
 import mil.dds.anet.AnetObjectEngine;
 import mil.dds.anet.beans.ApprovalStep;
+import mil.dds.anet.beans.ApprovalStep.ApprovalStepType;
 import mil.dds.anet.beans.Organization;
 import mil.dds.anet.beans.Task;
 import mil.dds.anet.database.ApprovalStepDao;
@@ -474,6 +475,7 @@ public class Utils {
       logger.debug("Editing planning approval steps for {}", entity);
       for (ApprovalStep step : planningApprovalSteps) {
         Utils.validateApprovalStep(step);
+        step.setType(ApprovalStepType.PLANNING_APPROVAL);
         step.setRelatedObjectUuid(entity.getUuid());
       }
 
@@ -488,6 +490,7 @@ public class Utils {
       logger.debug("Editing approval steps for {}", entity);
       for (ApprovalStep step : approvalSteps) {
         Utils.validateApprovalStep(step);
+        step.setType(ApprovalStepType.REPORT_APPROVAL);
         step.setRelatedObjectUuid(entity.getUuid());
       }
 
