67f94a184df2e666f20e5fa7e41590aced8db767
Vassil <vassil.iordanov@ncia.nato.int>
2020-07-04 12:00:13 +0200

    nci-agency/anet#2954: Add icons and alignment to period navigation buttons

--- client/src/components/PeriodsNavigation.js
+++ client/src/components/PeriodsNavigation.js
@@ -1,26 +1,26 @@
+import { Icon } from "@blueprintjs/core"
+import { IconNames } from "@blueprintjs/icons"
+import PropTypes from "prop-types"
 import React from "react"
 import { Button } from "react-bootstrap"
-import PropTypes from "prop-types"
 
 const PeriodsNavigation = ({ offset, onChange }) => (
-  <>
+  <div style={{ display: "flex", justifyContent: "space-between" }}>
     <Button
       bsStyle="default"
       type="button"
       onClick={() => onChange(offset + 1)}
     >
-      previous period
+      <Icon icon={IconNames.DOUBLE_CHEVRON_LEFT} /> previous period
     </Button>
-    {offset - 1 >= 0 && (
-      <Button
-        bsStyle="default"
-        type="button"
-        onClick={() => onChange(offset - 1)}
-      >
-        next period
-      </Button>
-    )}
-  </>
+    <Button
+      bsStyle="default"
+      type="button"
+      onClick={() => onChange(offset - 1)}
+    >
+      next period <Icon icon={IconNames.DOUBLE_CHEVRON_RIGHT} />
+    </Button>
+  </div>
 )
 PeriodsNavigation.propTypes = {
   offset: PropTypes.number,
