3fa7c11721bfb78b2c0f4a7ae0485c56691a86cc
Vassil Iordanov <vassil.iordanov@ncia.nato.int>
2020-07-29 14:57:50 +0200

    nci-agency/anet#3134: Replace d3 dom manipulation with reach for organization diagram

--- client/src/components/graphs/OrganizationalChart.js
+++ client/src/components/graphs/OrganizationalChart.js
@@ -8,16 +8,9 @@ import {
 } from "components/Page"
 import * as d3 from "d3"
 import { Symbol } from "milsymbol"
-
 import { Organization, Position } from "models"
 import PropTypes from "prop-types"
-import React, {
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-  useCallback
-} from "react"
+import React, { useEffect, useRef, useState, useCallback } from "react"
 import { connect } from "react-redux"
 import { useHistory } from "react-router-dom"
 import DEFAULT_AVATAR from "resources/default_avatar.svg"
@@ -63,8 +56,6 @@ const GQL_GET_CHART_DATA = gql`
     }
   }
 `
-const transitionDuration = 500
-
 const ranks = Settings.fields.person.ranks.map(rank => rank.value)
 
 const sortPositions = (positions, truncateLimit) => {
@@ -84,10 +75,7 @@ const OrganizationalChart = ({
   height: initialHeight
 }) => {
   const history = useHistory()
-  const canvasRef = useRef(null)
   const svgRef = useRef(null)
-  const linkRef = useRef(null)
-  const nodeRef = useRef(null)
   const treeLayout = useRef(null)
   const [root, setRoot] = useState(null)
   const [height, setHeight] = useState(initialHeight)
@@ -100,10 +88,6 @@ const OrganizationalChart = ({
     error,
     pageDispatchers
   })
-
-  const canvas = d3.select(canvasRef.current)
-  const link = d3.select(linkRef.current)
-  const node = d3.select(nodeRef.current)
 
   const getDescendant = useCallback(
     uuid =>
@@ -166,8 +150,6 @@ const OrganizationalChart = ({
       )
       treeLayout.current(tree)
       setRoot(tree)
-      console.log(tree)
-      console.log(treeLayout)
     }
   }, [data, isParent, getSize, isMain])
 
@@ -176,236 +158,8 @@ const OrganizationalChart = ({
       return
     }
 
-    canvas.attr("transform", `translate(${width / 2},50)`)
-
     setHeight(1000)
-  }, [canvas, data, height, width, root])
-
-  useLayoutEffect(() => {
-    if (!(link && node && data?.organization && treeLayout.current && root)) {
-      return
-    }
-
-    const linkSelect = link
-      .selectAll("path")
-      .data(treeLayout.current(root).links())
-
-    linkSelect
-      .transition()
-      .duration(transitionDuration)
-      .attr(
-        "d",
-        d3
-          .linkVertical()
-          .x(d => d.x)
-          .y(d => d.y)
-      )
-
-    linkSelect
-      .enter()
-      .append("path")
-      .attr("class", "link")
-      .attr("stroke-opacity", " 0.3")
-      .attr(
-        "d",
-        d3
-          .linkVertical()
-          .x(d => d.x)
-          .y(d => d.y)
-      )
-
-    linkSelect.exit().remove()
-
-    const nodeSelect = node
-      .selectAll("g.org")
-      .data(root.descendants(), d => d.data.uuid)
-
-    nodeSelect
-      .transition()
-      .duration(transitionDuration)
-      .attr(
-        "transform",
-        d =>
-          `translate(${d.x},${d.y}) scale(${getScale(d.data)} ${getScale(
-            d.data
-          )})`
-      )
-
-    nodeSelect.exit().remove()
-
-    const nodeEnter = nodeSelect
-      .enter()
-      .append("g")
-      .attr("class", "org")
-      .attr(
-        "transform",
-        d =>
-          `translate(${d.x},${d.y}) scale(${getScale(d.data)} ${getScale(
-            d.data
-          )})`
-      )
-
-    nodeEnter
-      .append("rect")
-      .attr("rx", 7)
-      .attr("ry", 7)
-      .attr("x", d => -getSize(d.data)[0] / 2)
-      .attr("y", 15)
-      .attr("width", d => getSize(d.data)[0])
-      .attr("height", d => getSize(d.data)[1] - 100 * getScale(d.data))
-      // .attr("width", d => d.size[0]})
-      // .attr("height", d => d.size[1] - 20)
-      .style("fill", d =>
-        isMain(d.data) ? "rgba(255, 255, 255, 1)" : "rgba(230, 230, 230, 0.5)"
-      )
-      .style("stroke", d => (isMain(d.data) ? "black" : "none"))
-
-    nodeSelect
-      .selectAll("rect")
-      .attr("x", d => -getSize(d.data)[0] / 2)
-      .attr("y", 15)
-      .attr("width", d => getSize(d.data)[0])
-      .attr("height", d => getSize(d.data)[1] - 100 * getScale(d.data))
-      // .attr("width", d => d.size[0]})
-      // .attr("height", d => d.size[1] - 20)
-      .style("fill", d =>
-        isMain(d.data) ? "rgba(255, 255, 255, 1)" : "rgba(230, 230, 230, 0.5)"
-      )
-      .style("stroke", d => (isMain(d.data) ? "black" : "none"))
-
-    const iconNodeG = nodeEnter
-      .append("g")
-      .attr("class", "orgDetails")
-      .attr("transform", "translate(-8,-15)")
-
-    iconNodeG
-      .append("g")
-      .on("click", d => history.push(Organization.pathFor(d.data)))
-      .each(function(d) {
-        const positions = sortPositions(d.data.positions)
-        const unitcode = Settings.fields.person.ranks.find(
-          element => element.value === positions?.[0]?.person?.rank
-        )?.app6Modifier
-
-        const sym = new Symbol(
-          `S${
-            d.data.type === Organization.TYPE.ADVISOR_ORG ? "F" : "N"
-          }GPU------${unitcode || "-"}`,
-          { size: 22 }
-        )
-        this.appendChild(sym.asDOM())
-      })
-
-    iconNodeG
-      .append("text")
-      .on("click", d => history.push(Organization.pathFor(d.data)))
-      .attr("font-size", "20px")
-      .attr("font-family", "monospace")
-      .attr("font-weight", "bold")
-      .attr("dy", 22)
-      .attr("x", 38)
-      .text(d =>
-        d.data.shortName?.length > 12
-          ? d.data.shortName.substring(0, 10) + ".."
-          : d.data.shortName
-      )
-
-    iconNodeG
-      .append("text")
-      .on("click", d => history.push(Organization.pathFor(d.data)))
-      .attr("font-family", "monospace")
-      .attr("dy", 45)
-      .attr("x", -40)
-      .text(d =>
-        d.data.longName?.length > 21
-          ? d.data.longName.substring(0, 18) + ".."
-          : d.data.longName
-      )
-
-    const headG = nodeEnter.selectAll("g.head").data(
-      d => sortPositions(d.data.positions, 1) || [],
-      d => d.uuid
-    )
-
-    const headGenter = headG
-      .enter()
-      .append("g")
-      .attr("class", "head")
-      .attr("transform", "translate(-63, 65)")
-      .on("click", d => history.push(Position.pathFor(d)))
-
-    headG.exit().remove()
-
-    headGenter
-      .append("image")
-      .attr("width", 26)
-      .attr("height", 26)
-      .attr("y", -15)
-      .attr("href", d => d.person && (d.person.avatar || DEFAULT_AVATAR))
-
-    headGenter
-      .append("text")
-      .attr("x", 26)
-      .attr("y", -4)
-      .attr("font-size", "11px")
-      .attr("font-family", "monospace")
-      .attr("font-weight", "bold")
-      .style("text-anchor", "start")
-      .text((position, i) => {
-        const name = `${position.person ? position.person.rank : ""} ${
-          position.person ? position.person.name : "unfilled"
-        }`
-        return name.length > 23 ? name.substring(0, 21) + ".." : name
-      })
-
-    headGenter
-      .append("text")
-      .attr("x", 26)
-      .attr("y", 6)
-      .attr("font-size", "11px")
-      .attr("font-family", "monospace")
-      .attr("font-weight", "bold")
-      .style("text-anchor", "start")
-      .text((position, i) =>
-        position.name.length > 23
-          ? position.name.substring(0, 21) + ".."
-          : position.name
-      )
-
-    const positionsG = nodeEnter.selectAll("g.position").data(
-      d => sortPositions(d.data.positions, 10).slice(1),
-      d => d.uuid
-    )
-
-    positionsG.exit().remove()
-
-    const positionsGA = positionsG
-      .enter()
-      .append("g")
-      .attr("class", "position")
-      .attr("transform", (d, i) => `translate(-63,${87 + i * 11})`)
-      .on("click", d => history.push(Position.pathFor(d)))
-
-    positionsGA
-      .append("image")
-      .attr("width", 13)
-      .attr("height", 13)
-      .attr("y", -10)
-      .attr("href", d => d.person && (d.person.avatar || DEFAULT_AVATAR))
-
-    positionsGA
-      .append("text")
-      .attr("x", 18)
-      .attr("font-size", "9px")
-      .attr("font-family", "monospace")
-      .style("text-anchor", "start")
-      .text((d, i) => {
-        const result = `${d.person ? d.person.rank : ""} ${
-          d.person ? d.person.name : "unfilled"
-        } ${d.name}`
-        return result.length > 31 ? result.substring(0, 28) + "..." : result
-      })
-  }, [data, history, root, link, node, isMain, getSize])
+  }, [data, height, width, root])
 
   return (
     <>
@@ -418,12 +172,126 @@ const OrganizationalChart = ({
           exportTitle || `${data.organization.shortName} organization chart`
         }
       >
-        <g ref={canvasRef}>
-          <g ref={linkRef} style={{ fill: "none", stroke: "#555" }} />
-          <g
-            ref={nodeRef}
-            style={{ cursor: "pointer", pointerEvents: "all" }}
-          />
+        <g transform={`translate(${width / 2},50)`}>
+          <g style={{ fill: "none", stroke: "#555" }}>
+            {treeLayout.current &&
+              treeLayout
+                .current(root)
+                .links()
+                .map((d, i) => {
+                  return (
+                    <path
+                      strokeOpacity={0.3}
+                      key={i}
+                      d={d3
+                        .linkVertical()
+                        .x(d => d.x)
+                        .y(d => d.y)(d)}
+                    />
+                  )
+                })}
+          </g>
+          <g style={{ cursor: "pointer", pointerEvents: "all" }}>
+            {root?.descendants().map(d => {
+              const org = d.data
+              const scale = getScale(org)
+              const size = getSize(org)
+              const positions = sortPositions(org.positions)
+              const unitcode = Settings.fields.person.ranks.find(
+                element => element.value === positions?.[0]?.person?.rank
+              )?.app6Modifier
+              const sym = new Symbol(
+                `S${
+                  org.type === Organization.TYPE.ADVISOR_ORG ? "F" : "N"
+                }GPU------${unitcode || "-"}`,
+                { size: 22 }
+              )
+
+              return (
+                <g
+                  className="org"
+                  key={`${org.uuid}`}
+                  transform={`translate(${d.x},${d.y}) scale(${scale} ${scale})`}
+                >
+                  <rect
+                    rx="7"
+                    ry="7"
+                    x={-size[0] / 2}
+                    y={15}
+                    width={size[0]}
+                    height={size[1] - 100 * scale}
+                    style={{
+                      fill: isMain(org)
+                        ? "rgba(255, 255, 255, 1)"
+                        : "rgba(230, 230, 230, 0.5)",
+                      stroke: isMain(org) ? "black" : "none"
+                    }}
+                  />
+                  <g className="orgDetails" transform="translate(-8,-15)">
+                    <g
+                      onClick={() => history.push(Organization.pathFor(org))}
+                      dangerouslySetInnerHTML={{ __html: sym.asSVG() }}
+                    />
+                    <text
+                      onClick={() => history.push(Organization.pathFor(org))}
+                      fontSize="20px"
+                      fontFamily="monospace"
+                      fontWeight="bold"
+                      dy={22}
+                      x={38}
+                    >
+                      {org.shortName?.length > 12
+                        ? org.shortName.substring(0, 10) + ".."
+                        : org.shortName}
+                    </text>
+                    <text
+                      onClick={() => history.push(Organization.pathFor(org))}
+                      fontFamily="monospace"
+                      dy={45}
+                      x={-40}
+                    >
+                      {org.longName?.length > 21
+                        ? org.longName.substring(0, 18) + ".."
+                        : org.longName}
+                    </text>
+
+                    {sortPositions(org.positions, 10).map((position, i) => {
+                      const result = `${
+                        position.person ? position.person.rank : ""
+                      } ${
+                        position.person ? position.person.name : "unfilled"
+                      } ${position.name}`
+
+                      return (
+                        <g
+                          key={position.uuid}
+                          transform={`translate(-63,${87 + i * 11})`}
+                          onClick={() => history.push(Position.pathFor(d))}
+                        >
+                          <image
+                            width={13}
+                            height={13}
+                            y={-10}
+                            href={position.person?.avatar || DEFAULT_AVATAR}
+                          />
+                          <text
+                            x={18}
+                            fontSize="9px"
+                            fontFamily="monospace"
+                            textAnchor="start"
+                          >
+                            {result.length > 31
+                              ? result.substring(0, 28) + "..."
+                              : result}
+                          </text>
+                        </g>
+                      )
+                    })}
+                  </g>
+                </g>
+              )
+            })}
+          </g>
         </g>
       </SVGCanvas>
     </>
