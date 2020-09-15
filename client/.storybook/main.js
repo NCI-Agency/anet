98101bfcb7e0f281901879e2a5922d811cd22fd5
Gertjan van Oosten <gertjan@west.nl>
2020-09-15 14:23:03 +0200

    NCI-Agency/anet#3139: Configure Storybook v6 with default example stories

---
+++ client/.storybook/main.js
@@ -0,0 +1,12 @@
+const paths = require("../config/paths")
+
+module.exports = {
+  "stories": [
+    "../stories/*.stories.mdx",
+    "../stories/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
--- client/stories/Button.stories.js
+++ client/stories/Button.stories.js
@@ -1,16 +1,36 @@
-import { action } from "@storybook/addon-actions"
-import { storiesOf } from "@storybook/react"
-import React from "react"
-import { Button } from "react-bootstrap"
+import React from 'react';
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>Hello Button</Button>
-  ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ))
+import { Button } from './Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
