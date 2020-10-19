16a3a8aee2fb3f021cf2bdac46ffb12c7f2d54d0
okan ayvazoglu <okan.ayvazoglu@simsoft.com.tr>
2020-10-19 10:51:52 +0300

    NCI-Agency/anet#2959: Add update rules for tables

--- anet-tools/src/core/anet_import.py
+++ anet-tools/src/core/anet_import.py
@@ -1,7 +1,9 @@
 import os
+import uuid
+import json
 import pandas as pd
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, and_
 
 class anet_import:
     def __init__(self):
@@ -11,6 +13,7 @@ class anet_import:
                             os.environ["ANET_DB_NAME"]
         self.path_root = "/".join(os.getcwd().split("/")[:4])
         self.path_log = os.path.join(self.path_root, "datasamples")
+        self.update_rules = { "tables": [] }
         
         print("db object created")
     
@@ -27,6 +30,13 @@ class anet_import:
         # Print db connection string
         print(self.dbConnString)
     
+    def check_data_type(self, variable, name, datatype):
+        if type(variable) is not datatype:
+            print("'" + name + "'", "is ", type(variable), ", however, it must be " , datatype)
+            return False
+        else:
+            return True
+    
     def connect(self):
         try:
             self.engine = create_engine(self.dbConnString)
@@ -37,20 +47,38 @@ class anet_import:
         except Exception as e:
             print("EXCEPTION WHILE CONNECTING TO DATABASE: ", str(e))
     
-    def save_new_data(self, content):
-        self.connect()
-        try:
-            if type(content) is list:
-                self.session.add_all(content)
+    def update_data(self, entity_list):
+        entity_list_insert = list()
+        for entity in entity_list:
+            returned_data_list = list()
+            for update_rule in self.update_rules["tables"]:
+                if entity.__tablename__ == update_rule["name"]:
+                    returned_data_list = self.session.query(entity.__class__).filter(and_(getattr(entity.__class__, attr_name) == getattr(entity, attr_name) for attr_name in tuple(update_rule["columns"]))).all()
+                    break
+            if len(returned_data_list) == 1:
+                r = returned_data_list[0]
+                for attr, value in entity.__dict__.items():
+                    if attr != "_sa_instance_state":
+                        setattr(r, attr, value)
+                self.session.commit()
             else:
-                self.session.add(content)
+                entity.uuid = str(uuid.uuid4())
+                entity_list_insert.append(entity)
+        return entity_list_insert
+
+    
+    def save_data(self, entity_list):
+        self.connect()       
+        entity_list_insert = self.update_data(entity_list)
+        try:
+            self.session.add_all(entity_list_insert)
             self.session.commit()
             print("Writing to DB is Successful")
         except Exception as e:
             print("EXCEPTION WHILE WRITING TO DATABASE: ", str(e))
     
-    def generate_entity_classes(self, tables, name):
-        path_out_file = "/".join(os.getcwd().split("/")[:-1]) + "/core/" + name + ".py "
+    def generate_entity_classes(self, tables, filename):
+        path_out_file = "/".join(os.getcwd().split("/")[:-1]) + "/core/" + filename + ".py "
         generate_command = "sqlacodegen " + self.dbConnString + " --outfile " + path_out_file
         if len(tables) != 0:
             generate_command += "--tables " + "'" + ",".join(tables) + "'"
@@ -61,10 +89,9 @@ class anet_import:
             print("EXCEPTION WHILE GENERATING ENTITY CLASSES: ", str(e))
     
     def save_log(self, list_row, log_file_name):
-        if type(list_row) != list:
-            raise Exception('Datatype must be list!')
-        if len(list_row) == 0:
-            print("Exiting, Nothing to write log file " + log_file_name)
+        if not self.check_data_type(list_row, "list_row", list):
+            return False
+        if not self.check_data_type(log_file_name, "log_file_name", str):
             return False
         path_file = os.path.join(self.path_log, log_file_name + ".csv")
         df_of_row = pd.DataFrame(list_row)
@@ -74,4 +101,20 @@ class anet_import:
         else:
             log_file = df_of_row
         log_file.to_csv(path_file, index=False)
-        return True
+        print("Log saving successful")
+    
+    def add_update_rule(self, tablename, col_names):
+        if not self.check_data_type(tablename, "tablename", str):
+            return False
+        if not self.check_data_type(col_names, "col_names", list):
+            return False
+        if False in [self.check_data_type(col_name, "elements of col_names", str) for col_name in col_names]:
+            return False
+        self.update_rules["tables"].append({ "name":tablename, "columns":col_names})
+        
+    def print_update_rules(self):
+        print(json.dumps(self.update_rules, indent = 4))
+
+    def clear_update_rules(self):
+        self.update_rules = { "tables": [] }
+        
--- anet-tools/src/examples/read_parse_import_example.py
+++ anet-tools/src/examples/read_parse_import_example.py
@@ -9,6 +9,7 @@ csv_obj = csv(csv_full_path)
 # Read csv file
 csv_obj.read_csv_file()
 
+# Import libraries
 import uuid
 from src.core.anet_import import anet_import
 
@@ -16,7 +17,7 @@ from src.core.anet_import import anet_import
 anet_import = anet_import()
 
 # Update entity classes in models.py file
-anet_import.generate_entity_classes(tables=["people","positions","peoplePositions", "tasks", "organizations", "taskTaskedOrganizations"], name="models")
+anet_import.generate_entity_classes(tables=["people","positions","peoplePositions", "tasks", "organizations", "taskTaskedOrganizations"], filename="models")
 
 # Import Entity Classes
 from src.core.models import Person, Position
@@ -41,22 +42,33 @@ for index, row in csv_obj.df.iterrows():
     # Create and fill entity objects with correct record.
     person = Person()
     person.name = row["employee name"]
-    person.uuid = str(uuid.uuid4())
+    person.rank = row["tashkil rank"]
     person.role = 1
-    
-    position = Position() 
-    position.name = "position of " + row["employee name"]
-    position.uuid = str(uuid.uuid4())
-    position.type = 1
-    position.status = 1
-    
-    position.person = person
-    
-    entity_list.append(position)
+
+    entity_list.append(person)
 
 # Save log files
 anet_import.save_log(list_row = correct_records, log_file_name = "correct_records")
 anet_import.save_log(list_row = erroneous_records, log_file_name = "erroneous_records")
 
+# Print update rule object
+anet_import.print_update_rules()
+
+# Add update rule method
+# This method adds an update rule each time it is called.
+# Multiple update rules can be added for multiple columns of many tables.
+# The tablename property determines for which table the update rule will be added.
+# The col_names property determines which columns of the specified table will be checked for updating. 
+# For example, when tablename = people and col_name = ["name"], 
+# a query is sent according to the name column in the people table in the database for each entity object,
+# and if a record comes, that record is updated, if no record is returned, entity is inserted as new record.
+anet_import.add_update_rule(tablename="people", col_names=["name"])
+
+# Print update rule object to check difference
+anet_import.print_update_rules()
+
+# Clear update rule object
+#anet_import.clear_update_rules()
+
 # Write to DB
-anet_import.save_new_data(entity_list)
+anet_import.save_data(entity_list)
