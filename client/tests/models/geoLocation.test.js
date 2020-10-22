ee8b90d964d2ac163fa55757725da30f11a9f89c
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-10-22 16:32:43 +0300

    NCI-Agency/anet#3085: Fix unit test, add selection test

--- client/tests/models/geoLocation.test.js
+++ client/tests/models/geoLocation.test.js
@@ -1,11 +1,11 @@
 import "@testing-library/jest-dom/extend-expect"
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { Form, Formik } from "formik"
 import React from "react"
 import { convertLatLngToMGRS } from "../../src/geoUtils"
 import GeoLocation from "../../src/pages/locations/GeoLocation"
 
-const GeoLocationTest = format => {
+const GeoLocationTest = () => {
   return (
     <Formik>
       {() => {
@@ -17,7 +17,6 @@ const GeoLocationTest = format => {
         return (
           <Form>
             <GeoLocation
-              locationFormat={format}
               coordinates={coordinates}
               editable
               setFieldTouched={() => {}}
@@ -32,16 +31,22 @@ const GeoLocationTest = format => {
 
 describe("In the location form", () => {
   it("We should be able to see Latitude, Longitude label and input field", () => {
-    render(GeoLocationTest("LAT_LON"))
+    render(GeoLocationTest())
+    // LAT_LON is default
     const latLngLabel = screen.getByText(/Latitude, Longitude/)
     expect(latLngLabel).toBeInTheDocument()
     const latLngInput = screen.getByLabelText(/Latitude, Longitude/)
     expect(latLngInput).toBeInTheDocument()
   })
   it("We should be able to see MGRS label and input field", () => {
-    render(GeoLocationTest("MGRS"))
-    const mgrsLabel = screen.getByText(/MGRS/)
-    expect(mgrsLabel).toBeInTheDocument()
+    render(GeoLocationTest())
+    const formatSelect = screen.getByRole("combobox")
+    expect(formatSelect).toBeInTheDocument()
+    fireEvent.change(formatSelect, {
+      target: {
+        value: "MGRS"
+      }
+    })
     const mgrsInput = screen.getByLabelText(/MGRS/)
     expect(mgrsInput).toBeInTheDocument()
   })
