597ce6a01a0c0d457f5178adccf2493f5e5004b2
okan ayvazoglu <okan.ayvazoglu@simsoft.com.tr>
2020-11-09 11:09:55 +0300

    nci-agency/anet#2959: Add core files

---
+++ anet-tools/src/core/db.py
@@ -0,0 +1,46 @@
+import os
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+class db:
+    def __init__(self, use_env=False, conn_json={}):
+        if use_env:
+            self.dbConnString = os.environ["DB_DRIVER"] + "://" + \
+                                os.environ["ANET_DB_USERNAME"] + ":" + \
+                                os.environ["ANET_DB_PASSWORD"] + "@" + \
+                                "192.168.1.20" + "/" + \
+                                os.environ["ANET_DB_NAME"]
+        else:
+            if conn_json == {}:
+                raise Exception("Connection json is empty")
+            else:
+                self.dbConnString = conn_json["DB_DRIVER"] + "://" + \
+                                    conn_json["DB_USERNAME"] + ":" +  \
+                                    conn_json["PASSWORD"] + "@" + \
+                                    conn_json["DB_SERVER"] + "/" + \
+                                    os.environ["ANET_DB_NAME"]
+        print("db object created")
+    
+    def print_db_env_vars(self):
+        # Print db connection info from os.environ
+        print("ANET_DB_EXPOSED_PORT\t:", os.environ["ANET_DB_EXPOSED_PORT"], "\n"\
+              "ANET_DB_PASSWORD\t:", os.environ["ANET_DB_PASSWORD"],"\n"\
+              "ANET_DB_SERVER\t\t:", os.environ["ANET_DB_SERVER"],"\n"\
+              "ANET_DB_NAME\t\t:", os.environ["ANET_DB_NAME"], "\n"\
+              "ANET_DB_USERNAME\t:", os.environ["ANET_DB_USERNAME"], "\n"\
+              "DB_DRIVER\t\t:", os.environ["DB_DRIVER"])
+    
+    def print_db_connection_string(self):
+        # Print db connection string
+        print(self.dbConnString)
+    
+    def connect(self):
+        try:
+            self.engine = create_engine(self.dbConnString)
+            #print("db engine created")
+            self.session = Session(self.engine)
+            #print("db session created")
+            self.conn = self.engine.connect()
+            print("Successfully connected to the database with conn_str: " + self.dbConnString)
+        except Exception as e:
+            print("EXCEPTION WHILE CONNECTING TO DATABASE: ", str(e))
