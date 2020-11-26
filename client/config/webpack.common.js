87004169a76104a7745f236d7cb764c429ce4d2e
Gertjan van Oosten <gertjan@west.nl>
2020-11-26 12:29:34 +0100

    NCI-Agency/anet#3320: Make introducing new circular dependencies an error now that we've fixed them all

--- client/config/webpack.common.js
+++ client/config/webpack.common.js
@@ -105,8 +105,8 @@ module.exports = {
       new CircularDependencyPlugin({
         // exclude detection of files based on a RegExp
         exclude: /node_modules/,
-        // add warnings to webpack instead of errors
-        failOnError: false,
+        // add errors to webpack instead of warnings
+        failOnError: true,
         // allow import cycles that include an asyncronous import,
         // e.g. via import(/* webpackMode: "weak" */ './file.js')
         allowAsyncCycles: false,
