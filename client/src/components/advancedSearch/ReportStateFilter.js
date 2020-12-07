NCI-Agency/anet

commit 35a28c4705347656f3abf01b9b04260a716b6025

NCI-Agency/anet#3315: Refactor Object.keys to entries where suitable
Also simplify some array manipulation logics

client/src/components/advancedSearch/ReportStateFilter.js
@@ -73,9 +73,9 @@ const ReportStateFilter = ({
           onChange={handleChangeState}
           multiple
         >
-          {Object.keys(Report.STATE_LABELS).map(key => (
+          {Object.entries(Report.STATE_LABELS).map(([key, label]) => (
             <option key={key} value={key}>
-              {Report.STATE_LABELS[key]}
+              {label}
             </option>
           ))}
         </select>
@@ -88,9 +88,9 @@ const ReportStateFilter = ({
               onChange={handleChangeCancelledReason}
             >
               <option value="">Everything</option>
-              {Object.keys(CANCELLATION_REASON_LABELS).map(key => (
+              {Object.entries(CANCELLATION_REASON_LABELS).map((key, label) => (
                 <option key={key} value={key}>
-                  {CANCELLATION_REASON_LABELS[key]}
+                  {label}
                 </option>
               ))}
             </select>

client/src/components/CustomFields.js
@@ -838,8 +838,7 @@ const CustomFields = ({
 }) => {
   return (
     <>
-      {Object.keys(fieldsConfig).map(key => {
-        const fieldConfig = fieldsConfig[key]
+      {Object.entries(fieldsConfig).map(([key, fieldConfig]) => {
         const fieldName = `${parentFieldName}.${key}`
         return invisibleFields.includes(fieldName) ? null : (
           <CustomField
@@ -889,9 +888,8 @@ export const ReadonlyCustomFields = ({
 }) => {
   return (
     <>
-      {Object.keys(fieldsConfig).map(key => {
+      {Object.entries(fieldsConfig).map(([key, fieldConfig]) => {
         const fieldName = `${parentFieldName}.${key}`
-        const fieldConfig = fieldsConfig[key]
         const fieldProps = getFieldPropsFromFieldConfig(fieldConfig)
         const { type } = fieldConfig
         let extraProps = {}

client/src/components/Kanban.js
@@ -65,8 +65,8 @@ const Column = ({ name, tasks }) => {
           segmentFill={entity => {
             const matching = Object.entries(
               Settings.fields.task.customFieldEnum1.enum
-            ).filter(candidate => {
-              return candidate[0] === entity.data.key
+            ).filter(([key, val]) => {
+              return key === entity.data.key
             })
             return matching.length > 0 ? matching[0][1].color : "#bbbbbb"
           }}

client/src/components/ReportStatistics.js
@@ -253,24 +253,28 @@ const ReportStatistics = ({
           <>
             {!hasStatistics && <NoStatisticsRow periods={periods} />}
             {hasStatistics &&
-              Object.keys(REPORT_FIELDS_FOR_STATISTICS).map((key, index) => (
+              Object.entries(
+                REPORT_FIELDS_FOR_STATISTICS
+              ).map(([key, field], index) => (
                 <FieldStatisticsRow
                   key={key}
                   idSuffix={`${key}-${idSuffix}`}
                   fieldName={key}
-                  fieldConfig={REPORT_FIELDS_FOR_STATISTICS[key]}
+                  fieldConfig={field}
                   periods={periods}
                   periodsData={dataPerPeriod}
                   isFirstRow={index === 0}
                 />
               ))}
             {hasStatistics &&
-              Object.keys(customFieldsConfig || {}).map((key, index) => (
+              Object.entries(
+                customFieldsConfig || {}
+              ).map(([key, field], index) => (
                 <FieldStatisticsRow
                   key={key}
                   idSuffix={`${key}-${idSuffix}`}
                   fieldName={`${CUSTOM_FIELDS_KEY}.${key}`}
-                  fieldConfig={customFieldsConfig[key]}
+                  fieldConfig={field}
                   periods={periods}
                   periodsData={dataPerPeriod}
                   isFirstRow={

client/src/components/SearchFilters.js
@@ -525,7 +525,7 @@ export const searchFilters = function() {
     filters: taskFilters()
   }
 
-  for (const [, filtersForType] of Object.entries(filters)) {
+  for (const filtersForType of Object.values(filters)) {
     filtersForType.filters.Status = StatusFilter
   }
 
@@ -646,8 +646,7 @@ export const deserializeQueryParams = (
     })
     const ALL_FILTERS = searchFilters()
     const filterDefs = ALL_FILTERS[objType].filters
-    Object.keys(filterDefs).map(filterKey => {
-      const filterDef = filterDefs[filterKey]
+    Object.entries(filterDefs).map(([filterKey, filterDef]) => {
       const deser = filterDef.deserializer(
         filterDef.props,
         queryParams,

client/src/components/advancedSelectWidget/AdvancedSelect.js
@@ -33,13 +33,13 @@ const FilterAsNav = ({ items, currentFilter, handleOnClick }) =>
   hasMultipleItems(items) && (
     <Col md={2} xsHidden smHidden>
       <ul className="advanced-select-filters" style={{ paddingInlineStart: 0 }}>
-        {Object.keys(items).map(filterType => (
+        {Object.entries(items).map(([filterType, filter]) => (
           <li
             key={filterType}
             className={currentFilter === filterType ? "active" : null}
           >
             <Button bsStyle="link" onClick={() => handleOnClick(filterType)}>
-              {items[filterType].label}
+              {filter.label}
             </Button>
           </li>
         ))}
@@ -58,9 +58,9 @@ const FilterAsDropdown = ({ items, handleOnChange }) =>
       <p style={{ padding: "5px 0" }}>
         Filter:
         <select onChange={handleOnChange} style={{ marginLeft: "5px" }}>
-          {Object.keys(items).map(filterType => (
+          {Object.entries(items).map(([filterType, filter]) => (
             <option key={filterType} value={filterType}>
-              {items[filterType].label}
+              {filter.label}
             </option>
           ))}
         </select>

client/src/components/advancedSelectWidget/MultiTypeAdvancedSelectComponent.js
@@ -1,4 +1,5 @@
 import { SEARCH_OBJECT_LABELS, SEARCH_OBJECT_TYPES } from "actions"
+import AdvancedMultiSelect from "components/advancedSelectWidget/AdvancedMultiSelect"
 import {
   LocationOverlayRow,
   OrganizationOverlayRow,
@@ -7,7 +8,6 @@ import {
   ReportDetailedOverlayRow,
   TaskSimpleOverlayRow
 } from "components/advancedSelectWidget/AdvancedSelectOverlayRow"
-import AdvancedMultiSelect from "components/advancedSelectWidget/AdvancedMultiSelect"
 import AdvancedSingleSelect from "components/advancedSelectWidget/AdvancedSingleSelect"
 import ButtonToggleGroup from "components/ButtonToggleGroup"
 import Model from "components/Model"
@@ -154,7 +154,7 @@ const MultiTypeAdvancedSelectComponent = ({
         <ButtonToggleGroup value={entityType} onChange={changeEntityType}>
           {Object.entries(ENTITY_TYPES)
             .filter(([, et]) => entityTypes.includes(et))
-            .map(([key, entityName], value) => {
+            .map(([key, entityName]) => {
               const entityLabel = SEARCH_OBJECT_LABELS[SEARCH_OBJECT_TYPES[key]]
               return (
                 <Button key={entityName} value={entityName}>

client/src/components/aggregations/utils.js
@@ -89,13 +89,13 @@ export const countPerValueAggregation = (fieldName, fieldConfig, data) => {
   const legendColors = _clone(CHART_COLORS)
   const legend = fieldConfig?.choices || {}
   const legendKeys = !_isEmpty(legend)
-    ? Object.keys(legend)
-    : Object.keys(counters)
+    ? Object.entries(legend)
+    : Object.entries(counters)
   legendKeys.forEach(
-    key =>
+    ([key, val]) =>
       (legend[key] = {
-        label: legend[key]?.label || key,
-        color: legend[key]?.color || legendColors.shift()
+        label: val?.label || key,
+        color: val?.color || legendColors.shift()
       })
   )
   legend.null = { label: "Unspecified", color: "#bbbbbb" }

commit f7f080e3d196af68f2040894c12d7d0bb13375a1

Fix error in advanced search cancellation labels
Problem was introduced in commit 35a28c4705

client/src/components/advancedSearch/ReportStateFilter.js
@@ -88,11 +88,13 @@ const ReportStateFilter = ({
               onChange={handleChangeCancelledReason}
             >
               <option value="">Everything</option>
-              {Object.entries(CANCELLATION_REASON_LABELS).map((key, label) => (
-                <option key={key} value={key}>
-                  {label}
-                </option>
-              ))}
+              {Object.entries(CANCELLATION_REASON_LABELS).map(
+                ([key, label]) => (
+                  <option key={key} value={key}>
+                    {label}
+                  </option>
+                )
+              )}
             </select>
           </div>
         )}