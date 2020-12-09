ecc71d6ce412d47957bf4e58ded14269762be8a5
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2020-12-09 11:40:33 +0300

    Order and group stories

--- client/stories/Page.stories.js
+++ client/stories/Page.stories.js
@@ -1,10 +1,9 @@
 import React from "react"
-
-import { Page } from "./Page"
 import * as HeaderStories from "./Header.stories"
+import { Page } from "./Page"
 
 export default {
-  title: "Example/Page",
+  title: "Storybook/Examples/Page",
   component: Page
 }
 
