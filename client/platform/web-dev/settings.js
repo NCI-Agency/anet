f4c3754fefbf0feeff75233b12cceb0f3985cf6d
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2021-01-04 17:45:53 +0300

    Fetch version in dev-mode as well

--- client/platform/web-dev/settings.js
+++ client/platform/web-dev/settings.js
@@ -1,8 +1,21 @@
+import { gql } from "apollo-boost"
 import { loadFileAjaxSync } from "../utils"
 
 const Settings = JSON.parse(
   loadFileAjaxSync("/api/admin/dictionary", "application/json")
 )
-const Version = "Dev-Mode"
+const GQL_GET_VERSION_INFO = gql`
+  query {
+    projectVersion
+  }
+`
+
+const Version = JSON.parse(
+  loadFileAjaxSync(
+    "/graphql",
+    "application/json",
+    JSON.stringify({ query: GQL_GET_VERSION_INFO })
+  )
+).data.projectVersion
 
 export { Version, Settings as default }
