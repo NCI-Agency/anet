73e6b27ed42701baf598632c3ebf48c00f82f410
Cemalettin Taş <cemalettin.tas@simsoft.com.tr>
2021-02-01 14:39:55 +0300

    nci-agency/anet#2959: Add support for  mssql

--- anet-tools/src/core/db.py
+++ anet-tools/src/core/db.py
@@ -7,24 +7,33 @@ from sqlalchemy.orm import Session
 class db:
     def __init__(self, use_env=False, conn_json={}):
         if use_env:
-            os.environ["ANET_DB_SERVER"] = os.environ["LOCAL_IP"] if os.environ[
-                "ANET_DB_SERVER"] == "localhost" else os.environ["ANET_DB_SERVER"]
+            if os.environ["DB_DRIVER"] == "sqlserver":
+                os.environ["DB_DRIVER"] = "mssql+pyodbc"
+
+            if os.environ["ANET_DB_SERVER"] == "localhost":
+                os.environ["ANET_DB_SERVER"] = os.environ["LOCAL_IP"]
+
             self.dbConnString = os.environ["DB_DRIVER"] + "://" + \
                 os.environ["ANET_DB_USERNAME"] + ":" + \
                 os.environ["ANET_DB_PASSWORD"] + "@" + \
                 os.environ["ANET_DB_SERVER"] + ":" + os.environ["ANET_DB_EXPOSED_PORT"] + "/" + \
                 os.environ["ANET_DB_NAME"]
+            if os.environ["DB_DRIVER"] == "mssql+pyodbc":
+                self.dbConnString += "?" + "driver=ODBC+Driver+17+for+SQL+Server"
+
         else:
             if conn_json == {}:
                 raise Exception("Connection json is empty")
             else:
+                if conn_json["DB_DRIVER"] == "sqlserver":
+                    conn_json["DB_DRIVER"] = "mssql+pyodbc"
                 self.dbConnString = conn_json["DB_DRIVER"] + "://" + \
                     conn_json["DB_USERNAME"] + ":" +  \
                     conn_json["DB_PASSWORD"] + "@" + \
                     conn_json["DB_SERVER"] + ":" + conn_json["DB_PORT"] + "/" + \
                     conn_json["DB_NAME"]
                 if conn_json["DB_DRIVER"] == "mssql+pyodbc":
-                    self.dbConnString += "?" + conn_json["DRIVER"]
+                    self.dbConnString += "?" + "driver=ODBC+Driver+17+for+SQL+Server"
 
         print("db object created")
 
