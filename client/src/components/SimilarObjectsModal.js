f7bd482d01941206ff098e19bdf7b44e75ec0eb0
Anıl Sönmez <anil.sonmez@simsoft.com.tr>
2021-03-17 14:29:09 +0300

    NCI-Agency/anet#106: Add SimilarObjectsModal to display possible duplicates

---
+++ client/src/components/SimilarObjectsModal.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { Button, Modal } from "react-bootstrap"
+import PropTypes from "prop-types"
+import PositionTable from "components/PositionTable"
+import PeopleTable from "./PeopleTable"
+import LocationTable from "./LocationTable"
+
+const DEFAULT_PAGESIZE = 10
+
+const SimilarObjectsModal = ({ objectType, userInput, onCancel }) => {
+  const queryParams = Object.assign({
+    sortBy: "NAME",
+    sortOrder: "ASC",
+    pageNum: 0,
+    pageSize: DEFAULT_PAGESIZE,
+    text: userInput
+  })
+
+  return (
+    <Modal show={true} onHide={onCancel}>
+      <Modal.Header closeButton>
+        <Modal.Title>Possible Duplicates</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {objectType === "Person" && (
+          <PeopleTable queryParams={queryParams} id="similar-people-table" />
+        )}
+        {objectType === "Location" && (
+          <LocationTable
+            queryParams={queryParams}
+            id="similar-locations-table"
+          />
+        )}
+        {objectType === "Position" && (
+          <PositionTable
+            queryParams={queryParams}
+            id="similar-positions-table"
+          />
+        )}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button className="pull-left" onClick={onCancel}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+SimilarObjectsModal.propTypes = {
+  objectType: PropTypes.string,
+  userInput: PropTypes.string,
+  onCancel: PropTypes.func
+}
+
+export default SimilarObjectsModal
