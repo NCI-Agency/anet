c3e3bb63e87ee800f410d675b93586c038162bc7
fb-simsoft <furkan.bagci@simsoft.com.tr>
2021-04-05 19:16:09 +0300

    NCI-Agency/anet#2959: Simplify api class

--- anet-tools/src/core/api/anet_import.py
+++ anet-tools/src/core/api/anet_import.py
@@ -3,166 +3,42 @@ import json
 import os
 
 import pandas as pd
-from sqlalchemy import exc
 
-from src.core.business_logic.base.base_methods import base_methods
-from src.core.utils.source.txt import txt
-from src.core.business_logic.base.db import db
-from src.core.model.base.base_model import BaseModel
+from src.core.database.db import db
 
 
 class anet_import(db):
-    """ This class is the user interface of import framework. 
-        It helps user to import data.
+    """ User interface of import framework.
     """
-    def __init__(self, hashfile_fullpath, logfile_fullpath, use_env=False, conn_json={}):
+
+    def __init__(self, hash_path, log_path, use_env = False, conn_json = {}):
         super().__init__(use_env=use_env, conn_json=conn_json)
-        self.path_log = logfile_fullpath
-        self.path_hashfile = hashfile_fullpath
+        self.log_path = log_path
+        self.hash_full_path = os.path.join(hash_path, "hashvalues.txt")
         self.update_rules = {"tables": []}
+        self.utc_now = datetime.datetime.now()
+        self.utc_now_str = str(self.utc_now).replace("-", "_").replace(" ", "_")
 
-    def print_info(self, content):
-        if self.verbose:
-            print(f"{content}")
-
-    def initialize_successful_entity_list_json(self):
-        self.successful_entity_list_json = list()
-
-    def initialize_unsuccessful_entity_list_json(self):
-        self.unsuccessful_entity_list_json = list()
-
-    def append_successful_entity_list_json(self, entity_json):
-        self.successful_entity_list_json.append(entity_json)
-
-    def append_unsuccessful_entity_list_json(self, entity_json):
-        self.unsuccessful_entity_list_json.append(entity_json)
-
-    def set_base_session(self):
-        BaseModel.set_session(self.session)
-
-    def sqlalc_exc(self, e, entity_json, from_relation_table):
-        if from_relation_table:
-            exc_reason = e
+    def import_objects(self):
+        if len(self.new_objects) == 0:
+            print("There is not any entity to import!")
         else:
-            exc_reason = f"{str(type(e))} --> {str(e.args)}"
-        self.print_info(exc_reason)
-        entity_json["row"]["exception_reason"] = exc_reason
-        self.append_unsuccessful_entity_list_json(entity_json)
-        self.print_info(f"Unsuccessfull: {len(self.unsuccessful_entity_list_json)}")
-
-    def write_data(self, entity_list_json):
-        if len(entity_list_json) == 0:
-            print("There is not any valid entity to import!")
-            return 
-        counter = 0
-        for entity_json in entity_list_json:
-            utc_now = datetime.datetime.now()
-            try:
-                entity = entity_json["entity"]
-                entity.session.begin_nested()
-                entity.import_entity(utc_now, self.update_rules)
-                self.append_successful_entity_list_json(entity_json)
-                self.print_info(f"Successfull: {len(self.successful_entity_list_json)}")
-                entity.session.commit()
-            except exc.SQLAlchemyError as e:
-                entity.session.rollback()
-                self.sqlalc_exc(e=e, entity_json=entity_json,
-                                from_relation_table=False)
-            except Exception as e:
-                entity.session.rollback()
-                self.sqlalc_exc(e=e, entity_json=entity_json,
-                                from_relation_table=True)
-            if not self.verbose and counter % 10 == 0:
-                print(f"Importing {counter}/{len(entity_list_json)} passed", end="\r")
-            counter = counter + 1
-            entity.session.commit()
-        print(f"Importing {counter}/{len(entity_list_json)} passed")
-        print(f"Import is complete for entities whose table name is {entity_json['entity'].__tablename__}")
-
-    def write_unsuccessful_records_to_csv(self):
-        if len(self.unsuccessful_entity_list_json) == 0:
-            print("All entities imported to db successfully")
-            print("\n")
-        else:
-            if self.tinylog:
-                exc_list = list()
-                for entity_json in self.unsuccessful_entity_list_json:
-                    exc_list.append({"data": str(vars(entity_json["entity"])),"exception": str(entity_json["row"]["exception_reason"])})
-                unsuccessful_entity_df = pd.DataFrame(exc_list)
-            else:
-                unsuccessful_entity_df = pd.DataFrame()
-                for entity_json in self.unsuccessful_entity_list_json:
-                    unsuccessful_entity_df = pd.concat(
-                        [unsuccessful_entity_df, pd.DataFrame(entity_json["row"]).T])
-            utc_now_str = str(datetime.datetime.now()).replace(" ", "_")
-            filename = "unsuccessful_import_" + self.unsuccessful_entity_list_json[0]["entity"].__tablename__ + "_" + utc_now_str
-            fullpath = os.path.join(self.path_log, filename + ".csv")
-            unsuccessful_entity_df.to_csv(fullpath)
-            print(f"Importing {len(self.unsuccessful_entity_list_json)} entities unsuccessful.")
-            print(f"It is written to log file named: {filename}")
-            print("\n")
-
-    def generate_new_empty_file_if_not_exists(self, path):
-        if not os.path.isfile(path):
-            print(f"Generating new file: {'/'.join(path.split('/')[4:])}")
-            print("\n")
-            # this call requires root privileges on OSX
-            os.mknod(path)
-
-    def read_from_hashlist_file(self, fullpath_hashfile):
-        self.generate_new_empty_file_if_not_exists(fullpath_hashfile)
-        hash_txt_obj = txt(fullpath_hashfile)
-        hash_txt_obj.read_txt_file()
-        print("Reading hash list from file successfull")
-        print("\n")
-        return hash_txt_obj
-
-    def write_hashlist_to_file(self, fullpath_hashfile):
-        with open(fullpath_hashfile, "w+") as h_f:
-            h_f.write("\n".join(self.hash_list))
-        print("Hash file updated with hash of successful entities")
-        print("\n")
-
-    def update_hash_list_from_file(self):
-        fullpath_hashfile = os.path.join(self.path_hashfile, "hashvalues.txt")
-        hash_txt_obj = self.read_from_hashlist_file(fullpath_hashfile)
-        self.hash_list = [h.replace(",", "").replace("\n", "")
-                          for h in hash_txt_obj.content]
-
-    def exclude_old_entity_compare_hashes(self, entity_list_json):
-        self.update_hash_list_from_file()
-        entity_list_json_old_excluded = list()
-        count_old_record = 0
-        for e in entity_list_json:
-            if str(pd.util.hash_pandas_object(e["row"]).sum()) not in self.hash_list:
-                entity_list_json_old_excluded.append(e)
-            else:
-                count_old_record = count_old_record + 1
-        if count_old_record != 0:
-            print(f"{count_old_record} entity will not be processed again since they were processed in the past!")
-            print("\n")
-        else:
-            print("No entity excluded, all entities are new.")
-            print("\n")
-        return entity_list_json_old_excluded
-
-    def exclude_incorrect_entity(self, entity_list_json):
-        entity_list_json_incorrect_excluded = list()
-        for e in entity_list_json:
-            if type(e["row"]) == pd.core.series.Series:
-                entity_list_json_incorrect_excluded.append(e)
-            else:
-                e["row"]["exception_reason"] = "row data type must be pandas.core.series.Series"
-                self.append_unsuccessful_entity_list_json(e)
-        return entity_list_json_incorrect_excluded
-
-    def write_successful_entities_hashfile(self):
-        # This line could be removed in future
-        self.update_hash_list_from_file()
-        for entity_json in self.successful_entity_list_json:
-            self.hash_list.append(str(pd.util.hash_pandas_object(entity_json["row"]).sum()))
-        fullpath_hashfile = os.path.join(self.path_hashfile, "hashvalues.txt")
-        self.write_hashlist_to_file(fullpath_hashfile)
+            for ind, obj in enumerate(self.new_objects):
+                obj_c = obj.get_fresh_one()
+                try:
+                    self.session.begin_nested()
+                    obj.import_entity(self.utc_now, self.update_rules, self.session)
+                    self.session.commit()
+                    self.imported_objects.append(obj_c)
+                except Exception as e:
+                    self.session.rollback()
+                    obj_c.exc_reason = e
+                    self.not_imported_objects.append(obj_c)
+                if ind % 10 == 0:
+                    print(f"Import {ind}/{len(self.new_objects)} passed", end="\r")
+            self.session.commit()
+            print(f"Import {len(self.new_objects)}/{len(self.new_objects)} passed")
+            print(f"Import is complete!")
 
     def add_update_rule(self, tablename, col_names):
         self.update_rules["tables"].append({"name": tablename, "columns": col_names})
@@ -173,27 +49,94 @@ class anet_import(db):
     def clear_update_rules(self):
         self.update_rules = {"tables": []}
 
-    def save_data(self, entity_list_json, verbose=True, remember_with_hash=True, write_unsuccessful=True, tinylog=True):
-        print("***DATA IMPORT AND MANIPULATION FRAMEWORK***")
-        self.verbose = verbose
-        self.tinylog = tinylog
-        self.connect()
-        self.set_base_session()
-        self.initialize_successful_entity_list_json()
-        self.initialize_unsuccessful_entity_list_json()
-        entity_list_json_incorrect_excluded = self.exclude_incorrect_entity(
-            entity_list_json)
-        if remember_with_hash:
-            entity_list_json_old_excluded = self.exclude_old_entity_compare_hashes(
-                entity_list_json_incorrect_excluded)
-            self.write_data(entity_list_json_old_excluded)
+    def generate_new_hash_file(self):
+        # If hashfile does not exists, generate new one
+        if not os.path.isfile(self.hash_full_path):
+            with open(self.hash_full_path, "w+") as t:
+                pass
+
+    def read_hash(self):
+        self.generate_new_hash_file()
+        with open(self.hash_full_path, "r") as hash_file:
+            hash_values = hash_file.read()
+        self.hash_list = hash_values.split(", ")
+        print("Read hashes from file!")
+
+    def remember_with_hash(self):
+        if not self.remember_hash:
+            self.new_objects = self.objects_from_user
         else:
-            self.write_data(entity_list_json_incorrect_excluded)
-        if write_unsuccessful:
-            self.write_unsuccessful_records_to_csv()
-        if remember_with_hash:
-            self.write_successful_entities_hashfile()
-        print(f"Invalid: {len(entity_list_json)-len(entity_list_json_incorrect_excluded)}")
-        print(f"Successful: {len(entity_list_json_incorrect_excluded)-len(self.unsuccessful_entity_list_json)}")
-        print(f"Unsuccessful: {len(self.unsuccessful_entity_list_json)}")
-        print()
+            self.read_hash()
+            hashes = self.hashes_from_obj_list(self.objects_from_user)
+            for index, value in hashes.items():
+                if str(value) in self.hash_list:
+                    self.previous_objects.append(self.objects_from_user[index])
+                else:
+                    self.new_objects.append(self.objects_from_user[index])
+
+    def update_parameters(self, **kwargs):
+        # Update api parameters comes from user
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        # Update internal api parameters
+        setattr(self, "imported_objects", list())
+        setattr(self, "not_imported_objects", list())
+        setattr(self, "previous_objects", list())
+        setattr(self, "new_objects", list())
+
+    def write_objects_to_csv(self, obj_list, name):
+        if obj_list:
+            pd.DataFrame([vars(obj) for obj in obj_list]) \
+                .drop(columns = ["_sa_instance_state"], errors = 'ignore') \
+                    .to_csv(os.path.join(self.log_path, name + self.utc_now_str + ".csv"))
+
+    def log_result(self):
+        if self.log:
+            self.write_objects_to_csv(self.imported_objects, "imported")
+            self.write_objects_to_csv(self.not_imported_objects, "not_imported")
+            self.write_objects_to_csv(self.previous_objects, "previous")
+            print()
+            print(f"Imported: {len(self.imported_objects)}")
+            print(f"Not_imported: {len(self.not_imported_objects)}")
+            print(f"Previous: {len(self.previous_objects)}")
+
+    def hashes_from_obj_list(self, obj_list):
+        if obj_list:
+            list_of_drop_cols = ["_sa_instance_state", "person",
+                                    "people", "organization",
+                                    "location", "organization1",
+                                    "positions", "reports", "parent"
+                                    "uuid"]
+            models_df = pd.DataFrame([vars(obj) for obj in obj_list]) \
+                        .drop(columns = list_of_drop_cols, errors = 'ignore')
+            hashes = pd.util.hash_pandas_object(models_df, index = False)
+            return hashes
+
+    def write_hash(self):
+        if self.imported_objects and self.remember_hash:
+            hashes = self.hashes_from_obj_list(self.imported_objects)
+            for index, value in hashes.items():
+                self.hash_list.append(str(value))
+            with open(self.hash_full_path, "w") as output:
+                output.write(", ".join(self.hash_list))
+
+    def check_parameters(self):
+        if not self.objects_from_user:
+            raise Exception("There is no object to import!")
+
+    def start_import(self, objects, remember_hash = False, log = True ):
+        # Framework is started
+        print("\n*** DATA IMPORT AND MANIPULATION FRAMEWORK ***\n")
+        # Update api parameters
+        self.update_parameters(objects_from_user = objects, remember_hash = remember_hash, log = log)
+        self.check_parameters()
+        # Exclude previously processed models 
+        self.remember_with_hash()
+        # Connect to db and initializa a session
+        self.connect()
+        # Import objects
+        self.import_objects()
+        # Write hash of imported ones
+        self.write_hash()
+        # Export result as log files
+        self.log_result()
