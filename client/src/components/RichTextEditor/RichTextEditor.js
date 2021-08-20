74b9acced03b55e94e896bbf9426a101d9bb05be
Anıl Sönmez <anil.sonmez@simsoft.com.tr>
2021-08-20 16:50:42 +0300

    NCI-Agency/anet#3762: Add serialize and deserialize methods

--- client/src/components/RichTextEditor/RichTextEditor.js
+++ client/src/components/RichTextEditor/RichTextEditor.js
@@ -1,13 +1,17 @@
+import escapeHtml from "escape-html"
 import PropTypes from "prop-types"
 import React, { useCallback, useMemo, useState } from "react"
-import { createEditor } from "slate"
+import { createEditor, Text } from "slate"
 import { withHistory } from "slate-history"
 import { Editable, Slate, withReact } from "slate-react"
 import Toolbar from "./Toolbar"
 
-const SlateEditor = () => {
+const SlateEditor = ({ value, onChange }) => {
   const editor = useMemo(() => withReact(withHistory(createEditor())), [])
+  const document = new DOMParser().parseFromString(value, "text/html")
   const [value, setValue] = useState(initialValueSlate)
+  const deserialized = deserialize(document.body)
+  const [slateValue, setSlateValue] = useState(deserialized)
 
   const renderElement = useCallback(props => <Element {...props} />, [])
   const renderLeaf = useCallback(props => <Leaf {...props} />, [])
@@ -15,13 +19,98 @@ const SlateEditor = () => {
   return (
     <Slate
       editor={editor}
-      value={value}
-      onChange={newValue => setValue(newValue)}
+      value={slateValue}
+      onChange={newValue => {
+        setSlateValue(newValue)
+        onChange(serialize(editor))
+      }}
     >
       <Toolbar />
       <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
     </Slate>
   )
+}
+
+SlateEditor.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
+}
+
+const serialize = node => {
+  if (Text.isText(node)) {
+    let string = escapeHtml(node.text)
+    if (node.bold) {
+      string = `<strong>${string}</strong>`
+    }
+    if (node.italic) {
+      string = `<em>${string}</em>`
+    }
+    if (node.code) {
+      string = `<code>${string}</code>`
+    }
+    if (node.underline) {
+      string = `<u>${string}</u>`
+    }
+    if (node.strikethrough) {
+      string = `<strike>${string}</strike>`
+    }
+    return string
+  }
+  const children = node.children.map(n => serialize(n)).join("")
+  switch (node.type) {
+    case "paragraph":
+      return `<p>${children}</p>`
+    case "block-quote":
+      return `<blockquote>${children}</blockquote>`
+    case "bulleted-list":
+      return `<ul>${children}</ul>`
+    case "heading-one":
+      return `<h1>${children}</h1>`
+    case "heading-two":
+      return `<h2>${children}</h2>`
+    case "heading-three":
+      return `<h3>${children}</h3>`
+    case "list-item":
+      return `<li>${children}</li>`
+    case "numbered-list":
+      return `<ol>${children}</ol>`
+    default:
+      return children
+  }
+}
+
+const deserialize = element => {
+  const children = Array.from(element.childNodes).map(deserialize)
+  switch (element.nodeName) {
+    case "BODY":
+      return jsx("fragment", {}, children)
+    case "P":
+      return jsx("element", { type: "paragraph" }, children)
+    case "STRONG":
+      return jsx("text", { bold: true }, children)
+    case "EM":
+      return jsx("text", { italic: true }, children)
+    case "U":
+      return jsx("text", { underline: true }, children)
+    case "STRIKE":
+      return jsx("text", { strikethrough: true }, children)
+    case "H1":
+      return jsx("element", { type: "heading-one" }, children)
+    case "H2":
+      return jsx("element", { type: "heading-two" }, children)
+    case "H3":
+      return jsx("element", { type: "heading-three" }, children)
+    case "BLOCKQUOTE":
+      return jsx("element", { type: "block-quote" }, children)
+    case "LI":
+      return jsx("element", { type: "list-item" }, children)
+    case "UL":
+      return jsx("element", { type: "bulleted-list" }, children)
+    case "OL":
+      return jsx("element", { type: "numbered-list" }, children)
+    default:
+      return element.textContent
+  }
 }
 
 const Element = ({ attributes, children, element }) => {
