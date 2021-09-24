aca25c80f2010452acc8f6b1a87e84df3a4d0310
Anıl Sönmez <anil.sonmez@simsoft.com.tr>
2021-09-24 11:25:54 +0300

    NCI-Agency/anet#3858: Convert general banner level to dropdown

--- client/src/components/GeneralBanner.js
+++ client/src/components/GeneralBanner.js
@@ -1,17 +1,24 @@
 import PropTypes from "prop-types"
 import React from "react"
 
+export const GENERAL_BANNER_LEVELS = {
+  NOTICE: { value: "notice", className: "alert-info", label: "Blue" },
+  SUCCESS: { value: "success", className: "alert-success", label: "Green" },
+  ERROR: { value: "error", className: "alert-danger", label: "Red" },
+  ALERT: { value: "alert", className: "alert-warning", label: "Yellow" }
+}
+
 function bannerClassName(level) {
-  let output = "general-banner"
+  let output = "general-banner "
   switch (level) {
-    case "notice":
-      return (output += " alert-info")
-    case "success":
-      return (output += " alert-success")
-    case "error":
-      return (output += " alert-danger")
-    case "alert":
-      return (output += " alert-warning")
+    case GENERAL_BANNER_LEVELS.NOTICE.value:
+      return (output += ` ${GENERAL_BANNER_LEVELS.NOTICE.className}`)
+    case GENERAL_BANNER_LEVELS.SUCCESS.value:
+      return (output += ` ${GENERAL_BANNER_LEVELS.SUCCESS.className}`)
+    case GENERAL_BANNER_LEVELS.ERROR.value:
+      return (output += ` ${GENERAL_BANNER_LEVELS.ERROR.className}`)
+    case GENERAL_BANNER_LEVELS.ALERT.value:
+      return (output += ` ${GENERAL_BANNER_LEVELS.ALERT.className}`)
     default:
       return output
   }
