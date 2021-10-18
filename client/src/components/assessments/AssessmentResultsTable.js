6c7134ca92358d6d64bfe02fb460a2ef6c964ce7
Gertjan van Oosten <gertjan@west.nl>
2021-10-18 15:27:47 +0200

    NCI-Agency/anet#3698: Make assessment results table borderless

--- client/src/components/assessments/AssessmentResultsTable.js
+++ client/src/components/assessments/AssessmentResultsTable.js
@@ -132,6 +132,7 @@ const AssessmentResultsTable = ({
           >
             <PeriodsNavigation offset={offset} onChange={setOffset} />
             <Table
+              borderless
               responsive
               className="assessments-table"
               style={{ tableLayout: "fixed" }}
