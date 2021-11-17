2d05450baffd8e6f3aa57ccc0b82825cdbaa8d98
Gertjan van Oosten <gertjan@west.nl>
2021-11-17 11:58:48 +0100

    Fix quotes in test titles

--- client/tests/webdriver/baseSpecs/userLogin.spec.js
+++ client/tests/webdriver/baseSpecs/userLogin.spec.js
@@ -10,7 +10,7 @@ describe("Anet home page", function() {
 })
 
 describe("Anet default user login", () => {
-  it('Default user is logged in"', () => {
+  it("Default user is logged in", () => {
     Home.open()
     const securityText = Home.securityBanner.getText()
     const defaultUserValue = "DEMO USE ONLY || ERINSON, Erin"
@@ -19,7 +19,7 @@ describe("Anet default user login", () => {
 })
 
 describe("Anet super user login", () => {
-  it('Super user is logged in"', () => {
+  it("Super user is logged in", () => {
     Home.openAsSuperUser()
     const securityText = Home.securityBanner.getText()
     const superUserValue = "DEMO USE ONLY || BECCABON, Rebecca"
@@ -28,7 +28,7 @@ describe("Anet super user login", () => {
 })
 
 describe("Anet admin user login", () => {
-  it('Admin user is logged in"', () => {
+  it("Admin user is logged in", () => {
     Home.openAsAdminUser()
     const securityText = Home.securityBanner.getText()
     const adminUserValue = "DEMO USE ONLY || DMIN, Arthur"
