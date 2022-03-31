714389eb4520c3eaa6d6d460c5b385401ea5df76
Gertjan van Oosten <gertjan@west.nl>
2022-03-31 11:37:16 +0200

    AB#388: Fix Storybook

--- client/platform/storybook/settings.js
+++ client/platform/storybook/settings.js
@@ -2,12 +2,13 @@ import jsyaml from "js-yaml"
 import { loadFileAjaxSync } from "../utils"
 
 console.log("Using dictionary file " + process.env.STORYBOOK_ANET_DICTIONARY)
-const anetConfig = jsyaml.load(
+const anetDictionary = jsyaml.load(
   loadFileAjaxSync(
     process.env.STORYBOOK_ANET_DICTIONARY,
     "text/yaml; charset=UTF-8"
   )
 )
-const Settings = anetConfig
+const Settings = anetDictionary
+Settings.keycloakConfiguration = {}
 
 export default Settings
