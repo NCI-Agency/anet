ed0a6e12afe8177e914aa88b1681ca694c8167c2
Gertjan van Oosten <gertjan@west.nl>
2022-06-16 16:53:19 +0200

    Extract Babel, ESLint, Jest and Prettier config
    Configurations are now in separate files instead of package.json.
    Command-line for eslint en prettier is also simplified.

---
+++ client/.eslintrc.js
@@ -0,0 +1,98 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jquery: true
+  },
+  extends: [
+    "plugin:chai-expect/recommended",
+    "react-app",
+    "standard",
+    "standard-react"
+  ],
+  globals: {
+    browser: "readonly"
+  },
+  parser: "@babel/eslint-parser",
+  plugins: [
+    "chai-expect",
+    "flowtype",
+    "import",
+    "jsx-a11y",
+    "node",
+    "promise",
+    "react",
+    "react-hooks"
+  ],
+  rules: {
+    eqeqeq: ["error", "always"],
+    "import/no-anonymous-default-export": "off",
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index"
+        ]
+      }
+    ],
+    indent: [
+      "warn",
+      2,
+      {
+        SwitchCase: 1,
+        ignoredNodes: [
+          "JSXElement",
+          "JSXElement > *",
+          "JSXAttribute",
+          "JSXIdentifier",
+          "JSXNamespacedName",
+          "JSXMemberExpression",
+          "JSXSpreadAttribute",
+          "JSXExpressionContainer",
+          "JSXOpeningElement",
+          "JSXClosingElement",
+          "JSXOpeningFragment",
+          "JSXOpeningFragment",
+          "JSXClosingFragment",
+          "JSXText",
+          "JSXEmptyExpression",
+          "JSXSpreadChild"
+        ]
+      }
+    ],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "multiline-ternary": "off",
+    "quote-props": ["warn", "as-needed"],
+    quotes: [
+      "warn",
+      "double",
+      {
+        avoidEscape: true
+      }
+    ],
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react/jsx-closing-bracket-location": "warn",
+    "react/jsx-closing-tag-location": "warn",
+    "react/jsx-handler-names": "off",
+    "react/jsx-indent": ["warn", 2],
+    "react/jsx-indent-props": ["warn", 2],
+    "react/no-did-update-set-state": "warn",
+    semi: ["warn", "never"],
+    "space-before-function-paren": ["warn", "never"],
+    curly: ["error", "all"],
+    "brace-style": [
+      "error",
+      "1tbs",
+      {
+        allowSingleLine: false
+      }
+    ]
+  }
+}
