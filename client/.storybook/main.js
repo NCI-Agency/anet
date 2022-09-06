968891c3ef2c2f039bf8d0f6a0cea00588cf2ec5
Anıl Sönmez <anil.sonmez@simsoft.com.tr>
2022-09-06 16:26:59 +0300

    AZ#561: Fix storybook scripts

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -9,15 +9,13 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  staticDirs: [
-    "../.."
-  ],
+  staticDirs: ["../stories/dictionaryForStories"],
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-links",
     "@storybook/addon-postcss"
   ],
-  webpackFinal: async (config, { configType }) => {
+  webpackFinal: async(config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
     // You can change the configuration based on that.
     // 'PRODUCTION' is used when building the static version of storybook.
