a10b5c8a387a6bb3a6c7b126cfca4c706ea25246
Anıl Sönmez <anil.sonmez@simsoft.com.tr>
2022-12-16 15:15:13 +0300

    AB#628: Add client side tests for creating a new organization

---
+++ client/tests/webdriver/pages/showOrganization.page.js
@@ -0,0 +1,24 @@
+import Page from "./page"
+
+class ShowOrganization extends Page {
+  get alertSuccess() {
+    return browser.$(".alert-success")
+  }
+
+  get longName() {
+    return browser.$('div[id="longName"')
+  }
+
+  get location() {
+    return browser.$('div[id="location"')
+  }
+
+  waitForAlertSuccessToLoad() {
+    if (!this.alertSuccess.isDisplayed()) {
+      this.alertSuccess.waitForExist()
+      this.alertSuccess.waitForDisplayed()
+    }
+  }
+}
+
+export default new ShowOrganization()
