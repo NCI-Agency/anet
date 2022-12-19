dbf6b090fb6de653082e59b660c7e06a8d1e5ca2
Gertjan van Oosten <gertjan@west.nl>
2022-12-19 16:17:04 +0100

    AB#635 Remove obsolete function

--- client/src/components/editor/LinkAnet.js
+++ client/src/components/editor/LinkAnet.js
@@ -1,22 +1,7 @@
 import LinkAnetEntity from "components/editor/LinkAnetEntity"
-import parse from "html-react-parser"
 import PropTypes from "prop-types"
 import React from "react"
 import { getEntityInfoFromUrl } from "utils_links"
-
-// Enhanced HTML so that links will be converted to link components
-export function parseHtmlWithLinkTo(html) {
-  if (!html) {
-    return null
-  }
-  return parse(html, {
-    replace: domNode => {
-      if (domNode.attribs && domNode.attribs.href) {
-        return <LinkAnet url={domNode.attribs.href} />
-      }
-    }
-  })
-}
 
 const LinkAnet = ({ entityKey, contentState, url, displayCallback }) => {
   const urlLink =
