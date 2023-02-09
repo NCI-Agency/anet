840b2ce24ee82ba05dc65d9e1f5e8a27ecfec350
Gertjan van Oosten <gertjan@west.nl>
2023-02-09 14:58:06 +0100

    Migrate Fullcalendar CSS vars usage to v6

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   plugins: [
-    require("postcss-custom-properties")({
-      disableDeprecationNotice: true, // suppress endless deprecation warnings about "importFrom" and "exportTo"
-      preserve: false, // completely reduce all css vars
-      importFrom: [
-        "src/fullcalendar-vars.css" // look here for the new values
-      ]
-    }),
     require("postcss-calc"),
     require("autoprefixer"),
     require("postcss-flexbugs-fixes")
