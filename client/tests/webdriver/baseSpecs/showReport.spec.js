a57c5c55efe90c537a99162d8dbe4da0c7797cdb
Gertjan van Oosten <gertjan@west.nl>
2023-07-12 11:21:26 +0200

    AB#894 Remove if's around wdio tests for attachments

--- client/tests/webdriver/baseSpecs/showReport.spec.js
+++ client/tests/webdriver/baseSpecs/showReport.spec.js
@@ -35,19 +35,15 @@ describe("Show report page", () => {
     it("We should see a container for Attachment List", async() => {
       // Attachment container
       await (await ShowReport.getAttachments()).waitForExist()
-      if (await (await ShowReport.getAttachments()).isExisting()) {
-        await (await ShowReport.getAttachments()).waitForDisplayed()
-      }
+      await (await ShowReport.getAttachments()).waitForDisplayed()
     })
     it("We should see a card of Attachment", async() => {
       // Attachment card list
       await (await ShowReport.getCard()).waitForExist()
-      if (await (await ShowReport.getCard()).isExisting()) {
-        await (await ShowReport.getCard()).waitForDisplayed()
-        expect(await await ShowReport.getFileData()).to.be.equal(
-          "test_at…\n12.0 KB"
-        )
-      }
+      await (await ShowReport.getCard()).waitForDisplayed()
+      expect(await await ShowReport.getFileData()).to.be.equal(
+        "test_at…\n12.0 KB"
+      )
     })
     it("We can go to the show page of Attachment", async() => {
       if (await (await ShowReport.getImageClick()).isClickable()) {
