03262cab39839f065d1507a1216e5a1b452d3df7
Gertjan van Oosten <gertjan@west.nl>
2023-10-05 12:16:02 +0200

    AB#966 Refactor ANET init command
    Make the command more robust by adding some additional checks.
    Addresses AB#970.

--- src/main/java/mil/dds/anet/InitializationCommand.java
+++ src/main/java/mil/dds/anet/InitializationCommand.java
@@ -13,7 +13,9 @@ import mil.dds.anet.beans.Person;
 import mil.dds.anet.beans.Person.Role;
 import mil.dds.anet.beans.Position;
 import mil.dds.anet.beans.Position.PositionType;
+import mil.dds.anet.beans.search.OrganizationSearchQuery;
 import mil.dds.anet.beans.search.PersonSearchQuery;
+import mil.dds.anet.beans.search.PositionSearchQuery;
 import mil.dds.anet.config.AnetConfiguration;
 import mil.dds.anet.database.AdminDao.AdminSettingKeys;
 import net.sourceforge.argparse4j.impl.Arguments;
@@ -45,21 +47,98 @@ public class InitializationCommand extends EnvironmentCommand<AnetConfiguration>
       throws Exception {
     final AnetObjectEngine engine = AnetObjectEngine.getInstance();
 
-    final PersonSearchQuery psq = new PersonSearchQuery();
-    final List<Person> currPeople = engine.getPersonDao().search(psq).getList();
+    checkPreconditions(engine);
+    final String defaultApprovalOrgUuid = createInitialAdministrator(namespace, engine);
+    saveAdminSettings(engine, defaultApprovalOrgUuid);
+
+    System.exit(0);
+  }
+
+  private void checkPreconditions(final AnetObjectEngine engine) {
+    try {
+      checkOrganizations(engine);
+      checkPositions(engine);
+      checkPeople(engine);
+    } catch (Exception e) {
+      exitWithError("ERROR: Could not query database: " + e.getMessage(),
+          "\tDid you run all migrations?");
+    }
+  }
+
+  private void checkOrganizations(final AnetObjectEngine engine) {
+    if (!engine.getOrganizationDao().search(new OrganizationSearchQuery()).getList().isEmpty()) {
+      exitWithError("ERROR: Existing organizations detected in database",
+          "\tThis task can only be run on an otherwise empty database");
+    }
+  }
+
+  private void checkPositions(final AnetObjectEngine engine) {
+    if (!engine.getPositionDao().search(new PositionSearchQuery()).getList().isEmpty()) {
+      exitWithError("ERROR: Existing positions detected in database",
+          "\tThis task can only be run on an otherwise empty database");
+    }
+  }
+
+  private void checkPeople(final AnetObjectEngine engine) {
+    final List<Person> currPeople = engine.getPersonDao().search(new PersonSearchQuery()).getList();
     if (currPeople.isEmpty()) {
-      System.err.println("ERROR: ANET Importer missing from database");
-      System.err.println("\tYou should run all migrations first");
-      System.exit(1);
-      return;
+      exitWithError("ERROR: ANET Importer missing from database",
+          "\tYou should run all migrations first");
     }
     // Only person should be "ANET Importer"
     if (currPeople.size() != 1 || !"ANET Importer".equals(currPeople.get(0).getName())) {
-      System.err.println("ERROR: Other people besides ANET Importer detected in database");
-      System.err.println("\tThis task can only be run on an otherwise empty database");
-      System.exit(1);
-      return;
+      exitWithError("ERROR: Other people besides ANET Importer detected in database",
+          "\tThis task can only be run on an otherwise empty database");
     }
+  }
+
+  private void exitWithError(final String errorMessage, final String userHint) {
+    System.err.println(errorMessage);
+    System.err.println(userHint);
+    System.exit(1);
+  }
+
+  private String createInitialAdministrator(Namespace namespace, AnetObjectEngine engine) {
+    // Create admin organization
+    Organization adminOrg = new Organization();
+    adminOrg.setType(OrganizationType.ADVISOR_ORG);
+    adminOrg.setShortName(namespace.getString("adminOrgName"));
+    adminOrg.setStatus(Organization.Status.ACTIVE);
+    adminOrg = engine.getOrganizationDao().insert(adminOrg);
+
+    // Create admin position
+    Position adminPos = new Position();
+    adminPos.setType(PositionType.ADMINISTRATOR);
+    adminPos.setOrganizationUuid(adminOrg.getUuid());
+    adminPos.setName(namespace.getString("adminPosName"));
+    adminPos.setStatus(Position.Status.ACTIVE);
+    adminPos.setRole(Position.PositionRole.MEMBER);
+    adminPos = engine.getPositionDao().insert(adminPos);
+
+    // Create admin user
+    Person admin = new Person();
+    admin.setName(namespace.getString("adminFullName"));
+    admin.setDomainUsername(namespace.getString("adminDomainUsername"));
+    admin.setRole(Role.ADVISOR);
+    admin = engine.getPersonDao().insert(admin);
+    engine.getPositionDao().setPersonInPosition(admin.getUuid(), adminPos.getUuid());
+
+    // Create default approval workflow
+    final ApprovalStep defaultStep = new ApprovalStep();
+    defaultStep.setName("Default Approver");
+    defaultStep.setType(ApprovalStepType.REPORT_APPROVAL);
+    defaultStep.setRelatedObjectUuid(adminOrg.getUuid());
+    defaultStep.setApprovers(List.of(adminPos));
+    engine.getApprovalStepDao().insert(defaultStep);
+
+    return adminOrg.getUuid();
+  }
+
+  private void saveAdminSettings(final AnetObjectEngine engine,
+      final String defaultApprovalOrgUuid) {
+    // Set Default Approval Chain.
+    saveAdminSetting(engine, AdminSettingKeys.DEFAULT_APPROVAL_ORGANIZATION,
+        defaultApprovalOrgUuid);
 
     // Set Classification String as default
     saveAdminSetting(engine, AdminSettingKeys.SECURITY_BANNER_CLASSIFICATION, "NATO UNCLASSIFIED");
@@ -91,45 +170,9 @@ public class InitializationCommand extends EnvironmentCommand<AnetConfiguration>
 
     // Set daily rollup max report age days as default
     saveAdminSetting(engine, AdminSettingKeys.DAILY_ROLLUP_MAX_REPORT_AGE_DAYS, "14");
-
-    // Create admin organization
-    Organization adminOrg = new Organization();
-    adminOrg.setType(OrganizationType.ADVISOR_ORG);
-    adminOrg.setShortName(namespace.getString("adminOrgName"));
-    adminOrg.setStatus(Organization.Status.ACTIVE);
-    adminOrg = engine.getOrganizationDao().insert(adminOrg);
-
-    // Create admin position
-    Position adminPos = new Position();
-    adminPos.setType(PositionType.ADMINISTRATOR);
-    adminPos.setOrganizationUuid(adminOrg.getUuid());
-    adminPos.setName(namespace.getString("adminPosName"));
-    adminPos.setStatus(Position.Status.ACTIVE);
-    adminPos.setRole(Position.PositionRole.MEMBER);
-    adminPos = engine.getPositionDao().insert(adminPos);
-
-    // Create admin user
-    Person admin = new Person();
-    admin.setName(namespace.getString("adminFullName"));
-    admin.setDomainUsername(namespace.getString("adminDomainUsername"));
-    admin.setRole(Role.ADVISOR);
-    admin = engine.getPersonDao().insert(admin);
-    engine.getPositionDao().setPersonInPosition(admin.getUuid(), adminPos.getUuid());
-
-    // Set Default Approval Chain.
-    saveAdminSetting(engine, AdminSettingKeys.DEFAULT_APPROVAL_ORGANIZATION, adminOrg.getUuid());
-
-    final ApprovalStep defaultStep = new ApprovalStep();
-    defaultStep.setName("Default Approver");
-    defaultStep.setType(ApprovalStepType.REPORT_APPROVAL);
-    defaultStep.setRelatedObjectUuid(adminOrg.getUuid());
-    defaultStep.setApprovers(List.of(adminPos));
-    engine.getApprovalStepDao().insert(defaultStep);
-
-    System.exit(0);
   }
 
-  private static void saveAdminSetting(final AnetObjectEngine engine, final AdminSettingKeys key,
+  private void saveAdminSetting(final AnetObjectEngine engine, final AdminSettingKeys key,
       final String value) {
     final AdminSetting adminSetting = new AdminSetting();
     adminSetting.setKey(key.name());
