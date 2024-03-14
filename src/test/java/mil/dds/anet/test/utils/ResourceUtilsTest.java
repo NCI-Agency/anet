e0717ac3b2c42779fc8c515254145a625de03923
Raimund Klein <770876+Chessray@users.noreply.github.com>
2024-03-14 09:44:01 +0000

    AB#1053: Align with existing test patterns

--- src/test/java/mil/dds/anet/test/utils/ResourceUtilsTest.java
+++ src/test/java/mil/dds/anet/test/utils/ResourceUtilsTest.java
@@ -1,11 +1,10 @@
 package mil.dds.anet.test.utils;
 
-import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static mil.dds.anet.utils.ResourceUtils.assertAllowedClassification;
 import static mil.dds.anet.utils.ResourceUtils.getAllowedClassifications;
-import static org.assertj.core.api.BDDAssertions.then;
-import static org.assertj.core.api.BDDAssertions.thenNoException;
-import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.invoke.MethodHandles;
@@ -14,14 +13,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Response;
 import mil.dds.anet.beans.Person;
 import mil.dds.anet.beans.PersonPositionHistory;
 import mil.dds.anet.beans.Position;
 import mil.dds.anet.test.integration.utils.TestApp;
 import mil.dds.anet.utils.ResourceUtils;
-import net.bytebuddy.utility.RandomString;
-import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -190,20 +186,17 @@ public class ResourceUtilsTest {
 
   @Test
   void shouldContainClassificationEntries() {
-    then(getAllowedClassifications()).isNotNull().isNotEmpty();
+    assertThat(getAllowedClassifications()).isNotNull().isNotEmpty();
   }
 
   @Test
   void shouldPassWithoutExceptionForExistingClassification() {
-    thenNoException().isThrownBy(() -> assertAllowedClassification("undefined"));
+    assertThatNoException().isThrownBy(() -> assertAllowedClassification("undefined"));
   }
 
   @Test
   void shouldThrowWebApplicationExceptionForUnknownClassification() {
-    thenThrownBy(() -> assertAllowedClassification(RandomString.make()))
-        .isInstanceOf(WebApplicationException.class).hasMessage("Classification is not allowed")
-        .asInstanceOf(InstanceOfAssertFactories.type(WebApplicationException.class))
-        .extracting(WebApplicationException::getResponse).extracting(Response::getStatus)
-        .isEqualTo(BAD_REQUEST.getStatusCode());
+    assertThatThrownBy(() -> assertAllowedClassification("Totally public"))
+        .isInstanceOf(WebApplicationException.class).hasMessage("Classification is not allowed");
   }
 }
