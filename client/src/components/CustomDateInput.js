b68627de39e499efdd7ddd1983f8695b301595f8
Gertjan van Oosten <gertjan@west.nl>
2024-04-25 14:21:09 +0200

    AB#722 Upgrade to @blueprintjs/datetime2

--- client/src/components/CustomDateInput.js
+++ client/src/components/CustomDateInput.js
@@ -1,6 +1,8 @@
+import "normalize.css"
 import "@blueprintjs/core/lib/css/blueprint.css"
-import { DateInput, TimePrecision } from "@blueprintjs/datetime"
+import { DateInput3, TimePrecision } from "@blueprintjs/datetime2"
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css"
+import "@blueprintjs/datetime2/lib/css/blueprint-datetime2.css"
 import moment from "moment"
 import PropTypes from "prop-types"
 import React, { useRef } from "react"
@@ -44,17 +46,17 @@ const CustomDateInput = ({
     ? Settings.dateFormats.forms.input.withTime
     : Settings.dateFormats.forms.input.date
   const inputFormat = dateFormats[0]
+  const timePrecision = !withTime ? undefined : TimePrecision.MINUTE
   const timePickerProps = !withTime
     ? undefined
     : {
-      precision: TimePrecision.MINUTE,
       selectAllOnFocus: true
       // FIXME: clicking a time arrow immediately closes the dialog;
       // see https://github.com/palantir/blueprint/issues/3474
       // showArrowButtons: true
     }
   return (
-    <DateInput
+    <DateInput3
       inputProps={{
         id,
         style,
@@ -63,7 +65,7 @@ const CustomDateInput = ({
       }}
       className={className}
       rightElement={rightElement}
-      value={value && moment(value).toDate()}
+      value={value && moment(value).toISOString()}
       onChange={onChange}
       formatDate={date => {
         const dt = moment(date)
@@ -79,7 +81,9 @@ const CustomDateInput = ({
       canClearSelection={canClearSelection}
       showActionsBar
       closeOnSelection={!withTime}
+      timePrecision={timePrecision}
       timePickerProps={timePickerProps}
+      showTimezoneSelect={false}
       popoverProps={{ usePortal: false, placement }}
       disabled={disabled}
     />
