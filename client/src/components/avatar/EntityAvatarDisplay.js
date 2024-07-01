46e4a1147568ce92ec0fecc5dec4225b7b00ecae
Francisco del Castillo <francisco.delcastillo@devops.ncia.nato.int>
2024-07-01 15:14:33 +0200

    AB#811 crop avatar picture client side, review fixes

--- client/src/components/avatar/EntityAvatarDisplay.js
+++ client/src/components/avatar/EntityAvatarDisplay.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 
 export const EntityAvatarDisplay = ({ avatar, width, height }) => {
   const avatarStyle = {
@@ -10,24 +10,50 @@ export const EntityAvatarDisplay = ({ avatar, width, height }) => {
     margin: "0 auto",
     marginBottom: "10px"
   }
-
-  function getAvatarLink() {
+  useEffect(() => {
     if (avatar.applyCrop) {
-      return `/api/attachment/view-cropped/${avatar.attachmentUuid}/${avatar.cropLeft}/${avatar.cropTop}/${avatar.cropWidth}/${avatar.cropHeight}`
-    } else {
-      return `/api/attachment/view/${avatar.attachmentUuid}`
+      const canvas = document.getElementById("avatar")
+      const context = canvas.getContext("2d")
+      const image = new Image()
+      image.src = `/api/attachment/view/${avatar.attachmentUuid}`
+      image.onload = function() {
+        context.drawImage(
+          image,
+          avatar.cropLeft,
+          avatar.cropTop,
+          avatar.cropWidth,
+          avatar.cropHeight,
+          0,
+          0,
+          width,
+          height
+        )
+      }
     }
-  }
+  }, [avatar, width, height])
 
   return (
     <>
-      <img
-        src={getAvatarLink()}
-        height={height}
-        width={width}
-        alt="Avatar"
-        style={avatarStyle}
-      />
+      {avatar.applyCrop ? (
+        <>
+          <canvas
+            id="avatar"
+            width={width}
+            height={height}
+            style={avatarStyle}
+          />
+        </>
+      ) : (
+        <>
+          <img
+            src={`/api/attachment/view/${avatar.attachmentUuid}`}
+            height={height}
+            width={width}
+            alt="Avatar"
+            style={avatarStyle}
+          />
+        </>
+      )}
     </>
   )
 }
