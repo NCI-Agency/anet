2ee0211f4385e9d4a9bf4c84db12e0d6d503c760
Gertjan van Oosten <gertjan@west.nl>
2024-08-14 13:02:20 +0200

    NCI-Agency/anet#3738 Wait for the database to be up before configuring Liquibase

--- src/main/java/mil/dds/anet/config/JdbiConfig.java
+++ src/main/java/mil/dds/anet/config/JdbiConfig.java
@@ -1,14 +1,18 @@
 package mil.dds.anet.config;
 
+import java.util.stream.Stream;
 import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
 import org.jdbi.v3.core.ConnectionFactory;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.mapper.CaseStrategy;
 import org.jdbi.v3.core.mapper.MapMappers;
 import org.jdbi.v3.spring5.EnableJdbiRepositories;
 import org.jdbi.v3.spring5.SpringConnectionFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
 
 @Configuration
 @EnableJdbiRepositories
@@ -24,4 +28,19 @@ public class JdbiConfig {
     jdbi.getConfig(MapMappers.class).setCaseChange(CaseStrategy.NOP);
     return jdbi;
   }
+
+  @Bean
+  public DatabaseStartupValidator databaseStartupValidator(DataSource dataSource) {
+    var dsv = new DatabaseStartupValidator();
+    dsv.setDataSource(dataSource);
+    return dsv;
+  }
+
+  @Bean
+  public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+    // Wait for database to be up before configuring Liquibase
+    return bf -> Stream.of(bf.getBeanNamesForType(SpringLiquibase.class)).map(bf::getBeanDefinition)
+        .forEach(bd -> bd.setDependsOn("databaseStartupValidator"));
+  }
+
 }
