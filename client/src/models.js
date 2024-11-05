9abd9926c839a96f5ddd3fc30f198ca19ef19e37
Gertjan van Oosten <gertjan@west.nl>
2024-11-05 16:50:38 +0100

    AB#1102 Export models

--- client/src/models.js
+++ client/src/models.js
@@ -1,10 +1,23 @@
-export Attachment from "models/Attachment"
-export AuthorizationGroup from "models/AuthorizationGroup"
-export Comment from "models/Comment"
-export Location from "models/Location"
-export Organization from "models/Organization"
-export Person from "models/Person"
-export Position from "models/Position"
-export Report from "models/Report"
-export RollupGraph from "models/RollupGraph"
-export Task from "models/Task"
+import Attachment from "models/Attachment"
+import AuthorizationGroup from "models/AuthorizationGroup"
+import Comment from "models/Comment"
+import Location from "models/Location"
+import Organization from "models/Organization"
+import Person from "models/Person"
+import Position from "models/Position"
+import Report from "models/Report"
+import RollupGraph from "models/RollupGraph"
+import Task from "models/Task"
+
+export {
+  Attachment,
+  AuthorizationGroup,
+  Comment,
+  Location,
+  Organization,
+  Person,
+  Position,
+  Report,
+  RollupGraph,
+  Task
+}
