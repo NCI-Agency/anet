15fe184c450d17eb4342367af1ad8c563f32bed3
midmarch <midmarch@users.noreply.github.com>
2024-11-15 14:45:02 +0100

    AB#814 Activate (make it accessible) the CXF NVG 2.0 EndPoint
    CXF Page accessible on: `/cxf`
    NVG 20 Endpoint WSDL accessible on: `/cxf/nvg/20?wsdl`.
    Made CXF Web Service path stateless.
    Authentication not implemented yet, service public accessible.

--- client/config/webpack.client.dev.js
+++ client/config/webpack.client.dev.js
@@ -25,7 +25,7 @@ module.exports = merge.merge(common.clientConfig, {
     port: process.env.DEV_PORT,
     proxy: [
       {
-        context: ["/graphql", "/api", "/assets", "/imagery", "/data"],
+        context: ["/graphql", "/api", "/assets", "/imagery", "/data", "/cxf"],
         target: process.env.SERVER_URL
       }
     ],
