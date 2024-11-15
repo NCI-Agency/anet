fd33373f22c85f6adaf4bef2e3b90a94d679c5b0
midmarch <midmarch@users.noreply.github.com>
2024-11-15 14:45:02 +0100

    AB#814 Activate (make it accessible) the CXF NVG 2.0 EndPoint
    CXF Page accessible on: `/cxf`
    NVG 20 Endpoint WSDL accessible on: `/cxf/nvg/20?wsdl`.
    Made CXF Web Service path stateless.
    Authentication not implemented yet, service public accessible.

---
+++ src/main/java/mil/dds/anet/ws/CxfConfig.java
@@ -0,0 +1,26 @@
+package mil.dds.anet.ws;
+
+import jakarta.xml.ws.Endpoint;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CxfConfig {
+
+  private final Bus bus;
+  public final Nvg20WebService nvg20WebService;
+
+  public CxfConfig(Bus bus, Nvg20WebService nvg20WebService) {
+    this.bus = bus;
+    this.nvg20WebService = nvg20WebService;
+  }
+
+  @Bean
+  public Endpoint nvg20Endpoint() {
+    EndpointImpl endpoint = new EndpointImpl(bus, nvg20WebService);
+    endpoint.publish("/nvg/20");
+    return endpoint;
+  }
+}
