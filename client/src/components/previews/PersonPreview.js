f86a66886bf310882126e0b28f534e71fccd5ccd
jlnat <jlnat@users.noreply.github.com>
2024-11-26 12:22:57 +0100

    Update name display for PersonPreview

--- client/src/components/previews/PersonPreview.js
+++ client/src/components/previews/PersonPreview.js
@@ -124,7 +124,7 @@ const PersonPreview = ({ className, uuid }) => {
   return (
     <div className={`${className} preview-content-scroll`}>
       <div className="preview-sticky-title">
-        <h4 className="ellipsized-text">{`${person.rank} ${person.name}`}</h4>
+        <h4 className="ellipsized-text">{`${person.rank} ${utils.toMilitaryName(person.name)}`}</h4>
       </div>
       <div className="preview-section">
         <Row>
