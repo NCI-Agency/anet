c8c9c91fae01dd5706f1c41a30f953731cc89aec
jlnat <jlnat@users.noreply.github.com>
2024-12-03 23:52:43 +0100

    Update tests to match changes

--- client/tests/e2e/position.js
+++ client/tests/e2e/position.js
@@ -22,9 +22,9 @@ test.serial("Move someone in and out of a position", async t => {
 
   const positionName = "EF 2.2 Advisor C"
   const positionRole = "Member"
-  const person = "REINTON, Reina"
+  const person = "Reinton, Reina"
   const rank = "CIV"
-  const personName = rank + " " + person
+  const personName = "CIV REINTON, Reina"
 
   await t.context.pageHelpers.clickPersonNameFromSupportedPositionsFieldset(
     personName,
--- client/tests/webdriver/baseSpecs/userLogin.spec.js
+++ client/tests/webdriver/baseSpecs/userLogin.spec.js
@@ -38,7 +38,7 @@ describe("Anet default user login", () => {
   it("Default user is logged in", async() => {
     await Home.open()
     const bannerUser = await (await Home.getBannerUser()).getText()
-    const defaultUserValue = "ERINSON, Erin"
+    const defaultUserValue = "Erinson, Erin"
     expect(bannerUser).to.equal(defaultUserValue)
     await Home.logout()
   })
@@ -48,7 +48,7 @@ describe("Anet superuser login", () => {
   it("Superuser is logged in", async() => {
     await Home.openAsSuperuser()
     const bannerUser = await (await Home.getBannerUser()).getText()
-    const superuserValue = "BECCABON, Rebecca"
+    const superuserValue = "Beccabon, Rebecca"
     expect(bannerUser).to.equal(superuserValue)
     await Home.logout()
   })
@@ -58,7 +58,7 @@ describe("Anet admin user login", () => {
   it("Admin user is logged in", async() => {
     await Home.openAsAdminUser()
     const bannerUser = await (await Home.getBannerUser()).getText()
-    const adminUserValue = "DMIN, Arthur"
+    const adminUserValue = "Dmin, Arthur"
     expect(bannerUser).to.equal(adminUserValue)
     await Home.logout()
   })
