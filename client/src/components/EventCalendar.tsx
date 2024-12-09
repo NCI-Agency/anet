d017abaff94109f35209792b7cd5e9359d320555
Gertjan van Oosten <gertjan@west.nl>
2024-12-09 10:43:08 +0100

    AB#1064 Simplify EventCollection

--- client/src/components/EventCalendar.tsx
+++ client/src/components/EventCalendar.tsx
@@ -2,23 +2,25 @@ import API from "api"
 import { eventsToCalendarEvents } from "components/aggregations/utils"
 import Calendar from "components/Calendar"
 import Model from "components/Model"
-import { PageDispatchersPropType } from "components/Page"
+import {
+  mapPageDispatchersToProps,
+  PageDispatchersPropType
+} from "components/Page"
 import _isEqual from "lodash/isEqual"
 import { Event } from "models"
 import moment from "moment"
 import React, { useRef } from "react"
+import { connect } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 interface EventCalendarProps {
   pageDispatchers?: PageDispatchersPropType
   queryParams?: any
-  setTotalCount?: (...args: unknown[]) => unknown
 }
 
 const EventCalendar = ({
   pageDispatchers: { showLoading, hideLoading },
-  queryParams,
-  setTotalCount
+  queryParams
 }: EventCalendarProps) => {
   const navigate = useNavigate()
   const prevEventQuery = useRef(null)
@@ -37,31 +39,24 @@ const EventCalendar = ({
   )
 
   function getEvents(fetchInfo, successCallback, failureCallback) {
-    const eventQuery = Object.assign({}, queryParams, {
+    const eventQuery = {
+      ...queryParams,
       status: Model.STATUS.ACTIVE,
       pageSize: 0,
       startDate: moment(fetchInfo.start).startOf("day"),
       endDate: moment(fetchInfo.end).endOf("day")
-    })
+    }
     if (_isEqual(prevEventQuery.current, eventQuery)) {
       // Optimise, return API promise instead of calling API.query again
       return apiPromise.current
     }
     prevEventQuery.current = eventQuery
-    if (setTotalCount) {
-      // Reset the total count
-      setTotalCount(null)
-    }
     // Store API promise to use in optimised case
     showLoading()
     apiPromise.current = API.query(Event.getEventListQuery, {
       eventQuery
     }).then(data => {
       const events = data ? data.eventList.list : []
-      if (setTotalCount) {
-        const { totalCount } = data.eventList
-        setTotalCount(totalCount)
-      }
       const results = eventsToCalendarEvents(events)
       hideLoading()
       return results
@@ -70,4 +65,4 @@ const EventCalendar = ({
   }
 }
 
-export default EventCalendar
+export default connect(null, mapPageDispatchersToProps)(EventCalendar)
