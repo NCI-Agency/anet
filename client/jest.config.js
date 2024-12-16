f82d4174c371f7bf727b2a9e6e0b39f495b25913
Gertjan van Oosten <gertjan@west.nl>
2024-12-16 10:57:47 +0100

    Transform react-resize-detector for Jest tests
    Also remove no longer needed module name mapping for change-case.

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   moduleDirectories: ["platform/node", "src", "node_modules"],
   moduleNameMapper: {
     "^d3$": "<rootDir>/node_modules/d3/dist/d3.min.js",
-    "^change-case$": "<rootDir>/node_modules/change-case/dist/index.js",
     "^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/config/jest/FileStub.js",
     "^.+\\.css$": "<rootDir>/config/jest/CSSStub.js"
@@ -30,6 +29,6 @@ module.exports = {
     ]
   },
   transformIgnorePatterns: [
-    "<rootDir>/node_modules/(?!(change-case|jsonpath-plus|title-case)/)"
+    "<rootDir>/node_modules/(?!(change-case|jsonpath-plus|react-resize-detector|title-case)/)"
   ]
 }
