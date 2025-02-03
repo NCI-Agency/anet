b6e4cc6e41542171b0d0cc1847dce41ac6c8cc5f
jlnat <jlnat@users.noreply.github.com>
2025-02-03 14:35:26 +0100

    AB#1231 Allow edit control to component

--- client/src/components/Attachment/AttachmentsDetailView.tsx
+++ client/src/components/Attachment/AttachmentsDetailView.tsx
@@ -1,26 +1,60 @@
+import UploadAttachment from "components/Attachment/UploadAttachment"
+import { useState } from "react"
+import { Button } from "react-bootstrap"
 import AttachmentCard from "./AttachmentCard"
 
-interface AttachmentsDetailViewProps {
-  attachments?: any
-  allowEdit?: boolean
+interface AttachmentsListProps {
+  attachments: any
 }
 
-const AttachmentsDetailView = ({
-  attachments,
-  allowEdit
-}: AttachmentsDetailViewProps) => {
+const AttachmentsList = ({ attachments }: AttachmentsListProps) => {
   if (attachments.length === 0) {
     return null
   }
   return (
     <div className="attachment-card-list">
       {attachments.map(attachment => (
-        <AttachmentCard
-          key={attachment.uuid}
-          attachment={attachment}
-        />
+        <AttachmentCard key={attachment.uuid} attachment={attachment} />
       ))}
     </div>
+  )
+}
+
+interface AttachmentsDetailViewProps {
+  attachments: any
+  updateAttachments: any
+  relatedObjectType: any
+  relatedObjectUuid: any
+  allowEdit?: boolean
+}
+
+const AttachmentsDetailView = ({
+  attachments,
+  updateAttachments,
+  relatedObjectType,
+  relatedObjectUuid,
+  allowEdit = false
+}: AttachmentsDetailViewProps) => {
+  const [editAttachments, setEditAttachments] = useState(false)
+
+  return allowEdit ? (
+    editAttachments ? (
+      <UploadAttachment
+        attachments={attachments}
+        updateAttachments={updateAttachments}
+        relatedObjectType={relatedObjectType}
+        relatedObjectUuid={relatedObjectUuid}
+      />
+    ) : (
+      <>
+        <AttachmentsList attachments={attachments} />
+        <Button variant="primary" onClick={() => setEditAttachments(true)}>
+          Edit attachments
+        </Button>
+      </>
+    )
+  ) : (
+    <AttachmentsList attachments={attachments} />
   )
 }
 
