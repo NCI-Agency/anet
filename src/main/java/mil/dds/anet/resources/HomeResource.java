f4f71a6ce03a0234e86f362b68dc651a36ec8b0b
Gertjan van Oosten <gertjan@west.nl>
2025-03-11 09:03:36 +0100

    AB#1280 Factor out getBaseUrl() method

--- src/main/java/mil/dds/anet/resources/HomeResource.java
+++ src/main/java/mil/dds/anet/resources/HomeResource.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import mil.dds.anet.utils.ResponseUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
@@ -12,8 +13,6 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class HomeResource {
@@ -48,9 +47,8 @@ public class HomeResource {
       Authentication authentication) throws IOException {
     this.logoutHandler.logout(request, response, authentication);
     // Log out of Keycloak
-    final UriComponents requestUrl =
-        UriComponentsBuilder.fromUriString(request.getRequestURL().toString()).build();
-    final String redirectUri = URLEncoder.encode(getLogoutUrl(requestUrl), StandardCharsets.UTF_8);
+    final String redirectUri =
+        URLEncoder.encode(getLogoutUrl(request.getRequestURL().toString()), StandardCharsets.UTF_8);
     // Redirect to Keycloak to log out
     final String idParam = (authentication instanceof OidcUser) ? clientId : publicClientId;
     // scan:ignore — false positive, we only let Keycloak redirect back to the original request URI
@@ -59,22 +57,8 @@ public class HomeResource {
             issuerUri, redirectUri, idParam));
   }
 
-  // Define these constants here
-  private static final String HTTP_SCHEME = "http";
-  private static final int HTTP_DEFAULT_PORT = 80;
-  private static final String HTTPS_SCHEME = "https";
-  private static final int HTTPS_DEFAULT_PORT = 443;
-
-  private String getLogoutUrl(UriComponents requestUrl) {
-    final String scheme = requestUrl.getScheme();
-    final int port = requestUrl.getPort();
-    final String host = requestUrl.getHost();
-    return (port == -1 // no port in the request URL
-        // or using the default port of the scheme
-        || HTTP_SCHEME.equals(scheme) && HTTP_DEFAULT_PORT == port
-        || HTTPS_SCHEME.equals(scheme) && HTTPS_DEFAULT_PORT == port)
-            ? String.format("%s://%s%s", scheme, host, LOGOUT_PAGE)
-            : String.format("%s://%s:%s%s", scheme, host, port, LOGOUT_PAGE);
+  private String getLogoutUrl(String requestUrl) {
+    return String.format("%s%s", ResponseUtils.getBaseUrl(requestUrl), LOGOUT_PAGE);
   }
 
 }
