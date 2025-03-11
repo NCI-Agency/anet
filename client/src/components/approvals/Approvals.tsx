33ac86215bbb95884f33e76c8793b42310ddd1e5
jlnat <jlnat@users.noreply.github.com>
2025-03-11 18:22:15 +0100

    AB#1254 Update Approvals.tsx to allow each approval type independently

--- client/src/components/approvals/Approvals.tsx
+++ client/src/components/approvals/Approvals.tsx
@@ -12,110 +12,119 @@ const Approvals = ({
   restrictedApprovalLabel,
   relatedObject
 }: ApprovalsProps) => {
-  const approvalSteps = relatedObject.approvalSteps
-  const planningApprovalSteps = relatedObject.planningApprovalSteps
+  const { planningApprovalSteps, approvalSteps } = relatedObject
 
   return (
     <div>
-      <Fieldset
-        id="planningApprovals"
-        title="Engagement planning approval process"
-      >
-        {planningApprovalSteps.map((step, idx) => (
-          <Fieldset title={`Step ${idx + 1}: ${step.name}`} key={"step_" + idx}>
-            {restrictedApprovalLabel && (
-              <FormCheck
-                type="checkbox"
-                label={restrictedApprovalLabel}
-                checked={step.restrictedApproval}
-                readOnly
-              />
-            )}
-            <Table striped hover responsive>
-              <thead>
-                <tr>
-                  <th>Person</th>
-                  <th>Position</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {step.approvers.map((position, approverIdx) => (
-                  <tr
-                    key={`${step.uuid}_${position.uuid}`}
-                    id={`step_${idx}_approver_${approverIdx}`}
-                  >
-                    {position.person && position.person.uuid ? (
-                      <td>
-                        <LinkTo modelType="Person" model={position.person} />
-                      </td>
-                    ) : (
-                      <td className="text-danger">Unfilled</td>
-                    )}
-                    <td>
-                      <LinkTo modelType="Position" model={position} />
-                    </td>
+      {planningApprovalSteps && (
+        <Fieldset
+          id="planningApprovals"
+          title="Engagement planning approval process"
+        >
+          {planningApprovalSteps.map((step, idx) => (
+            <Fieldset
+              title={`Step ${idx + 1}: ${step.name}`}
+              key={"planning_step_" + idx}
+            >
+              {restrictedApprovalLabel && (
+                <FormCheck
+                  type="checkbox"
+                  label={restrictedApprovalLabel}
+                  checked={step.restrictedApproval}
+                  readOnly
+                />
+              )}
+              <Table striped hover responsive>
+                <thead>
+                  <tr>
+                    <th>Person</th>
+                    <th>Position</th>
                   </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Fieldset>
-        ))}
+                </thead>
 
-        {planningApprovalSteps.length === 0 && (
-          <em>
-            This object doesn't have any engagement planning approval steps
-          </em>
-        )}
-      </Fieldset>
-      <Fieldset id="approvals" title="Report publication approval process">
-        {approvalSteps.map((step, idx) => (
-          <Fieldset title={`Step ${idx + 1}: ${step.name}`} key={"step_" + idx}>
-            {restrictedApprovalLabel && (
-              <FormCheck
-                type="checkbox"
-                label={restrictedApprovalLabel}
-                checked={step.restrictedApproval}
-                readOnly
-              />
-            )}
-            <Table striped hover responsive>
-              <thead>
-                <tr>
-                  <th>Person</th>
-                  <th>Position</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {step.approvers.map((position, approverIdx) => (
-                  <tr
-                    key={`${step.uuid}_${position.uuid}`}
-                    id={`step_${idx}_approver_${approverIdx}`}
-                  >
-                    {position.person && position.person.uuid ? (
+                <tbody>
+                  {step.approvers.map((position, approverIdx) => (
+                    <tr
+                      key={`${step.uuid}_${position.uuid}`}
+                      id={`planning_step_${idx}_approver_${approverIdx}`}
+                    >
+                      {position.person && position.person.uuid ? (
+                        <td>
+                          <LinkTo modelType="Person" model={position.person} />
+                        </td>
+                      ) : (
+                        <td className="text-danger">Unfilled</td>
+                      )}
                       <td>
-                        <LinkTo modelType="Person" model={position.person} />
+                        <LinkTo modelType="Position" model={position} />
                       </td>
-                    ) : (
-                      <td className="text-danger">Unfilled</td>
-                    )}
-                    <td>
-                      <LinkTo modelType="Position" model={position} />
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Fieldset>
-        ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Fieldset>
+          ))}
 
-        {approvalSteps.length === 0 && (
-          <em>
-            This object doesn't have any report publication approval steps
-          </em>
-        )}
-      </Fieldset>
+          {planningApprovalSteps.length === 0 && (
+            <em>
+              This object doesn't have any engagement planning approval steps
+            </em>
+          )}
+        </Fieldset>
+      )}
+      {approvalSteps && (
+        <Fieldset id="approvals" title="Report publication approval process">
+          {approvalSteps.map((step, idx) => (
+            <Fieldset
+              title={`Step ${idx + 1}: ${step.name}`}
+              key={"approval_step_" + idx}
+            >
+              {restrictedApprovalLabel && (
+                <FormCheck
+                  type="checkbox"
+                  label={restrictedApprovalLabel}
+                  checked={step.restrictedApproval}
+                  readOnly
+                />
+              )}
+              <Table striped hover responsive>
+                <thead>
+                  <tr>
+                    <th>Person</th>
+                    <th>Position</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {step.approvers.map((position, approverIdx) => (
+                    <tr
+                      key={`${step.uuid}_${position.uuid}`}
+                      id={`approval_step_${idx}_approver_${approverIdx}`}
+                    >
+                      {position.person && position.person.uuid ? (
+                        <td>
+                          <LinkTo modelType="Person" model={position.person} />
+                        </td>
+                      ) : (
+                        <td className="text-danger">Unfilled</td>
+                      )}
+                      <td>
+                        <LinkTo modelType="Position" model={position} />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Fieldset>
+          ))}
+
+          {approvalSteps.length === 0 && (
+            <em>
+              This object doesn't have any report publication approval steps
+            </em>
+          )}
+        </Fieldset>
+      )}
     </div>
   )
 }
