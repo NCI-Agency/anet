3bd163ccbdc712c26cd1621d24db62c3ce314903
jlnat <jlnat@users.noreply.github.com>
2025-03-28 13:03:26 +0100

    AB#1278 Adding dropdown options to display app6 previews

--- client/src/components/editor/EditApp6SymbolModal.tsx
+++ client/src/components/editor/EditApp6SymbolModal.tsx
@@ -5,7 +5,7 @@ import Fieldset from "components/Fieldset"
 import { Field, Form, Formik } from "formik"
 import { Organization } from "models"
 import React from "react"
-import { Button, Modal } from "react-bootstrap"
+import { Button, Dropdown, FormSelect, Modal } from "react-bootstrap"
 import Settings from "settings"
 import utils from "utils"
 
@@ -24,6 +24,14 @@ const EditApp6SymbolModal = ({
 }: EditApp6SymbolModalProps) => {
   const { parentContext, parentStandardIdentity, parentSymbolSet } =
     Organization.getApp6ParentFields(values.parentOrg, values)
+  const parentValues = {
+    app6context: parentContext,
+    app6standardIdentity: parentStandardIdentity,
+    app6symbolSet: parentSymbolSet,
+    app6hq: null,
+    app6amplifier: null
+  }
+
   const initialValues = {
     app6context: values.app6context,
     app6standardIdentity: values.app6standardIdentity,
@@ -40,6 +48,92 @@ const EditApp6SymbolModal = ({
       <option key="" value="" style={{ fontStyle: "italic", color: "grey" }}>
         {choices[parentValue]} (inherited)
       </option>
+    )
+  }
+
+  const getApp6Symbol = (size, field = null, value = null) => {
+    const symbolValues = {
+      app6context: values.app6context || parentContext,
+      app6standardIdentity:
+        values.app6standardIdentity || parentStandardIdentity,
+      app6symbolSet: values.app6symbolSet || parentSymbolSet,
+      app6hq: values.app6hq,
+      app6amplifier: values.app6amplifier
+    }
+    if (field && value !== null) {
+      symbolValues[field] = value
+    }
+    return (
+      <App6Symbol
+        context={symbolValues.app6context}
+        standardIdentity={symbolValues.app6standardIdentity}
+        symbolSet={symbolValues.app6symbolSet}
+        hq={symbolValues.app6hq}
+        amplifier={symbolValues.app6amplifier}
+        size={size}
+      />
+    )
+  }
+
+  const getFieldWidget = (field, setFieldValue, value) => {
+    const choicesObj = Settings.fields.organization[field]?.choices || {}
+    const choices = Object.entries(choicesObj).map(([key, value]) => ({
+      value: key,
+      label: value
+    }))
+
+    const parentValue = parentValues[field]
+    const parentChoice = choices.find(choice => choice.value === parentValue)
+    const selectedChoice = value
+      ? choicesObj[value]
+      : parentValue
+        ? `${choicesObj[parentValue]} (inherited)`
+        : ""
+    return (
+      <Dropdown>
+        <Dropdown.Toggle
+          variant="outline-tertiary"
+          id={`${field}-dropdown`}
+          style={{
+            width: 300,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            color: "#212529",
+            fontSize: "14px",
+            borderColor: "#212529"
+          }}
+        >
+          <div className="d-flex gap-2">
+            {getApp6Symbol(20)}
+            {selectedChoice}
+          </div>
+        </Dropdown.Toggle>
+        <Dropdown.Menu style={{ width: 300 }}>
+          {parentChoice && (
+            <Dropdown.Item
+              key=""
+              onClick={() => setFieldValue(field, "")}
+              className="d-flex align-items-center gap-2"
+              style={{ height: 40 }}
+            >
+              {getApp6Symbol(20, field, parentValue)}
+              {`${parentChoice.label} (inherited)`}
+            </Dropdown.Item>
+          )}
+          {choices.map(choice => (
+            <Dropdown.Item
+              key={choice.value}
+              onClick={() => setFieldValue(field, choice.value)}
+              className="d-flex align-items-center gap-2"
+              style={{ height: 40 }}
+            >
+              {getApp6Symbol(20, field, choice.value)}
+              {choice.label}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
     )
   }
 
@@ -70,14 +164,11 @@ const EditApp6SymbolModal = ({
                         wrappedComponent={Field}
                         dictProps={Settings.fields.organization.app6context}
                         name="app6context"
-                        component={FieldHelper.SelectField}
-                        buttons={utils.getButtonsFromChoices(
-                          Settings.fields.organization.app6context.choices
-                        )}
-                        onChange={value => setFieldValue("app6context", value)}
-                        defaultOption={getDefaultOption(
-                          parentContext,
-                          Settings.fields.organization.app6context.choices
+                        component={FieldHelper.SpecialField}
+                        widget={getFieldWidget(
+                          "app6context",
+                          setFieldValue,
+                          values.app6context
                         )}
                       />
                       <DictionaryField
@@ -86,54 +177,45 @@ const EditApp6SymbolModal = ({
                           Settings.fields.organization.app6standardIdentity
                         }
                         name="app6standardIdentity"
-                        component={FieldHelper.SelectField}
-                        buttons={utils.getButtonsFromChoices(
-                          Settings.fields.organization.app6standardIdentity
-                            .choices
-                        )}
-                        onChange={value =>
-                          setFieldValue("app6standardIdentity", value)}
-                        defaultOption={getDefaultOption(
-                          parentStandardIdentity,
-                          Settings.fields.organization.app6standardIdentity
-                            .choices
+                        component={FieldHelper.SpecialField}
+                        widget={getFieldWidget(
+                          "app6standardIdentity",
+                          setFieldValue,
+                          values.app6standardIdentity
                         )}
                       />
                       <DictionaryField
                         wrappedComponent={Field}
                         dictProps={Settings.fields.organization.app6symbolSet}
                         name="app6symbolSet"
-                        component={FieldHelper.SelectField}
-                        buttons={utils.getButtonsFromChoices(
-                          Settings.fields.organization.app6symbolSet.choices
-                        )}
-                        onChange={value =>
-                          setFieldValue("app6symbolSet", value)}
-                        defaultOption={getDefaultOption(
-                          parentSymbolSet,
-                          Settings.fields.organization.app6symbolSet.choices
+                        component={FieldHelper.SpecialField}
+                        widget={getFieldWidget(
+                          "app6symbolSet",
+                          setFieldValue,
+                          values.app6symbolSet
                         )}
                       />
                       <DictionaryField
                         wrappedComponent={Field}
                         dictProps={Settings.fields.organization.app6hq}
                         name="app6hq"
-                        component={FieldHelper.SelectField}
-                        buttons={utils.getButtonsFromChoices(
-                          Settings.fields.organization.app6hq.choices
+                        component={FieldHelper.SpecialField}
+                        widget={getFieldWidget(
+                          "app6hq",
+                          setFieldValue,
+                          values.app6hq
                         )}
-                        onChange={value => setFieldValue("app6hq", value)}
                       />
                       <DictionaryField
                         wrappedComponent={Field}
                         dictProps={Settings.fields.organization.app6amplifier}
                         name="app6amplifier"
-                        component={FieldHelper.SelectField}
-                        buttons={utils.getButtonsFromChoices(
-                          Settings.fields.organization.app6amplifier.choices
+                        component={FieldHelper.SpecialField}
+                        widget={getFieldWidget(
+                          "app6amplifier",
+                          setFieldValue,
+                          values.app6amplifier
                         )}
-                        onChange={value =>
-                          setFieldValue("app6amplifier", value)}
                       />
                     </Fieldset>
                   </div>
