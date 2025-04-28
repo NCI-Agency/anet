130bcba291d1a86bcb56f0547e31f48dd81a7329
Francisco del Castillo <francisco.delcastillo@devops.ncia.nato.int>
2025-04-28 10:45:26 +0200

    AB#1216 user preferences

--- src/main/java/mil/dds/anet/beans/Person.java
+++ src/main/java/mil/dds/anet/beans/Person.java
@@ -77,6 +77,8 @@ public class Person extends AbstractEmailableAnetBean
   private List<PersonPositionHistory> previousPositions;
   // annotated below
   private List<AuthorizationGroup> authorizationGroups;
+  // annotated below
+  private List<PersonPreference> preferences;
   @GraphQLQuery
   @GraphQLInputField
   private String code;
@@ -389,6 +391,27 @@ public class Person extends AbstractEmailableAnetBean
       @GraphQLRootContext GraphQLContext context,
       @GraphQLArgument(name = "network") String network) {
     return super.loadEmailAddresses(context, network);
+  }
+
+  @GraphQLQuery(name = "preferences")
+  public CompletableFuture<List<PersonPreference>> loadPreferences(
+      @GraphQLRootContext GraphQLContext context) {
+    if (preferences != null) {
+      return CompletableFuture.completedFuture(preferences);
+    }
+    return engine().getPersonDao().getPreferences(context, uuid).thenApply(o -> {
+      preferences = o;
+      return o;
+    });
+  }
+
+  public List<PersonPreference> getPreferences() {
+    return preferences;
+  }
+
+  @GraphQLInputField(name = "preferences")
+  public void setPreferences(List<PersonPreference> preferences) {
+    this.preferences = preferences;
   }
 
   @Override
