b67536f54cffb09950c2d290d5ff2c4148c30045
Francisco del Castillo <francisco.delcastillo@devops.ncia.nato.int>
2025-04-29 14:48:01 +0200

    AB#1271 review fixes

--- src/main/java/mil/dds/anet/ws/AccessTokenPrincipal.java
+++ src/main/java/mil/dds/anet/ws/AccessTokenPrincipal.java
@@ -1,18 +1,12 @@
 package mil.dds.anet.ws;
 
 import java.security.Principal;
-import javax.security.auth.Subject;
 import mil.dds.anet.beans.AccessToken;
 
 public record AccessTokenPrincipal(AccessToken accessToken) implements Principal {
 
   @Override
   public String getName() {
-    return "AccessTokenPrincipal";
-  }
-
-  @Override
-  public boolean implies(Subject subject) {
-    return Principal.super.implies(subject);
+    return accessToken.getName();
   }
 }
