fca7540ebded3a13f1c156ef7ad76defe8e45323
jlnat <jlnat@users.noreply.github.com>
2025-05-05 15:40:20 +0200

    AB#1346 Add compact prop to ListItems

--- client/src/components/ListItems.tsx
+++ client/src/components/ListItems.tsx
@@ -3,13 +3,18 @@ import React from "react"
 interface ListItemsProps {
   value: string
   forceList?: boolean
+  compact?: boolean
 }
 
-const ListItems = ({ value, forceList }: ListItemsProps) => {
+const ListItems = ({ value, forceList, compact }: ListItemsProps) => {
   const items = value.split(/[\r\n\v\f\u2028\u2029]+/)
   if (items.length > 1 || forceList) {
     return (
-      <ul style={{ marginLeft: "-18px" }}>
+      <ul
+        style={
+          compact ? { margin: 0 } : { marginLeft: "-18px", marginBottom: 0 }
+        }
+      >
         {items.map((item, index) => (
           <li key={index}>{item}</li>
         ))}
