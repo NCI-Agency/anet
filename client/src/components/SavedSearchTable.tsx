30a8a6ed755518bbb5669260e58bd2349afa9177
jlnat <jlnat@users.noreply.github.com>
2025-05-06 11:38:55 +0200

    AB#1179 Add display for people saved searches

--- client/src/components/SavedSearchTable.tsx
+++ client/src/components/SavedSearchTable.tsx
@@ -3,6 +3,7 @@ import LocationTable from "components/LocationTable"
 import ReportCollection from "components/ReportCollection"
 import React from "react"
 import utils from "utils"
+import PersonTable from "./PersonTable"
 
 interface SavedSearchTableProps {
   search: any
@@ -11,21 +12,23 @@ interface SavedSearchTableProps {
 const SavedSearchTable = (props: SavedSearchTableProps) => {
   const objType = SEARCH_OBJECT_TYPES[props.search.objectType]
   const query = utils.parseJsonSafe(props.search.query)
+  query.pageNum = query.pageNum || 0
+  query.pageSize = query.pageSize || 10
 
   if (objType === SEARCH_OBJECT_TYPES.REPORTS) {
     query.sortBy = query.sortBy || "ENGAGEMENT_DATE"
     query.sortOrder = query.sortOrder || "DESC"
-    query.pageNum = query.pageNum || 0
-    query.pageSize = query.pageSize || 10
     return (
       <ReportCollection paginationKey="r_saved-search" queryParams={query} />
     )
   } else if (objType === SEARCH_OBJECT_TYPES.LOCATIONS) {
     query.sortBy = query.sortBy || "NAME"
     query.sortOrder = query.sortOrder || "ASC"
-    query.pageNum = query.pageNum || 0
-    query.pageSize = query.pageSize || 10
     return <LocationTable paginationKey="l_saved-search" queryParams={query} />
+  } else if (objType === SEARCH_OBJECT_TYPES.PEOPLE) {
+    query.sortBy = query.sortBy || "NAME"
+    query.sortOrder = query.sortOrder || "ASC"
+    return <LocationTable paginationKey="p_saved-search" queryParams={query} />
   } else {
     return <em>Unsupported object type: {objType}</em>
   }
