ed0e8ab11e03c15da0cad0bae6d2ee46e2705739
jlnat <jlnat@users.noreply.github.com>
2025-05-06 11:32:19 +0200

    AB#1179 Add display of saved searches for locations

--- client/src/components/SavedSearchTable.tsx
+++ client/src/components/SavedSearchTable.tsx
@@ -1,4 +1,5 @@
 import { SEARCH_OBJECT_TYPES } from "actions"
+import LocationTable from "components/LocationTable"
 import ReportCollection from "components/ReportCollection"
 import React from "react"
 import utils from "utils"
@@ -8,21 +9,26 @@ interface SavedSearchTableProps {
 }
 
 const SavedSearchTable = (props: SavedSearchTableProps) => {
-  const objType =
-    SEARCH_OBJECT_TYPES[props.search.objectType] || SEARCH_OBJECT_TYPES.REPORTS
-  if (objType !== SEARCH_OBJECT_TYPES.REPORTS) {
-    // This table only shows reports
-    return <em>No reports found</em>
-  }
-
+  const objType = SEARCH_OBJECT_TYPES[props.search.objectType]
   const query = utils.parseJsonSafe(props.search.query)
-  // Add default sorting (if not specified/saved in the query); see SEARCH_CONFIG in pages/Search.js
-  query.sortBy = query.sortBy || "ENGAGEMENT_DATE"
-  query.sortOrder = query.sortOrder || "DESC"
-  query.pageNum = query.pageNum || 0
-  query.pageSize = query.pageSize || 10
 
-  return <ReportCollection paginationKey="r_saved-search" queryParams={query} />
+  if (objType === SEARCH_OBJECT_TYPES.REPORTS) {
+    query.sortBy = query.sortBy || "ENGAGEMENT_DATE"
+    query.sortOrder = query.sortOrder || "DESC"
+    query.pageNum = query.pageNum || 0
+    query.pageSize = query.pageSize || 10
+    return (
+      <ReportCollection paginationKey="r_saved-search" queryParams={query} />
+    )
+  } else if (objType === SEARCH_OBJECT_TYPES.LOCATIONS) {
+    query.sortBy = query.sortBy || "NAME"
+    query.sortOrder = query.sortOrder || "ASC"
+    query.pageNum = query.pageNum || 0
+    query.pageSize = query.pageSize || 10
+    return <LocationTable paginationKey="l_saved-search" queryParams={query} />
+  } else {
+    return <em>Unsupported object type: {objType}</em>
+  }
 }
 
 export default SavedSearchTable
