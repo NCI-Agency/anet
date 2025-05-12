d529d348f8d0346b966beb44d9444592deae8c43
Gertjan van Oosten <gertjan@west.nl>
2025-05-12 16:04:37 +0200

    AB#1222 Change URLs for authorizationGroups to communities

--- client/tests/webdriver/baseSpecs/showPosition.spec.js
+++ client/tests/webdriver/baseSpecs/showPosition.spec.js
@@ -6,8 +6,8 @@ const POSITION_WITH_AG_UUID = "05c42ce0-34a0-4391-8b2f-c4cd85ee6b47" // EF 5.1 A
 const POSITION_WITH_ATTACHMENTS_UUID = "888d6c4b-deaa-4218-b8fd-abfb7c81a4c6" // EF 1.1 Advisor G
 
 describe("Show position page", () => {
-  describe("When on the show page of a position with authorizationGroup(s)", () => {
-    it("We should see a table with authorizationGroups", async() => {
+  describe("When on the show page of a position with communities", () => {
+    it("We should see a table with communities", async() => {
       await ShowPosition.open(POSITION_WITH_AG_UUID)
       await (await ShowPosition.getAuthorizationGroupsTable()).waitForExist()
       await (
@@ -17,10 +17,10 @@ describe("Show position page", () => {
         await (await ShowPosition.getAuthorizationGroupsTable()).getText()
       ).to.contain("EF 5")
     })
-    it("We can go to the show page of authorizationGroup", async() => {
+    it("We can go to the show page of a community", async() => {
       await (await ShowPosition.getAuthorizationGroup(1)).click()
       await expect(await browser.getUrl()).to.include(
-        "/authorizationGroups/ab1a7d99-4529-44b1-a118-bdee3ca8296b"
+        "/communities/ab1a7d99-4529-44b1-a118-bdee3ca8296b"
       )
 
       await ShowPosition.logout()
