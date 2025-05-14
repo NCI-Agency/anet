7d63b180942642f78359e1222ef059013cc074af
Gertjan van Oosten <gertjan@west.nl>
2025-05-14 16:15:06 +0200

    AB#1222 For reportSensitiveInformation, only allow authorizationGroups that are forSensitiveInformation

--- client/src/pages/reports/Form.tsx
+++ client/src/pages/reports/Form.tsx
@@ -514,8 +514,8 @@ const ReportForm = ({
 
         const authorizationGroupsFilters = {
           allAuthorizationGroups: {
-            label: "All communities",
-            queryVars: {}
+            label: "Communities for sensitive information",
+            queryVars: { forSensitiveInformation: true }
           }
         }
 
