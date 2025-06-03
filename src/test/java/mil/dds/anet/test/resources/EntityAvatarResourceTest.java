b30f07f0990032762d471c14b7d18ac785de812e
Gertjan van Oosten <gertjan@west.nl>
2025-06-03 17:13:43 +0200

    AB#1352 Add server-side tests for location avatar

--- src/test/java/mil/dds/anet/test/resources/EntityAvatarResourceTest.java
+++ src/test/java/mil/dds/anet/test/resources/EntityAvatarResourceTest.java
@@ -3,11 +3,13 @@ package mil.dds.anet.test.resources;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
+import mil.dds.anet.database.LocationDao;
 import mil.dds.anet.database.OrganizationDao;
 import mil.dds.anet.database.PersonDao;
 import mil.dds.anet.test.client.Attachment;
 import mil.dds.anet.test.client.EntityAvatar;
 import mil.dds.anet.test.client.EntityAvatarInput;
+import mil.dds.anet.test.client.Location;
 import mil.dds.anet.test.client.Organization;
 import mil.dds.anet.test.client.Person;
 import org.junit.jupiter.api.Test;
@@ -18,18 +20,79 @@ class EntityAvatarResourceTest extends AbstractResourceTest {
       "{ relatedObjectType relatedObjectUuid applyCrop attachmentUuid cropLeft cropTop cropWidth cropHeight }";
   protected static final String FIELDS =
       String.format("{ uuid entityAvatar %s attachments { uuid } }", _ENTITY_AVATAR_FIELDS);
-  private static final String EF22_ORGANIZATION_UUID = "ccbee4bb-08b8-42df-8cb5-65e8172f657b";
-  private static final String EF22_ORGANIZATION_ATTACHMENT_UUID =
-      "9ac41246-25ac-457c-b7d6-946c5f625f1f";
+
+  @Test
+  void testLocationAvatar() {
+    // For the test we use location Antarctica which already has an attachment associated
+    final String ENTITY_UUID = "e5b3a4b9-acf7-4c79-8224-f248b9a7215d";
+    final String ENTITY_ATTACHMENT_UUID = "f7cd5b02-ef73-4ee8-814b-c5a7a916685d";
+
+    final EntityAvatarInput newEntityAvatarInput =
+        EntityAvatarInput.builder().withApplyCrop(true).withAttachmentUuid(ENTITY_ATTACHMENT_UUID)
+            .withRelatedObjectType(LocationDao.TABLE_NAME).withRelatedObjectUuid(ENTITY_UUID)
+            .withCropHeight(1).withCropLeft(2).withCropWidth(3).withCropTop(4).build();
+
+    // Regular user can not do this
+    try {
+      withCredentials(getRegularUser().getDomainUsername(),
+          t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
+      fail("Expected exception creating entity avatar");
+    } catch (Exception expectedException) {
+      // OK
+    }
+
+    // Superuser can do this
+    Integer numRows = withCredentials(getSuperuser().getDomainUsername(),
+        t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
+    assertThat(numRows).isOne();
+
+    // Admin can do this
+    numRows = withCredentials(adminUser,
+        t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
+    assertThat(numRows).isOne();
+
+    // Get the entity avatar via the location
+    Location location =
+        withCredentials(adminUser, t -> queryExecutor.location(FIELDS, ENTITY_UUID));
+    EntityAvatar entityAvatar = location.getEntityAvatar();
+    assertThat(entityAvatar).isNotNull();
+    assertThat(entityAvatar.getRelatedObjectUuid()).isEqualTo(ENTITY_UUID);
+    assertThat(entityAvatar.getAttachmentUuid()).isEqualTo(ENTITY_ATTACHMENT_UUID);
+    assertThat(entityAvatar.getApplyCrop()).isTrue();
+    assertThat(entityAvatar.getCropHeight()).isEqualTo(1);
+    assertThat(entityAvatar.getCropLeft()).isEqualTo(2);
+    assertThat(entityAvatar.getCropWidth()).isEqualTo(3);
+    assertThat(entityAvatar.getCropTop()).isEqualTo(4);
+
+    // Update the entity avatar
+    newEntityAvatarInput.setCropHeight(10);
+    numRows = withCredentials(adminUser,
+        t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
+    assertThat(numRows).isOne();
+    location = withCredentials(adminUser, t -> queryExecutor.location(FIELDS, ENTITY_UUID));
+    entityAvatar = location.getEntityAvatar();
+    assertThat(entityAvatar).isNotNull();
+    assertThat(entityAvatar.getCropHeight()).isEqualTo(10);
+
+    // Delete the entity avatar
+    numRows = withCredentials(adminUser,
+        t -> mutationExecutor.deleteEntityAvatar("", LocationDao.TABLE_NAME, ENTITY_UUID));
+    assertThat(numRows).isOne();
+    location = withCredentials(adminUser, t -> queryExecutor.location(FIELDS, ENTITY_UUID));
+    entityAvatar = location.getEntityAvatar();
+    assertThat(entityAvatar).isNull();
+  }
 
   @Test
   void testOrganizationAvatar() {
     // For the test we use organization EF 2.2 which already has an attachment associated
-    final EntityAvatarInput newEntityAvatarInput = EntityAvatarInput.builder().withApplyCrop(true)
-        .withAttachmentUuid(EF22_ORGANIZATION_ATTACHMENT_UUID)
-        .withRelatedObjectType(OrganizationDao.TABLE_NAME)
-        .withRelatedObjectUuid(EF22_ORGANIZATION_UUID).withCropHeight(1).withCropLeft(2)
-        .withCropWidth(3).withCropTop(4).build();
+    final String ENTITY_UUID = "ccbee4bb-08b8-42df-8cb5-65e8172f657b";
+    final String ENTITY_ATTACHMENT_UUID = "9ac41246-25ac-457c-b7d6-946c5f625f1f";
+
+    final EntityAvatarInput newEntityAvatarInput =
+        EntityAvatarInput.builder().withApplyCrop(true).withAttachmentUuid(ENTITY_ATTACHMENT_UUID)
+            .withRelatedObjectType(OrganizationDao.TABLE_NAME).withRelatedObjectUuid(ENTITY_UUID)
+            .withCropHeight(1).withCropLeft(2).withCropWidth(3).withCropTop(4).build();
 
     // Jack Jackson can not do this
     try {
@@ -43,15 +106,15 @@ class EntityAvatarResourceTest extends AbstractResourceTest {
     // Admin can do this
     Integer numRows = withCredentials(adminUser,
         t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
-    assertThat(numRows).isEqualTo(1);
+    assertThat(numRows).isOne();
 
     // Get the entity avatar via the organization
     Organization organization =
-        withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, EF22_ORGANIZATION_UUID));
+        withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, ENTITY_UUID));
     EntityAvatar entityAvatar = organization.getEntityAvatar();
     assertThat(entityAvatar).isNotNull();
-    assertThat(entityAvatar.getRelatedObjectUuid()).isEqualTo(EF22_ORGANIZATION_UUID);
-    assertThat(entityAvatar.getAttachmentUuid()).isEqualTo(EF22_ORGANIZATION_ATTACHMENT_UUID);
+    assertThat(entityAvatar.getRelatedObjectUuid()).isEqualTo(ENTITY_UUID);
+    assertThat(entityAvatar.getAttachmentUuid()).isEqualTo(ENTITY_ATTACHMENT_UUID);
     assertThat(entityAvatar.getApplyCrop()).isTrue();
     assertThat(entityAvatar.getCropHeight()).isEqualTo(1);
     assertThat(entityAvatar.getCropLeft()).isEqualTo(2);
@@ -62,19 +125,17 @@ class EntityAvatarResourceTest extends AbstractResourceTest {
     newEntityAvatarInput.setCropHeight(10);
     numRows = withCredentials(adminUser,
         t -> mutationExecutor.createOrUpdateEntityAvatar("", newEntityAvatarInput));
-    assertThat(numRows).isEqualTo(1);
-    organization =
-        withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, EF22_ORGANIZATION_UUID));
+    assertThat(numRows).isOne();
+    organization = withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, ENTITY_UUID));
     entityAvatar = organization.getEntityAvatar();
     assertThat(entityAvatar).isNotNull();
     assertThat(entityAvatar.getCropHeight()).isEqualTo(10);
 
     // Delete the entity avatar
-    numRows = withCredentials(adminUser, t -> mutationExecutor.deleteEntityAvatar("",
-        OrganizationDao.TABLE_NAME, EF22_ORGANIZATION_UUID));
-    assertThat(numRows).isEqualTo(1);
-    organization =
-        withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, EF22_ORGANIZATION_UUID));
+    numRows = withCredentials(adminUser,
+        t -> mutationExecutor.deleteEntityAvatar("", OrganizationDao.TABLE_NAME, ENTITY_UUID));
+    assertThat(numRows).isOne();
+    organization = withCredentials(adminUser, t -> queryExecutor.organization(FIELDS, ENTITY_UUID));
     entityAvatar = organization.getEntityAvatar();
     assertThat(entityAvatar).isNull();
   }
