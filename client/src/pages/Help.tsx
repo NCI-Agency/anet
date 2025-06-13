bea49caa828fb232fff0335e254eb56ee5368a7c
jlnat <jlnat@users.noreply.github.com>
2025-06-13 16:41:09 +0200

    AB#1056 Update headings of help page

--- client/src/pages/Help.tsx
+++ client/src/pages/Help.tsx
@@ -169,16 +169,16 @@ const HelpConditional = ({
   return (
     <div className="help-page">
       <Fieldset title="Need help with ANET?">
-        <p className="help-text">There are a few ways to get help:</p>
+        <h1 className="help-text">There are a few ways to get help:</h1>
 
         {helpText && (
           <>
-            <h4>1. Read the help text</h4>
+            <h2>Read the help text</h2>
             <RichTextEditor readOnly value={helpText} />
           </>
         )}
 
-        <h4>{helpText ? "2" : "1"}. Use the guided tours</h4>
+        <h2>Use the guided tours</h2>
         <p>
           If you're stuck on a page and you don't know what to do, look for the{" "}
           <strong>"Take a tour"</strong> link near the top of the page.
@@ -189,9 +189,7 @@ const HelpConditional = ({
           style={screenshotCss}
         />
 
-        <h4>
-          {helpText ? "3" : "2"}. Contact your superuser or administrators
-        </h4>
+        <h2>Contact your superuser or administrator</h2>
         <p>
           Your organization's superusers and administrators are able to modify a
           lot of data in the system regarding how your organization, position
