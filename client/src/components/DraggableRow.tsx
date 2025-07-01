746affc094cb54aa0d81bad2db54d7607d445b08
jlnat <jlnat@users.noreply.github.com>
2025-07-01 13:05:55 +0200

    AB#1383 Add DraggableRow component

---
+++ client/src/components/DraggableRow.tsx
@@ -0,0 +1,75 @@
+import { Icon } from "@blueprintjs/core"
+import { IconNames } from "@blueprintjs/icons"
+import React, { useRef } from "react"
+import { useDrag, useDrop } from "react-dnd"
+
+interface DraggableRowProps {
+  itemType: string
+  row: any
+  index: number
+  moveRow: (from: number, to: number) => void
+  onDropRow: (uuid: string, toIndex: number) => void
+  dragHandleProps?: any
+  children: React.ReactNode
+}
+
+const DraggableRow = ({
+  itemType,
+  row,
+  index,
+  moveRow,
+  onDropRow,
+  dragHandleProps,
+  children
+}: DraggableRowProps) => {
+  const ref = useRef(null)
+  const [, drop] = useDrop({
+    accept: itemType,
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      if (item.index === index) {
+        return
+      }
+      moveRow(item.index, index)
+      item.index = index
+    }
+  })
+  const [{ isDragging: dragActive }, drag, preview] = useDrag({
+    type: itemType,
+    item: { uuid: row.uuid, index },
+    end: (item, monitor) => {
+      if (!monitor.didDrop()) {
+        return
+      }
+      onDropRow(item.uuid, item.index)
+    },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+  drag(drop(ref))
+  return (
+    <div
+      ref={ref}
+      style={{
+        opacity: dragActive ? 0.4 : 1,
+        background: dragActive ? "#f0f4ff" : undefined,
+        cursor: "pointer"
+      }}
+    >
+      <div className="d-flex align-items-center">
+        <span {...dragHandleProps} style={{ cursor: "grab", marginRight: 12 }}>
+          <Icon
+            icon={IconNames.DRAG_HANDLE_VERTICAL}
+            style={{ fontSize: 22 }}
+          />
+        </span>
+        <div className="flex-grow-1">{children}</div>
+      </div>
+    </div>
+  )
+}
+
+export default DraggableRow
