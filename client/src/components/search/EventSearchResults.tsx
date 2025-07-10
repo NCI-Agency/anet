d31916a941de92e602f7d14bcbd084f81a643303
Gertjan van Oosten <gertjan@west.nl>
2025-07-10 09:47:01 +0200

    AB#1383 Factor out common code for search results components

--- client/src/components/search/EventSearchResults.tsx
+++ client/src/components/search/EventSearchResults.tsx
@@ -1,11 +1,11 @@
 import { gql } from "@apollo/client"
-import API from "api"
 import EventTable from "components/EventTable"
 import { GRAPHQL_ENTITY_AVATAR_FIELDS } from "components/Model"
-import { PageDispatchersPropType, useBoilerplate } from "components/Page"
-import { DEFAULT_PAGESIZE } from "components/search/common"
-import _isEqual from "lodash/isEqual"
-import React, { useEffect, useRef, useState } from "react"
+import {
+  CommonSearchResults,
+  GenericSearchResultsProps
+} from "components/search/CommonSearchResults"
+import React from "react"
 
 const GQL_GET_EVENT_LIST = gql`
   query ($eventQuery: EventSearchQueryInput) {
@@ -57,77 +57,16 @@ const GQL_GET_EVENT_LIST = gql`
   }
 `
 
-interface EventSearchResultsProps {
-  pageDispatchers?: PageDispatchersPropType
-  queryParams?: any
-  setTotalCount?: (...args: unknown[]) => unknown
-  paginationKey?: string
-  pagination?: any
-  setPagination?: (...args: unknown[]) => unknown
-}
-
-const EventSearchResults = ({
-  pageDispatchers,
-  queryParams,
-  setTotalCount,
-  paginationKey,
-  pagination,
-  setPagination
-}: EventSearchResultsProps) => {
-  // (Re)set pageNum to 0 if the queryParams change, and make sure we retrieve page 0 in that case
-  const latestQueryParams = useRef(queryParams)
-  const queryParamsUnchanged = _isEqual(latestQueryParams.current, queryParams)
-  const [pageNum, setPageNum] = useState(
-    (queryParamsUnchanged && pagination?.[paginationKey]?.pageNum) ?? 0
-  )
-  useEffect(() => {
-    if (!queryParamsUnchanged) {
-      latestQueryParams.current = queryParams
-      setPagination?.(paginationKey, 0)
-      setPageNum(0)
-    }
-  }, [queryParams, setPagination, paginationKey, queryParamsUnchanged])
-  const eventQuery = {
-    ...queryParams,
-    pageNum: queryParamsUnchanged ? pageNum : 0,
-    pageSize: queryParams.pageSize || DEFAULT_PAGESIZE
-  }
-  const { loading, error, data } = API.useApiQuery(GQL_GET_EVENT_LIST, {
-    eventQuery
-  })
-  const { done, result } = useBoilerplate({
-    loading,
-    error,
-    pageDispatchers
-  })
-  // Update the total count
-  const totalCount = done ? null : data?.eventList?.totalCount
-  useEffect(() => {
-    setTotalCount?.(totalCount)
-  }, [setTotalCount, totalCount])
-  if (done) {
-    return result
-  }
-
-  const paginatedEvents = data ? data.eventList : []
-  const { pageSize, pageNum: curPage, list: events } = paginatedEvents
-
-  return (
-    <EventTable
-      events={events}
-      pageSize={setPagination && pageSize}
-      pageNum={setPagination && curPage}
-      totalCount={setPagination && totalCount}
-      goToPage={setPagination && setPage}
-      showEventSeries
-      id="events-search-results"
-    />
-  )
-
-  function setPage(pageNum) {
-    setPagination(paginationKey, pageNum)
-    setPageNum(pageNum)
-  }
-}
+const EventSearchResults = (props: GenericSearchResultsProps) => (
+  <CommonSearchResults
+    gqlQuery={GQL_GET_EVENT_LIST}
+    gqlQueryParamName="eventQuery"
+    gqlQueryResultName="eventList"
+    tableComponent={EventTable}
+    tableResultsProp="events"
+    tableId="events-search-results"
+    {...props}
+  />
+)
 
 export default EventSearchResults
--- client/src/components/search/PositionSearchResults.tsx
+++ client/src/components/search/PositionSearchResults.tsx
@@ -1,18 +1,12 @@
 import { gql } from "@apollo/client"
-import API from "api"
 import { GRAPHQL_ENTITY_AVATAR_FIELDS } from "components/Model"
-import { PageDispatchersPropType, useBoilerplate } from "components/Page"
 import PositionTable from "components/PositionTable"
 import {
-  _isAllSelected,
-  _isSelected,
-  _toggleAll,
-  _toggleSelection,
-  DEFAULT_PAGESIZE,
+  CommonSearchResults,
+  GenericSearchResultsWithEmailProps,
   GQL_EMAIL_ADDRESSES
-} from "components/search/common"
-import _isEqual from "lodash/isEqual"
-import React, { useEffect, useRef, useState } from "react"
+} from "components/search/CommonSearchResults"
+import React from "react"
 
 const GQL_GET_POSITION_LIST = gql`
   query ($positionQuery: PositionSearchQueryInput, $emailNetwork: String) {
@@ -52,120 +46,19 @@ const GQL_GET_POSITION_LIST = gql`
   }
 `
 
-interface PositionSearchResultsProps {
-  pageDispatchers?: PageDispatchersPropType
-  queryParams?: any
-  setTotalCount?: (...args: unknown[]) => unknown
-  paginationKey?: string
-  pagination?: any
-  setPagination?: (...args: unknown[]) => unknown
-  allowSelection?: boolean
-  updateRecipients?: (...args: unknown[]) => unknown
-}
-
-const PositionSearchResults = ({
-  pageDispatchers,
-  queryParams,
-  setTotalCount,
-  paginationKey,
-  pagination,
-  setPagination,
-  allowSelection,
-  updateRecipients
-}: PositionSearchResultsProps) => {
-  // (Re)set pageNum to 0 if the queryParams change, and make sure we retrieve page 0 in that case
-  const latestQueryParams = useRef(queryParams)
-  const queryParamsUnchanged = _isEqual(latestQueryParams.current, queryParams)
-  const [pageNum, setPageNum] = useState(
-    (queryParamsUnchanged && pagination?.[paginationKey]?.pageNum) ?? 0
-  )
-  const [selectedEmailAddresses, setSelectedEmailAddresses] = useState(
-    new Map()
-  )
-  useEffect(() => {
-    if (!queryParamsUnchanged) {
-      latestQueryParams.current = queryParams
-      setPagination?.(paginationKey, 0)
-      setPageNum(0)
-      setSelectedEmailAddresses(new Map())
-    }
-  }, [queryParams, setPagination, paginationKey, queryParamsUnchanged])
-  const positionQuery = {
-    ...queryParams,
-    pageNum: queryParamsUnchanged ? pageNum : 0,
-    pageSize: queryParams.pageSize || DEFAULT_PAGESIZE
-  }
-  const { emailNetwork } = queryParams
-  const { loading, error, data } = API.useApiQuery(GQL_GET_POSITION_LIST, {
-    positionQuery,
-    emailNetwork
-  })
-  const { done, result } = useBoilerplate({
-    loading,
-    error,
-    pageDispatchers
-  })
-  // Update the total count
-  const totalCount = done ? null : data?.positionList?.totalCount
-  useEffect(() => {
-    setTotalCount?.(totalCount)
-  }, [setTotalCount, totalCount])
-  if (done) {
-    return result
-  }
-
-  const paginatedPositions = data ? data.positionList : []
-  const { pageSize, pageNum: curPage, list: positions } = paginatedPositions
-
-  return (
-    <PositionTable
-      positions={positions}
-      showLocation
-      pageSize={setPagination && pageSize}
-      pageNum={setPagination && curPage}
-      totalCount={setPagination && totalCount}
-      goToPage={setPagination && setPage}
-      allowSelection={allowSelection}
-      selection={selectedEmailAddresses}
-      isAllSelected={isAllSelected}
-      toggleAll={toggleAll}
-      isSelected={isSelected}
-      toggleSelection={toggleSelection}
-      id="positions-search-results"
-    />
-  )
-
-  function setPage(pageNum) {
-    setPagination(paginationKey, pageNum)
-    setPageNum(pageNum)
-  }
-
-  function isAllSelected() {
-    return _isAllSelected(positions, selectedEmailAddresses)
-  }
-
-  function toggleAll() {
-    _toggleAll(
-      positions,
-      selectedEmailAddresses,
-      setSelectedEmailAddresses,
-      updateRecipients
-    )
-  }
-
-  function isSelected(uuid) {
-    return _isSelected(uuid, selectedEmailAddresses)
-  }
-
-  function toggleSelection(uuid, emailAddresses) {
-    _toggleSelection(
-      uuid,
-      emailAddresses,
-      selectedEmailAddresses,
-      setSelectedEmailAddresses,
-      updateRecipients
-    )
-  }
-}
+const PositionSearchResults = (props: GenericSearchResultsWithEmailProps) => (
+  <CommonSearchResults
+    gqlQuery={GQL_GET_POSITION_LIST}
+    gqlQueryParamName="positionQuery"
+    gqlQueryResultName="positionList"
+    tableComponent={PositionTable}
+    tableResultsProp="positions"
+    tableId="positions-search-results"
+    {...props}
+    extraProps={{
+      showLocation: true
+    }}
+  />
+)
 
 export default PositionSearchResults
