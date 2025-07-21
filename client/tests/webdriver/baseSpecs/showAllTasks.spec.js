767b0754f595db9854e07cef852a3e5bedc16dfa
jlnat <jlnat@users.noreply.github.com>
2025-07-21 17:50:55 +0200

    AB#1379 Remove Event matrix from top tasks page

--- client/tests/webdriver/baseSpecs/showAllTasks.spec.js
+++ client/tests/webdriver/baseSpecs/showAllTasks.spec.js
@@ -78,25 +78,4 @@ describe("Show All Tasks Page", () => {
       "No descendants should be present for EF 5"
     )
   })
-
-  it("Should show the event matrix for all tasks", async () => {
-    const eventMatrix = await ShowAllTasks.getEventMatrix()
-    await eventMatrix.waitForExist()
-    await eventMatrix.waitForDisplayed()
-    const tasksTableHeader = await ShowAllTasks.getTasksTableHeader()
-    await tasksTableHeader.waitForExist()
-    await tasksTableHeader.waitForDisplayed()
-
-    const topTasks = await ShowAllTasks.getAllTasks()
-    const tasksTableRows = await ShowAllTasks.getTasksTableRows()
-    // Assert that we have at least a sensible number of rows
-    expect(tasksTableRows.length).to.be.at.least(topTasks.length)
-
-    const containsStarIcon = await tasksTableRows.map(
-      async task => await task.$("img").isExisting()
-    )
-    // Assert that all the tasks have a star, displaying the full path
-    // eslint-disable-next-line no-unused-expressions
-    expect(containsStarIcon.every(Boolean)).to.be.true
-  })
 })
