6d02f99121451fd9900d224b0d32f0887878d7a2
Francisco del Castillo <francisco.delcastillo@devops.ncia.nato.int>
2025-07-23 11:34:34 +0200

    AB#1178 email subscription updates

--- insertBaseData-psql.sql
+++ insertBaseData-psql.sql
@@ -1833,7 +1833,7 @@ INSERT INTO "savedSearches" ("uuid", "name", "objectType", "ownerUuid", "query",
 
 -- Create some preferences
 INSERT INTO "preferences" ("uuid", "name", "description","type", "defaultValue", "createdAt", "updatedAt") VALUES
-    ('4fbb173a-501b-4823-a4b7-d3e623dcfadd', 'SUBSCRIPTIONS_EMAILS', 'Whether or not to receive emails related to subscriptions', 'BOOLEAN', 'TRUE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    ('4fbb173a-501b-4823-a4b7-d3e623dcfadd', 'SUBSCRIPTIONS_EMAILS', 'Whether or not to receive emails related to subscriptions', 'BOOLEAN', 'FALSE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO "preferences" ("uuid", "name", "description","type", "defaultValue", "createdAt", "updatedAt") VALUES
     ('0ba90456-6b31-49cc-9d2b-bd31eb23b5eb', 'REPORTS_EMAILS', 'Whether or not to receive emails related to reports', 'BOOLEAN', 'TRUE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
@@ -1841,7 +1841,7 @@ INSERT INTO "preferences" ("uuid", "name", "description","type", "defaultValue",
 INSERT INTO "peoplePreferences" ("personUuid", "preferenceUuid", "value", "createdAt", "updatedAt") VALUES
     ((SELECT uuid from people where "name" = 'Dmin, Arthur'), '4fbb173a-501b-4823-a4b7-d3e623dcfadd', 'FALSE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 INSERT INTO "peoplePreferences" ("personUuid", "preferenceUuid", "value", "createdAt", "updatedAt") VALUES
-    ((SELECT uuid from people where "name" = 'Dmin, Arthur'), '0ba90456-6b31-49cc-9d2b-bd31eb23b5eb', 'FALSE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+    ((SELECT uuid from people where "name" = 'Dmin, Arthur'), '0ba90456-6b31-49cc-9d2b-bd31eb23b5eb', 'TRUE', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- Update the link-text indexes
 REFRESH MATERIALIZED VIEW CONCURRENTLY "mv_lts_attachments";
