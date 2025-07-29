50ab8512a929e1948e3d61df1e29bcd517d8f827
Gertjan van Oosten <gertjan@west.nl>
2025-07-29 10:28:40 +0200

    AB#1398 Filter out inactive tasks at the leaf level
    Recursively filter out leaf tasks and sub-trees if they do not contain active tasks.

--- client/src/components/TaskTree.tsx
+++ client/src/components/TaskTree.tsx
@@ -4,6 +4,7 @@ import classNames from "classnames"
 import LinkTo from "components/LinkTo"
 import Model from "components/Model"
 import cloneDeep from "lodash/cloneDeep"
+import _isEmpty from "lodash/isEmpty"
 import React, { useCallback, useReducer } from "react"
 import utils from "utils"
 
@@ -92,7 +93,24 @@ function createTreeNodes(tasks: any[]): TreeNodeInfo[] {
       rootNodes.push(nodeMap[task.uuid])
     }
   })
-  return rootNodes
+  return filterOutInactiveNodes(rootNodes, taskMap)
+}
+
+function filterOutInactiveNodes(
+  nodes: TreeNodeInfo[],
+  taskMap: any
+): TreeNodeInfo[] {
+  if (_isEmpty(nodes)) {
+    return null
+  }
+  return nodes.filter((node: TreeNodeInfo) => {
+    node.childNodes = filterOutInactiveNodes(node.childNodes, taskMap)
+    if (_isEmpty(node.childNodes)) {
+      node.childNodes = null
+      return taskMap[node.id]?.status === Model.STATUS.ACTIVE
+    }
+    return true
+  })
 }
 
 interface TaskTreeProps {
