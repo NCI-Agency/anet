b257378574c234ab475b3cc64088ad04a1c5c83f
Gertjan van Oosten <gertjan@west.nl>
2025-08-19 17:25:16 +0200

    AB#236 Simplify back-end for getting nearby locations

---
+++ src/main/java/mil/dds/anet/beans/search/BoundingBox.java
@@ -0,0 +1,75 @@
+package mil.dds.anet.beans.search;
+
+import io.leangen.graphql.annotations.GraphQLInputField;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import java.util.Objects;
+
+public class BoundingBox implements Cloneable {
+  @GraphQLQuery
+  @GraphQLInputField
+  private double minLng;
+  @GraphQLQuery
+  @GraphQLInputField
+  private double minLat;
+  @GraphQLQuery
+  @GraphQLInputField
+  private double maxLng;
+  @GraphQLQuery
+  @GraphQLInputField
+  private double maxLat;
+
+  public double getMinLng() {
+    return minLng;
+  }
+
+  public void setMinLng(double minLng) {
+    this.minLng = minLng;
+  }
+
+  public double getMinLat() {
+    return minLat;
+  }
+
+  public void setMinLat(double minLat) {
+    this.minLat = minLat;
+  }
+
+  public double getMaxLng() {
+    return maxLng;
+  }
+
+  public void setMaxLng(double maxLng) {
+    this.maxLng = maxLng;
+  }
+
+  public double getMaxLat() {
+    return maxLat;
+  }
+
+  public void setMaxLat(double maxLat) {
+    this.maxLat = maxLat;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(minLng, minLat, maxLng, maxLat);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof BoundingBox)) {
+      return false;
+    }
+    final BoundingBox other = (BoundingBox) obj;
+    return Objects.equals(getMinLng(), other.getMinLng())
+        && Objects.equals(getMinLat(), other.getMinLat())
+        && Objects.equals(getMaxLng(), other.getMaxLng())
+        && Objects.equals(getMaxLat(), other.getMaxLat());
+  }
+
+  @Override
+  public Object clone() throws CloneNotSupportedException {
+    final BoundingBox clone = (BoundingBox) super.clone();
+    return clone;
+  }
+}
