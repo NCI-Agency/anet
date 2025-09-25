0cedb2d48988f731a6cc887be9d5305f30cca6c8
jlnat <jlnat@users.noreply.github.com>
2025-09-25 18:13:50 +0200

    AB#1447 Refactoring refreshing of adminSettings

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -1,4 +1,6 @@
+import { gql } from "@apollo/client"
 import { resetPages } from "actions"
+import API from "api"
 import AppContext from "components/AppContext"
 import GeneralBanner, {
   GENERAL_BANNER_LEVEL,
@@ -10,10 +12,19 @@ import GeneralBanner, {
 import Header from "components/Header"
 import NoPositionBanner from "components/NoPositionBanner"
 import SecurityBanner from "components/SecurityBanner"
-import React, { useContext, useEffect, useRef, useState } from "react"
+import React, { useContext, useEffect, useMemo, useRef, useState } from "react"
 import { connect } from "react-redux"
 
-const POLLING_INTERVAL = 60000
+const POLLING_INTERVAL = 60_000 // milliseconds
+
+const GQL_GET_ADMIN_SETTINGS = gql`
+  query TopBarAdminSettings {
+    adminSettings {
+      key
+      value
+    }
+  }
+`
 
 interface TopBarProps {
   handleTopbarHeight: (...args: unknown[]) => unknown
@@ -30,39 +41,52 @@ const TopBar = ({
   toggleMenuAction,
   handleSecurityBannerBottom
 }: TopBarProps) => {
-  const { appSettings, currentUser, loadAppData } = useContext(AppContext)
+  const { appSettings, currentUser } = useContext(AppContext)
   const [bannerVisibility, setBannerVisibility] = useState(false)
   const [height, setHeight] = useState(0)
   const topbarDiv = useRef()
 
+  const {
+    data: adminData,
+    startPolling,
+    stopPolling
+  } = API.useApiQuery(GQL_GET_ADMIN_SETTINGS)
+
   useEffect(() => {
-    let isLoading = false
+    startPolling?.(POLLING_INTERVAL)
+    return () => stopPolling?.()
+  }, [startPolling, stopPolling])
 
-    const tick = async () => {
-      if (isLoading) {
-        return
-      }
-      isLoading = true
-      try {
-        await loadAppData()
-      } catch {
-      } finally {
-        isLoading = false
-      }
+  const polledSettings = useMemo(() => {
+    const list = adminData?.adminSettings || []
+    if (!list.length) {
+      return undefined
     }
+    const output = {}
+    for (const setting of list) {
+      output[setting.key] = setting.value
+    }
+    return output
+  }, [adminData])
 
-    tick()
-    const id = setInterval(tick, POLLING_INTERVAL)
-    return () => clearInterval(id)
-  }, [loadAppData])
-
-  const visibilitySetting = parseInt(appSettings[GENERAL_BANNER_VISIBILITY], 10)
-  const bannerOptions = {
-    level: appSettings[GENERAL_BANNER_LEVEL],
-    message: appSettings[GENERAL_BANNER_TEXT],
-    title: GENERAL_BANNER_TITLE,
-    visible: bannerVisibility
-  }
+  const effectiveSettings = polledSettings || appSettings
+  const visibilitySetting = parseInt(
+    effectiveSettings[GENERAL_BANNER_VISIBILITY],
+    10
+  )
+  const bannerOptions = useMemo(
+    () => ({
+      level: effectiveSettings[GENERAL_BANNER_LEVEL],
+      message: effectiveSettings[GENERAL_BANNER_TEXT],
+      title: GENERAL_BANNER_TITLE,
+      visible: bannerVisibility
+    }),
+    [
+      effectiveSettings[GENERAL_BANNER_LEVEL],
+      effectiveSettings[GENERAL_BANNER_TEXT],
+      bannerVisibility
+    ]
+  )
 
   useEffect(() => {
     function updateTopbarHeight() {
