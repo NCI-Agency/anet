dec29dc56b5b3aa65468bdd612428130a48b1ec9
Gertjan van Oosten <gertjan@west.nl>
2025-10-09 15:57:23 +0200

    AB#1469 Avoid showing '0' if conditions are falsy

--- client/src/pages/organizations/Show.tsx
+++ client/src/pages/organizations/Show.tsx
@@ -679,7 +679,7 @@ const OrganizationShow = ({ pageDispatchers }: OrganizationShowProps) => {
           )}
         </Fieldset>
 
-        {(allTasks.length || allEventSeries.length) && (
+        {(!!allTasks.length || !!allEventSeries.length) && (
           <Fieldset
             id="syncMatrix"
             title={`Sync matrix for ${organization.shortName}`}
