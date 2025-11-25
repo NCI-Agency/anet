4b0195e9c6e037030cbf69154af31d1eb63a0029
jlnat <jlnat@users.noreply.github.com>
2025-11-25 11:47:50 +0100

    AB#1505 Add LocationModal

---
+++ client/src/pages/locations/LocationModal.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+import { Modal } from "react-bootstrap"
+import CreateNewLocation from "./CreateNewLocation"
+
+interface LocationModalProps {
+  show: boolean
+  onHide: () => void
+  name?: string
+  coords: { lat: number; lng: number } | null
+  setFieldTouched: (...args: unknown[]) => unknown
+  setFieldValue: (...args: unknown[]) => unknown
+}
+
+const LocationModal = ({
+  show,
+  onHide,
+  name,
+  coords,
+  setFieldTouched,
+  setFieldValue
+}: LocationModalProps) => {
+  const lat = coords?.lat
+  const lng = coords?.lng
+
+  return (
+    <Modal backdrop="static" centered size="xl" show={show} onHide={onHide}>
+      <Modal.Body>
+        <CreateNewLocation
+          name={name}
+          lat={lat}
+          lng={lng}
+          setFieldTouched={setFieldTouched}
+          setFieldValue={setFieldValue}
+          setDoReset={() => {
+            onHide()
+          }}
+        />
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default LocationModal
