ba715258eb7f141158287aa2c66b753a5fc4fb63
Gertjan van Oosten <gertjan@west.nl>
2025-12-09 08:35:56 +0100

    AB#1084 Move "Edit history" button below the table
    This also makes the position history table wider, so more information can be shown without wrapping.

--- client/src/components/PreviousPositions.tsx
+++ client/src/components/PreviousPositions.tsx
@@ -1,51 +1,72 @@
+import { Icon, IconSize } from "@blueprintjs/core"
+import { IconNames } from "@blueprintjs/icons"
 import LinkTo from "components/LinkTo"
 import _isEmpty from "lodash/isEmpty"
 import moment from "moment"
 import React from "react"
-import { Table } from "react-bootstrap"
+import { Button, OverlayTrigger, Table, Tooltip } from "react-bootstrap"
 import Settings from "settings"
 
 interface PreviousPositionsProps {
   history?: any[]
+  canEditHistory?: boolean
   action?: (...args: unknown[]) => unknown
 }
 
 function PreviousPositions({
   history: previousPositions,
+  canEditHistory,
   action
 }: PreviousPositionsProps) {
   return _isEmpty(previousPositions) ? (
     <em>No positions found</em>
   ) : (
-    <Table id="previous-positions" striped hover responsive>
-      <thead>
-        <tr>
-          <th>Position</th>
-          <th>Dates</th>
-          {action && <th>Action</th>}
-        </tr>
-      </thead>
-      <tbody>
-        {previousPositions.map((pp, idx) => (
-          <tr key={idx} id={`previousPosition_${idx}`}>
-            <td>
-              <LinkTo modelType="Position" model={pp.position} />
-            </td>
-            <td>
-              {moment(pp.startTime).format(
-                Settings.dateFormats.forms.displayShort.date
-              )}{" "}
-              - &nbsp;
-              {pp.endTime &&
-                moment(pp.endTime).format(
-                  Settings.dateFormats.forms.displayShort.date
-                )}
-            </td>
-            {action && <td>{action(pp, idx)}</td>}
+    <>
+      <Table id="previous-positions" striped hover responsive>
+        <thead>
+          <tr>
+            <th>Position</th>
+            <th>Dates</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {previousPositions.map((pp, idx) => (
+            <tr key={idx} id={`previousPosition_${idx}`}>
+              <td>
+                <LinkTo modelType="Position" model={pp.position} />
+              </td>
+              <td>
+                {moment(pp.startTime).format(
+                  Settings.dateFormats.forms.displayShort.date
+                )}{" "}
+                - &nbsp;
+                {pp.endTime &&
+                  moment(pp.endTime).format(
+                    Settings.dateFormats.forms.displayShort.date
+                  )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      {canEditHistory && (
+        <div className="w-100">
+          <div className="float-end">
+            <OverlayTrigger
+              key="edit-history-overlay"
+              placement="top"
+              overlay={
+                <Tooltip id="edit-history-tooltip">Edit history</Tooltip>
+              }
+            >
+              <Button onClick={action} className="edit-history float-end">
+                <Icon size={IconSize.LARGE} icon={IconNames.EDIT} />
+              </Button>
+            </OverlayTrigger>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
