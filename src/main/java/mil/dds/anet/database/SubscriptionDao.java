78f8b5bf68f2a655802304227cb1aa7ea1a12e8a
Gertjan van Oosten <gertjan@west.nl>
2026-01-06 09:17:07 +0100

    AB#1465 Move insert of subscription updates to SubscriptionUpdateDao

--- src/main/java/mil/dds/anet/database/SubscriptionDao.java
+++ src/main/java/mil/dds/anet/database/SubscriptionDao.java
@@ -10,18 +10,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
-import mil.dds.anet.beans.AnetEmail;
 import mil.dds.anet.beans.Person;
 import mil.dds.anet.beans.Position;
 import mil.dds.anet.beans.Subscription;
-import mil.dds.anet.beans.SubscriptionUpdate;
 import mil.dds.anet.beans.lists.AnetBeanList;
 import mil.dds.anet.beans.search.SubscriptionSearchQuery;
 import mil.dds.anet.database.mappers.SubscriptionMapper;
-import mil.dds.anet.database.mappers.SubscriptionUpdateMapper;
-import mil.dds.anet.emails.SubscriptionUpdateEmail;
 import mil.dds.anet.search.pg.PostgresqlSubscriptionSearcher;
-import mil.dds.anet.threads.AnetEmailWorker;
 import mil.dds.anet.utils.DaoUtils;
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.mapper.MapMapper;
@@ -42,8 +37,12 @@ public class SubscriptionDao extends AnetBaseDao<Subscription, SubscriptionSearc
   public static final String SUBSCRIPTION_FIELDS =
       DaoUtils.buildFieldAliases(TABLE_NAME, fields, true);
 
-  public SubscriptionDao(DatabaseHandler databaseHandler) {
+  private final SubscriptionUpdateDao subscriptionUpdateDao;
+
+  public SubscriptionDao(DatabaseHandler databaseHandler,
+      SubscriptionUpdateDao subscriptionUpdateDao) {
     super(databaseHandler);
+    this.subscriptionUpdateDao = subscriptionUpdateDao;
   }
 
   @Override
@@ -159,81 +158,10 @@ public class SubscriptionDao extends AnetBaseDao<Subscription, SubscriptionSearc
       final int nRows = handle.createUpdate(sqlPre + sqlSuf)
           .bind("updatedAt", DaoUtils.asLocalDateTime(subscriptionUpdate.getUpdatedAt()))
           .bindMap(params).execute();
-      insertSubscriptionUpdates(subscriptionUpdate, auditTrailUuid);
+      subscriptionUpdateDao.insert(subscriptionUpdate, auditTrailUuid);
       return nRows;
     } finally {
       closeDbHandle(handle);
-    }
-  }
-
-  private int insertSubscriptionUpdates(SubscriptionUpdateGroup subscriptionUpdate,
-      String auditTrailUuid) {
-    final Handle handle = getDbHandle();
-    try {
-      final StringBuilder sqlPre =
-          new StringBuilder("/* insertSubscriptionUpdates */ INSERT INTO \"subscriptionUpdates\""
-              + " (\"subscriptionUuid\", \"updatedObjectType\", \"updatedObjectUuid\", \"isNote\","
-              + " \"auditTrailUuid\", \"createdAt\")"
-              + " SELECT s.uuid, :updatedObjectType, :updatedObjectUuid, :isNote,"
-              + " :auditTrailUuid, :createdAt FROM subscriptions s WHERE ");
-      final String paramObjectTypeTpl = "objectType%1$d";
-      final String stmtTpl =
-          "( \"subscribedObjectType\" = :%1$s AND \"subscribedObjectUuid\" IN ( %2$s ) )";
-      final List<String> stmts = new ArrayList<>();
-      final Map<String, Object> params = new HashMap<>();
-      final ListIterator<SubscriptionUpdateStatement> iter =
-          subscriptionUpdate.getStmts().listIterator();
-      while (iter.hasNext()) {
-        final String objectTypeParam = String.format(paramObjectTypeTpl, iter.nextIndex());
-        final SubscriptionUpdateStatement stmt = iter.next();
-        if (stmt != null && stmt.sql != null && stmt.objectType != null && stmt.params != null) {
-          stmts.add(String.format(stmtTpl, objectTypeParam, stmt.sql));
-          params.put(objectTypeParam, stmt.objectType);
-          params.putAll(stmt.params);
-        }
-      }
-      final String sqlSuf = "( " + Joiner.on(" OR ").join(stmts) + " ) RETURNING"
-          + SubscriptionUpdateDao.SUBSCRIPTION_UPDATE_FIELDS;
-      logger.info(
-          "Inserting subscription updates: sql={}, createdAt={}, updatedObjectType={}, "
-              + "updatedObjectUuid={}, isNote={}, auditTrailUuid={}, params={}",
-          sqlSuf, subscriptionUpdate.getUpdatedAt(), subscriptionUpdate.getObjectType(),
-          subscriptionUpdate.getObjectUuid(), subscriptionUpdate.isNote(), auditTrailUuid, params);
-
-      final List<SubscriptionUpdate> subscriptionUpdates = handle.createQuery(sqlPre + sqlSuf)
-          .bind("createdAt", DaoUtils.asLocalDateTime(subscriptionUpdate.getUpdatedAt()))
-          .bind("updatedObjectType", subscriptionUpdate.getObjectType())
-          .bind("updatedObjectUuid", subscriptionUpdate.getObjectUuid())
-          .bind("isNote", subscriptionUpdate.isNote()).bind("auditTrailUuid", auditTrailUuid)
-          .bindMap(params).map(new SubscriptionUpdateMapper()).list();
-      sendEmailToSubscribers(subscriptionUpdates);
-      return subscriptionUpdates.size();
-    } finally {
-      closeDbHandle(handle);
-    }
-  }
-
-  private void sendEmailToSubscribers(List<SubscriptionUpdate> subscriptionUpdates) {
-    final GraphQLContext context = engine().getContext();
-    for (final SubscriptionUpdate subscriptionUpdate : subscriptionUpdates) {
-      final Subscription subscription = subscriptionUpdate.loadSubscription(context).join();
-      final Position subscribedPosition = subscription.loadSubscriber(context).join();
-      final Person subscriber = subscribedPosition.loadPerson(context).join();
-      final List<String> addresses = getEmailAddressesBasedOnPreference(List.of(subscriber),
-          PreferenceDao.PREFERENCE_SUBSCRIPTIONS, PreferenceDao.CATEGORY_EMAILING);
-      if (!addresses.isEmpty()) {
-        final SubscriptionUpdateEmail action = new SubscriptionUpdateEmail();
-        action.setSubscriptionUuid(subscriptionUpdate.getSubscriptionUuid());
-        action.setUpdatedObjectType(subscriptionUpdate.getUpdatedObjectType());
-        action.setUpdatedObjectUuid(subscriptionUpdate.getUpdatedObjectUuid());
-        action.setAuditTrailUuid(subscriptionUpdate.getAuditTrailUuid());
-        action.setIsNote(subscriptionUpdate.getIsNote());
-        action.setCreatedAt(subscriptionUpdate.getCreatedAt());
-        final AnetEmail email = new AnetEmail();
-        email.setAction(action);
-        email.setToAddresses(addresses);
-        AnetEmailWorker.sendEmailAsync(email);
-      }
     }
   }
 
