3546e84416fd00fd8be8d407ffa205f34de6bd42
Gertjan van Oosten <gertjan@west.nl>
2026-01-26 10:31:40 +0100

    AB#1535 Fix ESLint errors

--- client/src/pages/reports/Show.tsx
+++ client/src/pages/reports/Show.tsx
@@ -242,7 +242,7 @@ const ReportShow = ({ setSearchQuery, pageDispatchers }: ReportShowProps) => {
     return [
       {
         label: "Review",
-        prompt: `Review the report`,
+        prompt: "Review the report",
         icon: "check-circle",
         iconColor: "#19aa28ff"
       },
