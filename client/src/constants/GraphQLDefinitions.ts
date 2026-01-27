8a4a8d3bbe047cf3220e2f4261df54293daf4663
Gertjan van Oosten <gertjan@west.nl>
2026-01-27 14:43:02 +0100

    AB#1533 Refactor changes to be more in line with existing code

--- client/src/constants/GraphQLDefinitions.ts
+++ client/src/constants/GraphQLDefinitions.ts
@@ -24,8 +24,8 @@ export const gqlBasicAttachmentFields = `
   ${gqlMinimalAttachmentFields}
   createdAt
   updatedAt
-  description
   classification
+  description
 `
 
 export const gqlAllAttachmentFields = `
