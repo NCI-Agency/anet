6b1986ff09d468faf04a9af2bd1270523e88d7b6
Gertjan van Oosten <gertjan@west.nl>
2026-03-17 15:40:43 +0100

    Move routing inside the AppContext

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,11 @@
 import { ApolloProvider } from "@apollo/client/react"
 import { OverlaysProvider } from "@blueprintjs/core"
 import API from "api"
-import { jumpToTop } from "components/Page"
 import "locale-compare-polyfill"
 import App from "pages/App"
 import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
-import { BrowserRouter } from "react-router-dom"
 import { persistStore } from "redux-persist"
 import { PersistGate } from "redux-persist/lib/integration/react"
 import "./index.css"
@@ -27,11 +25,9 @@ root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <ApolloProvider client={API.client}>
-        <BrowserRouter onUpdate={jumpToTop}>
-          <OverlaysProvider>
-            <App />
-          </OverlaysProvider>
-        </BrowserRouter>
+        <OverlaysProvider>
+          <App />
+        </OverlaysProvider>
       </ApolloProvider>
     </PersistGate>
   </Provider>
