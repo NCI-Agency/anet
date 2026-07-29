abea38bec46e55b033fa2feebec2a8be75e70d64
Gertjan van Oosten <gertjan@west.nl>
2026-07-29 17:01:09 +0200

    Update imports for js-yaml v5

--- client/platform/node/settings.js
+++ client/platform/node/settings.js
@@ -1,13 +1,13 @@
 import fs from "fs"
-import jsyaml from "js-yaml"
+import { load, loadAll } from "js-yaml"
 
 const anetConfigFile = "../application.yml"
 console.log(`Using config file ${anetConfigFile}`)
-const anetConfig = jsyaml.loadAll(fs.readFileSync(anetConfigFile, "utf8"))
+const anetConfig = loadAll(fs.readFileSync(anetConfigFile, "utf8"))
 const anetDictionaryFile =
   process.env.ANET_DICTIONARY || "../anet-dictionary.yml"
 console.log(`Using dictionary file ${anetDictionaryFile}`)
-const anetDictionary = jsyaml.load(fs.readFileSync(anetDictionaryFile, "utf8"))
+const anetDictionary = load(fs.readFileSync(anetDictionaryFile, "utf8"))
 const Settings = anetDictionary
 const {
   realm,
